7355e632ed5f42fab0194decdc942babe3abbee6
smartapant <smartapant@gmail.com>
2017-02-16 15:52:20 +0300

    feat(vuex): add vuex to repo

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { sync } from 'vuex-router-sync'
+import store from './store'
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  strict: true,  // process.env.NODE_ENV !== 'production',
+  modules: {
+
+  },
+  state: {
+  },
+  mutations: {
+  }
+})
+
+export default store
