47eaf21691e6d2f409427bd1acecf374dea07866
smartapant <smartapant@gmail.com>
2017-02-17 17:55:51 +0300

    feat(navbar): icons and layout

--- src/components/layout/navbar/Navbar.vue
+++ src/components/layout/navbar/Navbar.vue
@@ -1,26 +1,19 @@
 <template>
-  <nav class="navbar app-navbar navbar-toggleable-md navbar-light bg-faded">
+  <nav class="navbar app-navbar navbar-toggleable-md">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <a class="navbar-brand" href="#">Navbar</a>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#"><span class="i-nav-messages"></span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+          <a class="nav-link" href="#"><span class="i-nav-notification"></span></a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
     </div>
   </nav>
 </template>
@@ -35,6 +28,9 @@
   @import "../../../sass/_variables.scss";
 
   .navbar.app-navbar {
+    height: $top-nav-height;
+    padding-left: $nav-padding-left;
+    padding-right: $nav-padding-right;
     background-color: $top-nav-bg;
   }
 </style>
