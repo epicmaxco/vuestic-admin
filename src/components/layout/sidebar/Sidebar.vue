ca3e69d12336b54da2dd5d34d94633a28231820e
smartapant <smartapant@gmail.com>
2017-02-18 16:39:57 +0300

    fix(sidebar): delete expanding el

--- src/components/layout/sidebar/Sidebar.vue
+++ src/components/layout/sidebar/Sidebar.vue
@@ -1,45 +1,38 @@
 <template>
- <aside class="sidebar">
-   <router-link to="/charts" aria-expanded="show" @click.native="show = !show">
-     <span class="icon is-small"><i class="fa fa-bar-chart-o"></i></span>
-     Charts
-     <span class="icon is-small is-angle"><i class="fa fa-angle-down"></i></span>
-   </router-link>
-   <expanding>
-     <ul v-show="show">
-       <li>
-         <router-link to="/charts/chartjs">
-           ChartJs
-         </router-link>
-       </li>
-       <li>
-         <router-link to="/charts/chartist">
-           Chartist
-         </router-link>
-       </li>
-       <li>
-         <router-link to="/charts/peity">
-           Peity
-         </router-link>
-       </li>
-       <li>
-         <router-link to="/charts/plotly">
-           Plotly
-         </router-link>
-       </li>
-     </ul>
-   </expanding>
- </aside>
+  <aside class="sidebar">
+    <router-link to="/charts" aria-expanded="show" @click.native="show = !show">
+      <span class="icon is-small"><i class="fa fa-bar-chart-o"></i></span>
+      Charts
+      <span class="icon is-small is-angle"><i class="fa fa-angle-down"></i></span>
+    </router-link>
+    <ul v-show="show">
+      <li>
+        <router-link to="/charts/chartjs">
+          ChartJs
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/charts/chartist">
+          Chartist
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/charts/peity">
+          Peity
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/charts/plotly">
+          Plotly
+        </router-link>
+      </li>
+    </ul>
+  </aside>
 </template>
 
 <script>
-  import Expanding from 'vue-bulma-expanding'
-
   export default {
     name: 'sidebar',
-    components: {
-      Expanding
-    },
     data () {
       return {
         show: false
