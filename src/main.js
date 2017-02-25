62707bbe356662ba5f1468e6fa3ff0847d40410f
smartapant <smartapant@gmail.com>
2017-02-25 21:14:32 +0300

    feat(vue bootstrap): add vue bootstrap dependency

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import store from './store'
 import router from './router'
 import { sync } from 'vuex-router-sync'
+
+Vue.use(BootstrapVue)
 
 sync(store, router)
 
