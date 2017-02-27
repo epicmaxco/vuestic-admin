a5c84a26892505080b18c9a6b44bc3ef615374b3
smartapant <smartapant@gmail.com>
2017-02-27 18:03:09 +0300

    feat(navbar): close navbar on body click

--- src/directives/Dropdown.js
+++ src/directives/Dropdown.js
@@ -1,7 +1,17 @@
 export default function (el) {
   let ddToggle = el.querySelector('.dropdown-toggle')
+  let ddMenu = el.querySelector('.dropdown-menu')
 
-  ddToggle.addEventListener('click', function () {
+  ddToggle.addEventListener('click', function (evt) {
     el.classList.toggle('show')
+    evt.stopPropagation()
+  })
+
+  window.addEventListener('click', function () {
+    el.classList.remove('show')
+  })
+
+  ddMenu.addEventListener('click', function (evt) {
+    evt.stopPropagation()
   })
 }
