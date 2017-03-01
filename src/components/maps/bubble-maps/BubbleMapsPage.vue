daa59bf8f35989ed1fdd5837bde9c5bdccc37657
smartapant <smartapant@gmail.com>
2017-03-01 16:17:07 +0300

    feat(maps): bubble maps refactor

--- src/components/maps/bubble-maps/BubbleMapsPage.vue
+++ src/components/maps/bubble-maps/BubbleMapsPage.vue
@@ -2,7 +2,7 @@
   <div class="bubble-maps-page">
     <div class="row">
       <widget class="col-sm-12">
-        <bubble-map></bubble-map>
+        <bubble-map v-bind:map-data="bubbleMapData"></bubble-map>
       </widget>
     </div>
   </div>
@@ -11,12 +11,18 @@
 <script>
   import BubbleMap from './BubbleMap'
   import Widget from 'components/common/widget/Widget'
+  import BubbleMapData from './BubbleMapData'
 
   export default {
     name: 'bubble-maps-page',
     components: {
       BubbleMap,
       Widget
+    },
+    data: function () {
+      return {
+        bubbleMapData: BubbleMapData
+      }
     }
   }
 </script>
