97e38f7a45dbe0258ca6303bef10aec0e9e030c6
smartapant <smartapant@gmail.com>
2017-04-25 02:05:55 +0300

    feat(buttons): primary buttons

--- src/components/ui/buttons/Buttons.vue
+++ src/components/ui/buttons/Buttons.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="ui-buttons">
+  <div class="buttons-page-page">
     <div class="row">
-      <widget class="col-md-4">
-        Buttons
+      <widget class="col-sm-12"  header-text="Primary & Secondary Buttons">
+        <div class="row">
+          <div class="col-sm-3">
+            <button class="btn btn-primary">
+              BUTTON
+            </button>
+          </div>
+          <div class="col-sm-3">
+            <button class="btn btn-primary hover">
+              HOVER
+            </button>
+          </div>
+          <div class="col-sm-3">
+            <button class="btn btn-primary focus">
+              PRESSED
+            </button>
+          </div>
+          <div class="col-sm-3">
+            <button class="btn btn-primary" disabled>
+              DISABLED
+            </button>
+          </div>
+        </div>
       </widget>
     </div>
   </div>
