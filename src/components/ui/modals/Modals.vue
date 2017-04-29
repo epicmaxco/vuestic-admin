4eea01366b45ac577f259eb42dd03657a2d9b91a
smartapant <smartapant@gmail.com>
2017-04-30 01:25:36 +0300

    feat(modals): small and large modals

--- src/components/ui/modals/Modals.vue
+++ src/components/ui/modals/Modals.vue
@@ -8,18 +8,39 @@
         <button class="btn btn-info" @click="showMediumModal()">
           MEDIUM
         </button>
-        <button class="btn btn-warning">
+        <button class="btn btn-warning" @click="showLargeModal()">
           LARGE
         </button>
       </widget>
     </div>
 
     <!--//Modals-->
-    <modal :show.sync="show" ref="smallModal">
-      Modal Text
+    <modal :show.sync="show" ref="smallModal" v-bind:small="true">
+      <div slot="title">Small modal</div>
+      <div>
+        There are three species of zebras: the plains zebra, the mountain zebra and the Grévy's zebra. The plains zebra
+        and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus
+        Dolichohippus. The latter resembles an ass, to which it is closely related, while the former two are more
+        horse-like. All three belong to the genus Equus, along with other living equids.
+      </div>
+      <div slot="footer">
+        <button class="btn btn-primary">REPLY</button>
+      </div>
     </modal>
     <modal :show.sync="show" ref="mediumModal">
       <div slot="title">Medium modal</div>
+      <div>
+        There are three species of zebras: the plains zebra, the mountain zebra and the Grévy's zebra. The plains zebra
+        and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus
+        Dolichohippus. The latter resembles an ass, to which it is closely related, while the former two are more
+        horse-like. All three belong to the genus Equus, along with other living equids.
+      </div>
+      <div slot="footer">
+        <button class="btn btn-primary">REPLY</button>
+      </div>
+    </modal>
+    <modal :show.sync="show" v-bind:large="true" ref="largeModal">
+      <div slot="title">Large Modal</div>
       <div>
         There are three species of zebras: the plains zebra, the mountain zebra and the Grévy's zebra. The plains zebra
         and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus
@@ -54,6 +75,9 @@
       },
       showMediumModal () {
         this.$refs.mediumModal.open()
+      },
+      showLargeModal () {
+        this.$refs.largeModal.open()
       }
     }
   }
