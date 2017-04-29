ab3fb958806386837c75fffc6ac0f65748c1d99c
smartapant <smartapant@gmail.com>
2017-04-29 17:30:22 +0300

    feat(modals): modal component

---
+++ src/components/common/modal/Modal.vue
@@ -0,0 +1,133 @@
+<template>
+  <div v-show="show" :transition="transition">
+    <div class="modal" @click.self="clickMask">
+      <div class="modal-dialog" :class="modalClass">
+        <div class="modal-content">
+          <!--Header-->
+          <div class="modal-header">
+            <slot name="header">
+              <!--<a type="button" class="close" @click="cancel">x</a>-->
+              <h4 class="modal-title">
+                <slot name="title"></slot>
+              </h4>
+            </slot>
+          </div>
+          <!--Container-->
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+          <!--Footer-->
+          <div class="modal-footer">
+            <slot name="footer"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop in"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'modal',
+    props: {
+      transition: {
+        type: String,
+        default: 'modal'
+      },
+      small: {
+        type: Boolean,
+        default: false
+      },
+      large: {
+        type: Boolean,
+        default: false
+      },
+      full: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data () {
+      return {
+        show: false
+      }
+    },
+    computed: {
+      modalClass () {
+        return {
+          'modal-lg': this.large,
+          'modal-sm': this.small,
+          'modal-full': this.full
+        }
+      }
+    },
+    created () {
+      if (this.show) {
+        document.body.className += ' modal-open'
+      }
+    },
+    beforeDestroy () {
+      document.body.className = document.body.className.replace(/\s?modal-open/, '')
+    },
+    watch: {
+      show (value) {
+        if (value) {
+          document.body.className += ' modal-open'
+        } else {
+          if (!this.duration) {
+            this.duration = window.getComputedStyle(this.$el)['transition-duration'].replace('s', '') * 1000
+          }
+          window.setTimeout(() => {
+            document.body.className = document.body.className.replace(/\s?modal-open/, '')
+          }, this.duration || 0)
+        }
+      }
+    },
+    methods: {
+      ok () {
+        this.$emit('ok')
+        if (this.closeWhenOK) {
+          this.show = false
+        }
+      },
+      cancel () {
+        this.$emit('cancel')
+        this.show = false
+      },
+      clickMask () {
+        this.cancel()
+      },
+      open () {
+        this.show = true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../../../sass/_variables.scss";
+
+  .modal {
+    display: block;
+  }
+  .modal-transition {
+    transition: all .6s ease;
+  }
+  .modal-leave {
+    border-radius: 1px !important;
+  }
+  .modal-transition .modal-dialog, .modal-transition .modal-backdrop {
+    transition: all .5s ease;
+  }
+  .modal-enter .modal-dialog, .modal-leave .modal-dialog {
+    opacity: 0;
+    transform: translateY(-30%);
+  }
+  .modal-enter .modal-backdrop, .modal-leave .modal-backdrop {
+    opacity: 0;
+  }
+  .modal-backdrop {
+    opacity: 0.5;
+  }
+</style>
--- src/components/ui/modals/Modals.vue
+++ src/components/ui/modals/Modals.vue
@@ -2,24 +2,41 @@
   <div class="modals-page">
     <div class="row">
       <widget class="col-sm-12 modals-list larger-padding" header-text="Modals">
-        <button class="btn btn-danger">
-          MINI
+        <button class="btn btn-danger" @click="showSmallModal()">
+          SMALL
         </button>
         <button class="btn btn-info">
           DETAILED
         </button>
       </widget>
     </div>
+
+    <!--//Modals-->
+    <modal :show.sync="show" ref="smallModal">
+      Modal Text
+    </modal>
   </div>
 </template>
 
 <script>
   import Widget from 'components/common/widget/Widget'
+  import Modal from 'components/common/modal/Modal'
 
   export default {
     name: 'modals',
     components: {
-      Widget
+      Widget,
+      Modal
+    },
+    data () {
+      return {
+        show: true
+      }
+    },
+    methods: {
+      showSmallModal () {
+        this.$refs.smallModal.open()
+      }
     }
   }
 </script>
