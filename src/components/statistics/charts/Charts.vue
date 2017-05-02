059f9615492ebb214e7806baf69972920b9f73cc
smartapant <smartapant@gmail.com>
2017-05-02 19:03:00 +0300

    feat(charts): smoothed line chart stub

--- src/components/statistics/charts/Charts.vue
+++ src/components/statistics/charts/Charts.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="charts">
-    Charts!
+  <div class="charts-page">
+    <div class="row">
+      <widget class="col-sm-12 widget-viewport-height"  header-text="Smoothed Line Chart">
+        <smoothed-line-chart v-bind:chart-data="smoothedLineChartData"></smoothed-line-chart>
+      </widget>
+    </div>
   </div>
 </template>
 
 <script>
+  import SmoothedLineChart from './smoothed-line-chart/SmoothedLineChart.vue'
+  import SmoothedLineChartData from './smoothed-line-chart/SmoothedLineChartData'
+  import Widget from 'components/common/widget/Widget'
+
   export default {
-    name: 'charts'
+    name: 'charts',
+    components: {
+      Widget,
+      SmoothedLineChart
+    },
+    data: function () {
+      return {
+        smoothedLineChartData: SmoothedLineChartData
+      }
+    }
   }
 </script>
 
---
+++ src/components/statistics/charts/smoothed-line-chart/SmoothedLineChart.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="smoothed-line-chart"></div>
+</template>
+
+<script>
+  import 'amcharts3'
+  import 'amcharts3/amcharts/plugins/responsive/responsive.js'
+  import 'amcharts3/amcharts/serial.js'
+  import 'amcharts3/amcharts/themes/light'
+
+  import 'ammap3'
+  import 'ammap3/ammap/maps/js/worldLow'
+
+  export default {
+    name: 'smoothed-line-chart',
+
+    props: ['chartData'],
+
+    methods: {
+      drawMap () {
+        /* global AmCharts */
+        let map
+        let minBulletSize = 3
+        let maxBulletSize = 70
+        let min = Infinity
+        let max = -Infinity
+        AmCharts.theme = AmCharts.themes.light
+
+        // get min and max values
+        this.chartData.data.forEach((dataItem) => {
+          let value = dataItem.value
+          if (value < min) {
+            min = value
+          }
+          if (value > max) {
+            max = value
+          }
+        })
+
+        // build map
+        map = new AmCharts.AmMap()
+
+        map.projection = 'winkel3'
+        map.addTitle('Population of the World in 2011', 14, 1, 1, false)
+        map.addTitle('source: Gapminder', 11, 1, 1, 1, false)
+        map.areasSettings = {
+          unlistedAreasColor: '#eee',
+          unlistedAreasAlpha: 1,
+          outlineColor: '#fff',
+          outlineThickness: 2
+        }
+        map.imagesSettings = {
+          balloonText: '<span style="font-size:14px"><b>[[title]]</b>: [[value]]</span>',
+          alpha: 0.75
+        }
+
+        let dataProvider = {
+          mapVar: AmCharts.maps.worldLow,
+          images: []
+        }
+
+        // create circle for each country
+        // it's better to use circle square to show difference between values, not a radius
+        var maxSquare = maxBulletSize * maxBulletSize * 2 * Math.PI
+        var minSquare = minBulletSize * minBulletSize * 2 * Math.PI
+
+        // create circle for each country
+        this.chartData.data.forEach((dataItem) => {
+          var value = dataItem.value
+          // calculate size of a bubble
+          var square = (value - min) / (max - min) * (maxSquare - minSquare) + minSquare
+          if (square < minSquare) {
+            square = minSquare
+          }
+          var size = Math.sqrt(square / (Math.PI * 2))
+          var id = dataItem.code
+          dataProvider.images.push({
+            type: 'circle',
+            width: size,
+            height: size,
+            color: dataItem.color,
+            longitude: this.chartData.latlong[id].longitude,
+            latitude: this.chartData.latlong[id].latitude,
+            title: dataItem.name,
+            value: value
+          })
+        })
+
+        map.dataProvider = dataProvider
+        map.write(this.$el)
+      }
+    },
+
+    mounted () {
+      this.drawMap()
+    }
+  }
+</script>
+
+<style lang='scss'>
+  @import '../../../../sass/_variables.scss';
+  @import '../../../../../node_modules/ammap3/ammap/ammap.css';
+
+  .smoothed-line-chart {
+    height: 100%;
+  }
+</style>
