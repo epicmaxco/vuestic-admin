f91b26d0fa18606dbd6e1fd28c6fb8d6a4663bc1
smartapant <smartapant@gmail.com>
2017-05-03 12:39:01 +0300

    feat(forms): abc checkboxes

--- src/components/forms/form-elements/FormElements.vue
+++ src/components/forms/form-elements/FormElements.vue
@@ -1,12 +1,54 @@
 <template>
   <div class="form-elements">
-    Form Elements
+    <div class="row">
+      <div class="col-md-12">
+        <widget class="chart-widget"  header-text="Checkboxes, Radios, Swithces">
+          <form role="form">
+            <div class="row">
+              <div class="col-md-4">
+                <fieldset>
+                  <div class="abc-checkbox abc-checkbox-primary">
+                    <input id="checkbox1" type="checkbox">
+                    <label for="checkbox1">
+                      <span class="abc-label-text">Subscribe to newsletter</span>
+                    </label>
+                  </div>
+                  <div class="abc-checkbox abc-checkbox-primary">
+                    <input id="checkbox2" type="checkbox" checked>
+                    <label for="checkbox2">
+                      <span class="abc-label-text">Subscribe to newsletter</span>
+                    </label>
+                  </div>
+                  <div class="abc-checkbox abc-checkbox-primary">
+                    <input id="checkbox3" type="checkbox" disabled>
+                    <label for="checkbox3">
+                      <span class="abc-label-text">Subscribe to newsletter</span>
+                    </label>
+                  </div>
+                  <div class="abc-checkbox abc-checkbox-primary">
+                    <input id="checkbox4" type="checkbox" disabled checked>
+                    <label for="checkbox4">
+                      <span class="abc-label-text">Subscribe to newsletter</span>
+                    </label>
+                  </div>
+                </fieldset>
+              </div>
+            </div>
+          </form>
+        </widget>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Widget from 'components/common/widget/Widget'
+
   export default {
-    name: 'form-elements'
+    name: 'form-elements',
+    components: {
+      Widget
+    }
   }
 </script>
 
