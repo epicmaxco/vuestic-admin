6b58a7cd8751df3e809cef6bd2357463f7032ad9
smartapant <smartapant@gmail.com>
2017-07-10 15:38:33 +0300

    feat(form-wizard): page dummy

--- src/components/forms/form-wizard/FormWizard.vue
+++ src/components/forms/form-wizard/FormWizard.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="form-wizard">
-    Form Wizard
+    <div class="row">
+      <div class="col-md-12">
+        <widget class="chart-widget" header-text="Rich Wizard">
+
+        </widget>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Widget from 'components/common/widget/Widget'
+
   export default {
-    name: 'form-wizard'
+    name: 'form-wizard',
+    components: {
+      Widget
+    },
+    methods: {
+      onComplete () {
+
+      }
+    }
   }
 </script>
 
