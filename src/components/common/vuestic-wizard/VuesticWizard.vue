92e5611804e8f4bc42ee94e2f26780981ea0eaf2
smartapant <smartapant@gmail.com>
2017-07-11 15:25:25 +0300

    feat(form wizard): simple horizontal indicator component

--- src/components/common/vuestic-wizard/VuesticWizard.vue
+++ src/components/common/vuestic-wizard/VuesticWizard.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="wizard">
-    <ul class="wizard-steps">
-      <li class="wizard-step" :class="{'active': currentStep >= index, 'current': currentStep === index}" :style="{ width: 100/steps.length + '%' }" v-for="(step, index) of steps">
-        <span class="wizard-step-line"></span>
-        <span class="wizard-step-label">{{step.label}}</span>
-        <span class="wizard-step-indicator"></span>
-      </li>
-    </ul>
+    <simple-horizontal-indicator :steps="steps" :currentStep="currentStep"></simple-horizontal-indicator>
+    
     <div class="wizard-body">
       <div class="wizard-body-step"><slot :name="currentSlot" class="step-content"></slot></div>
       <div class="wizard-body-actions clearfix">
@@ -25,6 +20,8 @@
 </template>
 
 <script>
+  import SimpleHorizontalIndicator from './indicators/SimpleHorizontalIndicator.vue'
+
   export default {
     name: 'vuestic-wizard',
     props: {
@@ -37,6 +34,9 @@
       return {
         currentStep: 0
       }
+    },
+    components: {
+      SimpleHorizontalIndicator
     },
     computed: {
       currentSlot () {
@@ -82,10 +82,6 @@
 <style lang="scss" scoped>
   @import "../../../sass/_variables.scss";
 
-  $wizard-step-height: 3.75rem;
-  $wizard-step-indicator-height: 1rem;
-  $wizard-step-label-font-size: $font-size-h4;
-
   $wizard-body-step-h-padding: 15%;
   $wizard-body-step-v-padding: 2.25rem;
 
@@ -95,88 +91,8 @@
     position: relative;
     width:  100%;
   }
-  .wizard-steps{
-    list-style-type:  none;
-    text-align: justify;
-    -ms-text-justify: distribute-all-lines;
-    text-justify: distribute-all-lines;
-    padding:  0;
-    height:  $wizard-step-height;
-    position:  relative;
-  }
-  .wizard-step{
-    height: $wizard-step-height;
-    vertical-align: bottom;
-    display: inline-block;
-    text-align: center;
-    position:  relative;
 
-    .wizard-step-line{
-      position: absolute;
-      width:  100%;
-      left:  -50%;
-      bottom:  12px;
-      height:  2px;
-      background-color: $lighter-gray;
-      transition: background-color 300ms linear;
-    }
 
-    .wizard-step-indicator{
-      box-sizing: content-box;
-      display:  block;
-      width:  $wizard-step-indicator-height;
-      height:  $wizard-step-indicator-height;
-      background-color: $lighter-gray;
-      border-radius: 50%;
-      position:  absolute;
-      left:  50%;
-      margin-left:  -7px;
-      bottom:  5px;
-      z-index: 1;
-      transition: background-color 300ms linear;
-
-    }
-
-    .wizard-step-label{
-      color:  $lighter-gray;
-      font-size: $wizard-step-label-font-size;
-      font-weight: bold;
-      transition: color 300ms linear;
-    }
-
-    &:first-child {
-      .wizard-step-line {
-        width: 50%;
-        left: 0;
-      }
-    }
-
-    &:last-child {
-      .wizard-step-line {
-        width: 150%;
-
-      }
-    }
-
-    &.active {
-      .wizard-step-indicator{
-        background-color: $brand-primary;
-      }
-      .wizard-step-line{
-        background-color: $brand-primary;
-      }
-
-      .wizard-step-label {
-        color: $brand-primary;
-      }
-
-      &.current:not(:last-child) .wizard-step-label {
-        color: $vue-darkest-blue;
-      }
-    }
-  }
-  /* Wizard body
-  *******************************/
   .wizard-body{
     padding: $wizard-body-step-v-padding  $wizard-body-step-h-padding;
     position: relative;
@@ -192,8 +108,7 @@
       }
     }
   }
-  /* Wizard body
-  *******************************/
+
   .wizard-body-actions{
     display: flex;
     flex-direction: row;
