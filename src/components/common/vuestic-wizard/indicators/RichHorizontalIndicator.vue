dcb19a7aedbd26267ffda0e6c0e935f3f0ee2e92
smartapant <smartapant@gmail.com>
2017-07-11 16:36:13 +0300

    feat(form wizard): rich wizard

--- src/components/common/vuestic-wizard/indicators/RichHorizontalIndicator.vue
+++ src/components/common/vuestic-wizard/indicators/RichHorizontalIndicator.vue
@@ -1,9 +1,10 @@
 <template>
     <ul class="wizard-steps horizontal-steps rich-steps">
       <li class="wizard-step" :class="{'active': currentStep >= index, 'current': currentStep === index}" :style="{ width: 100/steps.length + '%' }" v-for="(step, index) of steps">
+        <i class="ion ion-android-close step-icon icon-cross"></i>
+        <i class="ion ion-android-done step-icon icon-check"></i>
+        <span class="wizard-step-label">{{step.label}}</span>
         <span class="wizard-step-line"></span>
-        <span class="wizard-step-label">{{step.label}} Rich</span>
-        <span class="wizard-step-indicator"></span>
       </li>
     </ul>
 </template>
@@ -27,86 +28,80 @@
 <style lang="scss" scoped>
   @import "../../../../sass/_variables.scss";
 
-  $wizard-step-height: 3.75rem;
-  $wizard-step-indicator-height: 1rem;
+  $wizard-step-height: 5.5rem;
   $wizard-step-label-font-size: $font-size-h4;
+  $wizard-step-padding-v: 0;
+  $wizard-step-padding-h: 0.315rem;
+  $wizard-step-icon-height: 2.25rem;
+  $wizard-step-icon-fs: 3.125rem;
+  $wizard-step-ion-icon-alignment: 0.5rem;
 
   .wizard-steps{
     list-style-type:  none;
-    text-align: justify;
-    -ms-text-justify: distribute-all-lines;
-    text-justify: distribute-all-lines;
-    padding:  0;
+    padding: $wizard-step-padding-v $wizard-step-padding-h;
     height:  $wizard-step-height;
     position:  relative;
   }
   .wizard-step{
     height: $wizard-step-height;
-    vertical-align: bottom;
-    display: inline-block;
-    text-align: center;
-    position:  relative;
+    padding: $wizard-step-padding-v $wizard-step-padding-h;
+    color:  $lighter-gray;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    transition: color 300ms linear;
 
     .wizard-step-line{
-      position: absolute;
       width:  100%;
-      left:  -50%;
-      bottom:  12px;
       height:  2px;
       background-color: $lighter-gray;
       transition: background-color 300ms linear;
     }
 
-    .wizard-step-indicator{
-      box-sizing: content-box;
-      display:  block;
-      width:  $wizard-step-indicator-height;
-      height:  $wizard-step-indicator-height;
-      background-color: $lighter-gray;
-      border-radius: 50%;
-      position:  absolute;
-      left:  50%;
-      margin-left:  -7px;
-      bottom:  5px;
-      z-index: 1;
-      transition: background-color 300ms linear;
-
-    }
-
     .wizard-step-label{
-      color:  $lighter-gray;
       font-size: $wizard-step-label-font-size;
       font-weight: bold;
-      transition: color 300ms linear;
     }
 
-    &:first-child {
-      .wizard-step-line {
-        width: 50%;
-        left: 0;
+    .step-icon {
+      font-size: $wizard-step-icon-fs;
+      height: $wizard-step-icon-height;
+      position: relative;
+      top: $wizard-step-ion-icon-alignment;
+
+      &.icon-cross {
+        display: block;
       }
-    }
 
-    &:last-child {
-      .wizard-step-line {
-        width: 150%;
+      &.icon-check {
+        display: none;
+      }
 
+      &::before {
+        position: relative;
+        top: -$wizard-step-ion-icon-alignment;
       }
     }
 
     &.active {
-      .wizard-step-indicator{
-        background-color: $brand-primary;
-      }
+      color: $brand-primary;
+
       .wizard-step-line{
         background-color: $brand-primary;
       }
 
-      .wizard-step-label {
-        color: $brand-primary;
+      &:not(.current), &.current:last-child {
+        .step-icon.icon-cross {
+          display: none;
+        }
+
+        .step-icon.icon-check {
+          display: block;
+        }
       }
 
-      &.current:not(:last-child) .wizard-step-label {
+      &.current:not(:last-child) {
         color: $vue-darkest-blue;
       }
     }
