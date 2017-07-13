92284a56ccf009f8df6bccbd53b4dab11be3601d
smartapant <smartapant@gmail.com>
2017-07-13 15:15:04 +0300

    fix(dropdown): make directive bound once only

--- src/directives/Dropdown.js
+++ src/directives/Dropdown.js
@@ -1,21 +1,24 @@
-export default function (el, binding) {
-  let ddToggle = el.querySelector('.dropdown-toggle')
-  let ddMenu = el.querySelector('.dropdown-menu')
-  let closeOnMenuClick = binding.modifiers.closeOnMenuClick
+export default {
+  bind: function (el, binding) {
+    let ddToggle = el.querySelector('.dropdown-toggle')
+    let ddMenu = el.querySelector('.dropdown-menu')
+    let closeOnMenuClick = binding.modifiers.closeOnMenuClick
+    console.log('INIT')
 
-  ddToggle.addEventListener('click', function (evt) {
-    evt.preventDefault()
-    let isShown = el.classList.contains('show')
-    setTimeout(() => el.classList.toggle('show', !isShown))
-  })
+    ddToggle.addEventListener('click', function (evt) {
+      evt.preventDefault()
+      let isShown = el.classList.contains('show')
+      setTimeout(() => el.classList.toggle('show', !isShown))
+    })
 
-  window.addEventListener('click', function () {
-    el.classList.remove('show')
-  })
+    window.addEventListener('click', function () {
+      el.classList.remove('show')
+    })
 
-  ddMenu.addEventListener('click', function (evt) {
-    if (!closeOnMenuClick) {
-      evt.stopPropagation()
-    }
-  })
+    ddMenu.addEventListener('click', function (evt) {
+      if (!closeOnMenuClick) {
+        evt.stopPropagation()
+      }
+    })
+  }
 }
