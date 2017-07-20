238cf1bc7a73642c62cfd3cfe016fac74c95917f
smartapant <smartapant@gmail.com>
2017-07-20 15:35:18 +0300

    feat(charts): bar charts

--- src/components/statistics/charts/Charts.vue
+++ src/components/statistics/charts/Charts.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="charts-page">
+
     <div class="row">
       <div class="col-md-12">
         <widget class="chart-widget"  header-text="Line Chart">
           <vuestic-line-chart v-bind:data="lineChartData"></vuestic-line-chart>
+        </widget>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <widget class="chart-widget"  header-text="Vertical Bar Chart">
+          <vuestic-vertical-bar-chart v-bind:data="verticalBarChartData"></vuestic-vertical-bar-chart>
+        </widget>
+      </div>
+      <div class="col-md-6">
+        <widget class="chart-widget"  header-text="Horizontal Bar Chart">
+          <vuestic-horizontal-bar-chart v-bind:data="horizontalBarChartData"></vuestic-horizontal-bar-chart>
         </widget>
       </div>
     </div>
@@ -40,6 +54,10 @@
   import PieChartData from './vuestic-pie-chart/PieChartData'
   import VuesticDonutChart from './vuestic-donut-chart/VuesticDonutChart.vue'
   import DonutChartData from './vuestic-donut-chart/DonutChartData'
+  import VuesticVerticalBarChart from './vuestic-vertical-bars/VuesticVerticalBarChart.vue'
+  import VerticalBarChartData from './vuestic-vertical-bars/VerticalBarChartData'
+  import VuesticHorizontalBarChart from './vuestic-horizontal-bars/VuesticHorizontalBarChart.vue'
+  import HorizontalBarChartData from './vuestic-horizontal-bars/HorizontalBarChartData'
   import Widget from 'components/common/widget/Widget'
 
   export default {
@@ -49,14 +67,18 @@
       VuesticBubbleChart,
       VuesticLineChart,
       VuesticPieChart,
-      VuesticDonutChart
+      VuesticDonutChart,
+      VuesticVerticalBarChart,
+      VuesticHorizontalBarChart
     },
     data: function () {
       return {
         bubbleChartData: BubbleChartData,
         lineChartData: LineChartData,
         pieChartData: PieChartData,
-        donutChartData: DonutChartData
+        donutChartData: DonutChartData,
+        verticalBarChartData: VerticalBarChartData,
+        horizontalBarChartData: HorizontalBarChartData
       }
     }
   }
---
+++ src/components/statistics/charts/vuestic-vertical-bars/VerticalBarChartData.js
@@ -0,0 +1,15 @@
+export default {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  datasets: [
+    {
+      label: 'USA',
+      backgroundColor: '#4ae387',
+      data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+    },
+    {
+      label: 'USSR',
+      backgroundColor: '#4ab2e3',
+      data: [50, 10, 22, 39, 15, 20, 85, 32, 60, 50, 20, 30]
+    }
+  ]
+}
