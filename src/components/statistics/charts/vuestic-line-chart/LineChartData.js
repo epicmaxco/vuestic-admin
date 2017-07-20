deba3903808c44735803d71f901cf4bb19f226dd
smartapant <smartapant@gmail.com>
2017-07-20 13:40:18 +0300

    feat(charts): transparency

--- src/components/statistics/charts/vuestic-line-chart/LineChartData.js
+++ src/components/statistics/charts/vuestic-line-chart/LineChartData.js
@@ -1,14 +1,16 @@
+import utils from 'services/utils'
+
 export default {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   datasets: [
     {
       label: 'Data One',
-      backgroundColor: '#4ae387',
+      backgroundColor: utils.hex2rgb('#4ae387', 0.6).css,
       data: [40, 39, 10, 40, 39, 80, 40]
     },
     {
       label: 'Data Two',
-      backgroundColor: '#4ab2e3',
+      backgroundColor: utils.hex2rgb('#4ab2e3', 0.6).css,
       data: [50, 20, 70, 30, 10, 5, 70]
     }
   ]
