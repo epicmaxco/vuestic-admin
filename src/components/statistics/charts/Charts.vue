628d1af35cd0a7ed2bacacdb6c11eef315b94a2c
smartapant <smartapant@gmail.com>
2017-07-20 11:47:07 +0300

    feat(charts): bubble chart

--- src/components/statistics/charts/Charts.vue
+++ src/components/statistics/charts/Charts.vue
@@ -2,44 +2,12 @@
   <div class="charts-page">
     <div class="row">
       <div class="col-md-12">
-        <widget class="chart-widget"  header-text="Combo Chart">
-          <combo-chart v-bind:chart-data="comboChartData"></combo-chart>
-        </widget>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-6">
-        <widget class="chart-widget"  header-text="Column Chart">
-          <column-chart v-bind:chart-data="columnChartData"></column-chart>
-        </widget>
-      </div>
-      <div class="col-md-6">
-        <widget class="chart-widget"  header-text="Time Based Area Chart">
-          <time-based-area-chart v-bind:chart-data="timeBasedAreaChartData"></time-based-area-chart>
+        <widget class="chart-widget"  header-text="Bubble Chart">
+          <vuestic-bubble-chart v-bind:data="bubbleChartData"></vuestic-bubble-chart>
         </widget>
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-md-12">
-        <widget class="chart-widget"  header-text="Smoothed Line Chart">
-          <smoothed-line-chart v-bind:chart-data="smoothedLineChartData"></smoothed-line-chart>
-        </widget>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-6">
-        <widget class="chart-widget"  header-text="Layered Column Chart">
-          <layered-column-chart v-bind:chart-data="layeredColumnChartData"></layered-column-chart>
-        </widget>
-      </div>
-      <div class="col-md-6">
-        <widget class="chart-widget"  header-text="Clustered Bar Chart">
-          <clustered-bar-chart v-bind:chart-data="clusteredBarChartData"></clustered-bar-chart>
-        </widget>
-      </div>
-    </div>
 
     <div class="row">
       <div class="col-md-6">
@@ -57,47 +25,27 @@
 </template>
 
 <script>
-  import SmoothedLineChart from './smoothed-line-chart/SmoothedLineChart.vue'
-  import SmoothedLineChartData from './smoothed-line-chart/SmoothedLineChartData'
-  import ColumnChart from './column-chart/ColumnChart.vue'
-  import ColumnChartData from './column-chart/ColumnChartData'
-  import TimeBasedAreaChart from './time-based-area-chart/TimeBasedAreaChart.vue'
-  import TimeBasedAreaChartData from './time-based-area-chart/TimeBasedAreaChartData'
-  import LayeredColumnChart from './layered-column-chart/LayeredColumnChart.vue'
-  import LayeredColumnChartData from './layered-column-chart/LayeredColumnChartData'
-  import ClusteredBarChart from './clustered-bar-chart/ClusteredBarChart.vue'
-  import ClusteredBarChartData from './clustered-bar-chart/ClusteredBarChartData'
+  import VuesticBubbleChart from './vuestic-bubble-chart/VuesticBubbleChart.vue'
+  import BubbleChartData from './vuestic-bubble-chart/BubbleChartData'
   import VuesticPieChart from './vuestic-pie-chart/VuesticPieChart.vue'
   import PieChartData from './vuestic-pie-chart/PieChartData'
   import VuesticDonutChart from './vuestic-donut-chart/VuesticDonutChart.vue'
   import DonutChartData from './vuestic-donut-chart/DonutChartData'
-  import ComboChart from './combo-chart/ComboChart.vue'
-  import ComboChartData from './combo-chart/ComboChartData'
   import Widget from 'components/common/widget/Widget'
 
   export default {
     name: 'charts',
     components: {
       Widget,
-      SmoothedLineChart,
-      ColumnChart,
-      TimeBasedAreaChart,
-      LayeredColumnChart,
-      ClusteredBarChart,
+      VuesticBubbleChart,
       VuesticPieChart,
-      VuesticDonutChart,
-      ComboChart
+      VuesticDonutChart
     },
     data: function () {
       return {
-        smoothedLineChartData: SmoothedLineChartData,
-        columnChartData: ColumnChartData,
-        timeBasedAreaChartData: TimeBasedAreaChartData,
-        layeredColumnChartData: LayeredColumnChartData,
-        clusteredBarChartData: ClusteredBarChartData,
+        bubbleChartData: BubbleChartData,
         pieChartData: PieChartData,
-        donutChartData: DonutChartData,
-        comboChartData: ComboChartData
+        donutChartData: DonutChartData
       }
     }
   }
