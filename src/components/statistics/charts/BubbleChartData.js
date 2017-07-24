1393da47fd7d1b4b6abde0fd224355da4c835a6b
smartapant <smartapant@gmail.com>
2017-07-24 11:49:03 +0300

    feat(charts): bubble chart data

--- src/components/statistics/charts/BubbleChartData.js
+++ src/components/statistics/charts/BubbleChartData.js
@@ -1,4 +1,5 @@
 import store from 'vuex-store'
+import utils from 'services/utils'
 
 let palette = store.getters.palette
 
@@ -6,7 +7,7 @@ export default {
   datasets: [
     {
       label: 'USA',
-      backgroundColor: palette.danger,
+      backgroundColor: utils.hex2rgb(palette.danger, 0.9).css,
       data: [
         {
           x: 23,
@@ -27,12 +28,32 @@ export default {
           x: 7,
           y: 43,
           r: 40
+        },
+        {
+          x: 23,
+          y: 27,
+          r: 120
+        },
+        {
+          x: 20,
+          y: 15,
+          r: 11
+        },
+        {
+          x: 7,
+          y: 10,
+          r: 35
+        },
+        {
+          x: 10,
+          y: 20,
+          r: 40
         }
       ]
     },
     {
       label: 'Russia',
-      backgroundColor: palette.primary,
+      backgroundColor: utils.hex2rgb(palette.primary, 0.9).css,
       data: [
         {
           x: 0,
@@ -53,12 +74,32 @@ export default {
           x: 31,
           y: 46,
           r: 30
+        },
+        {
+          x: 20,
+          y: 14,
+          r: 25
+        },
+        {
+          x: 34,
+          y: 17,
+          r: 30
+        },
+        {
+          x: 44,
+          y: 44,
+          r: 10
+        },
+        {
+          x: 39,
+          y: 25,
+          r: 35
         }
       ]
     },
     {
       label: 'Canada',
-      backgroundColor: palette.warning,
+      backgroundColor: utils.hex2rgb(palette.warning, 0.9).css,
       data: [
         {
           x: 10,
@@ -74,12 +115,32 @@ export default {
           x: 5,
           y: 5,
           r: 30
+        },
+        {
+          x: 40,
+          y: 30,
+          r: 20
+        },
+        {
+          x: 33,
+          y: 15,
+          r: 18
+        },
+        {
+          x: 40,
+          y: 20,
+          r: 40
+        },
+        {
+          x: 33,
+          y: 33,
+          r: 40
         }
       ]
     },
     {
       label: 'Belarus',
-      backgroundColor: palette.info,
+      backgroundColor: utils.hex2rgb(palette.info, 0.9).css,
       data: [
         {
           x: 35,
@@ -100,12 +161,32 @@ export default {
           x: 5,
           y: 20,
           r: 40
+        },
+        {
+          x: 10,
+          y: 40,
+          r: 15
+        },
+        {
+          x: 3,
+          y: 10,
+          r: 10
+        },
+        {
+          x: 15,
+          y: 40,
+          r: 40
+        },
+        {
+          x: 7,
+          y: 15,
+          r: 10
         }
       ]
     },
     {
       label: 'Ukraine',
-      backgroundColor: palette.success,
+      backgroundColor: utils.hex2rgb(palette.success, 0.9).css,
       data: [
         {
           x: 25,
@@ -126,6 +207,26 @@ export default {
           x: 3,
           y: 40,
           r: 10
+        },
+        {
+          x: 40,
+          y: 40,
+          r: 40
+        },
+        {
+          x: 20,
+          y: 10,
+          r: 10
+        },
+        {
+          x: 10,
+          y: 27,
+          r: 35
+        },
+        {
+          x: 7,
+          y: 26,
+          r: 40
         }
       ]
     }
