5114e31b386391807c27b61d5fa4049fec06660f
smartapant <smartapant@gmail.com>
2017-07-24 10:14:15 +0300

    feat(charts): vuestic-chart component

---
+++ src/components/common/vuestic-chart/VuesticChart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class='vuestic-chart'>
+    <pie-chart :options="options" :data="data" v-if="type === 'pie'"></pie-chart>
+    <donut-chart :options="options" :data="data" v-if="type === 'donut'"></donut-chart>
+    <bubble-chart :options="options" :data="data" v-if="type === 'bubble'"></bubble-chart>
+    <line-chart :options="options" :data="data" v-if="type === 'line'"></line-chart>
+    <horizontal-bar-chart :options="options" :data="data" v-if="type === 'horizontal-bar'"></horizontal-bar-chart>
+    <vertical-bar-chart :options="options" :data="data" v-if="type === 'vertical-bar'"></vertical-bar-chart>
+  </div>
+</template>
+
+<script>
+  import PieChart from './chart-types/PieChart'
+  import BubbleChart from './chart-types/BubbleChart'
+  import DonutChart from './chart-types/DonutChart'
+  import HorizontalBarChart from './chart-types/HorizontalBarChart'
+  import VerticalBarChart from './chart-types/VerticalBarChart'
+  import LineChart from './chart-types/LineChart'
+
+  export default {
+    name: 'vuestic-pie-chart',
+    props: ['data', 'options', 'type'],
+    components: {
+      PieChart,
+      LineChart,
+      VerticalBarChart,
+      HorizontalBarChart,
+      DonutChart,
+      BubbleChart
+    }
+  }
+</script>
+
+<style lang='scss'>
+  @import '../../../sass/_variables.scss';
+
+  .vuestic-chart {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    > * {
+      height: 100%;
+      width: 100%;
+    }
+
+    canvas {
+      width: 100%;
+      height: auto;
+    }
+  }
+</style>
--- src/components/statistics/charts/Charts.vue
+++ src/components/statistics/charts/Charts.vue
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-md-12">
         <widget class="chart-widget"  header-text="Line Chart">
-          <vuestic-line-chart v-bind:data="lineChartData"></vuestic-line-chart>
+          <vuestic-chart v-bind:data="lineChartData" type="line"></vuestic-chart>
         </widget>
       </div>
     </div>
@@ -12,12 +12,12 @@
     <div class="row">
       <div class="col-md-6">
         <widget class="chart-widget"  header-text="Vertical Bar Chart">
-          <vuestic-vertical-bar-chart v-bind:data="verticalBarChartData"></vuestic-vertical-bar-chart>
+          <vuestic-chart v-bind:data="verticalBarChartData" type="vertical-bar"></vuestic-chart>
         </widget>
       </div>
       <div class="col-md-6">
         <widget class="chart-widget"  header-text="Horizontal Bar Chart">
-          <vuestic-horizontal-bar-chart v-bind:data="horizontalBarChartData"></vuestic-horizontal-bar-chart>
+          <vuestic-chart v-bind:data="horizontalBarChartData" type="horizontal-bar"></vuestic-chart>
         </widget>
       </div>
     </div>
@@ -25,7 +25,7 @@
     <div class="row">
       <div class="col-md-12">
         <widget class="chart-widget"  header-text="Bubble Chart">
-          <vuestic-bubble-chart v-bind:data="bubbleChartData"></vuestic-bubble-chart>
+          <vuestic-chart v-bind:data="bubbleChartData" type="bubble"></vuestic-chart>
         </widget>
       </div>
     </div>
@@ -33,12 +33,12 @@
     <div class="row">
       <div class="col-md-6">
         <widget class="chart-widget"  header-text="Pie Chart">
-          <vuestic-pie-chart :data="pieChartData"></vuestic-pie-chart>
+          <vuestic-chart :data="pieChartData" type="pie"></vuestic-chart>
         </widget>
       </div>
       <div class="col-md-6">
         <widget class="chart-widget"  header-text="Donut Chart">
-          <vuestic-donut-chart v-bind:data="donutChartData"></vuestic-donut-chart>
+          <vuestic-chart v-bind:data="donutChartData" type="donut"></vuestic-chart>
         </widget>
       </div>
     </div>
@@ -46,30 +46,20 @@
 </template>
 
 <script>
-  import VuesticLineChart from './vuestic-line-chart/VuesticLineChart.vue'
-  import LineChartData from './vuestic-line-chart/LineChartData'
-  import VuesticBubbleChart from './vuestic-bubble-chart/VuesticBubbleChart.vue'
-  import BubbleChartData from './vuestic-bubble-chart/BubbleChartData'
-  import VuesticPieChart from './vuestic-pie-chart/VuesticPieChart.vue'
-  import PieChartData from './vuestic-pie-chart/PieChartData'
-  import VuesticDonutChart from './vuestic-donut-chart/VuesticDonutChart.vue'
-  import DonutChartData from './vuestic-donut-chart/DonutChartData'
-  import VuesticVerticalBarChart from './vuestic-vertical-bars/VuesticVerticalBarChart.vue'
-  import VerticalBarChartData from './vuestic-vertical-bars/VerticalBarChartData'
-  import VuesticHorizontalBarChart from './vuestic-horizontal-bars/VuesticHorizontalBarChart.vue'
-  import HorizontalBarChartData from './vuestic-horizontal-bars/HorizontalBarChartData'
+  import VuesticChart from 'components/common/vuestic-chart/VuesticChart.vue'
+  import LineChartData from './LineChartData'
+  import BubbleChartData from './BubbleChartData'
+  import PieChartData from './PieChartData'
+  import DonutChartData from './DonutChartData'
+  import VerticalBarChartData from './VerticalBarChartData'
+  import HorizontalBarChartData from './HorizontalBarChartData'
   import Widget from 'components/common/widget/Widget'
 
   export default {
     name: 'charts',
     components: {
       Widget,
-      VuesticBubbleChart,
-      VuesticLineChart,
-      VuesticPieChart,
-      VuesticDonutChart,
-      VuesticVerticalBarChart,
-      VuesticHorizontalBarChart
+      VuesticChart
     },
     data: function () {
       return {
