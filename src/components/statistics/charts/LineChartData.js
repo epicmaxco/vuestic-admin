e6d791e1f66e6f5914ef5a6b57ab1fcd525d96a6
smartapant <smartapant@gmail.com>
2017-07-24 14:38:26 +0300

    fix(charts): labels shadows #2

--- src/components/statistics/charts/LineChartData.js
+++ src/components/statistics/charts/LineChartData.js
@@ -9,13 +9,13 @@ export default {
     {
       label: 'Debit',
       backgroundColor: utils.hex2rgb(palette.primary, 0.6).css,
-      borderColor: '#92eeb7',
+      borderColor: palette.transparent,
       data: [40, 39, 10, 40, 39, 80, 40]
     },
     {
       label: 'Credit',
       backgroundColor: utils.hex2rgb(palette.info, 0.6).css,
-      borderColor: '#92d1ee',
+      borderColor: palette.transparent,
       data: [50, 20, 70, 30, 10, 5, 70]
     }
   ]
