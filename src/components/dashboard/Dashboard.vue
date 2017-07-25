12bb737ca915edea817ffb70fe40aec29a0e729a
smartapant <smartapant@gmail.com>
2017-07-25 14:58:32 +0300

    feat(alerts): alerts style

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="dashboard">
     <div class="row">
-      <widget class="col-md-4">
-        Dashboard!
-      </widget>
+      <div class="col-md-12">
+        <div class="alert alert-success with-close" role="alert">
+          <span class="badge badge-pill badge-success">SUCCESS</span>
+          You successfully read this important alert message.
+          <i class="fa fa-close alert-close"></i>
+        </div>
+        <div class="alert alert-info with-close" role="alert">
+          <span class="badge badge-pill badge-info">INFO</span>
+          This alert needs your attention, but it's not super important.
+          <i class="fa fa-close alert-close"></i>
+        </div>
+        <div class="alert alert-warning with-close" role="alert">
+          <span class="badge badge-pill badge-warning">WARNING</span>
+          Better check yourself, you're not looking too good.
+          <i class="fa fa-close alert-close"></i>
+        </div>
+        <div class="alert alert-danger with-close" role="alert">
+          <span class="badge badge-pill badge-danger">DANGER</span>
+          Change a few things up and try submitting again.
+          <i class="fa fa-close alert-close"></i>
+        </div>
+      </div>
     </div>
   </div>
 </template>
