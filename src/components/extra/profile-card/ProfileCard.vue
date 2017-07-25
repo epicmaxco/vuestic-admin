e829fcd622328d64ad6e61b1041c5ab412ff3a21
papasikis <ydamovich@gmail.com>
2017-07-25 23:55:17 +0300

    feat(profile-card): added profile card

--- src/components/extra/profile-card/ProfileCard.vue
+++ src/components/extra/profile-card/ProfileCard.vue
@@ -3,59 +3,9 @@
     <div class="row">
       <div class="col-md-12">
         <widget header-text="Profile Card">
-          <vuestic-wizard
-            :steps="steps"
-            wizard-layout="vertical"
-            wizard-type="rich">
-            <div slot="page1" class="form-wizard-tab-content">
-              <p>Zebras communicate with facial expressions and sounds. They make loud braying or barking sounds and
-                soft snorting sounds. The position of their ears, how wide open their eyes are, and whether they show
-                their teeth all send a signal. For example, ears flat back means trouble, or "you better follow orders!"</p>
-              <div class="form-group with-icon-right" :class="{'has-error': errors.has('name'), 'valid': isFormFieldValid('name')}">
-                <div class="input-group">
-                  <input
-                    type="text"
-                    name="name"
-                    v-model="name"
-                    v-validate="'required'"
-                    required="required"/>
-                  <i class="fa fa-exclamation-triangle error-icon icon-right input-icon"></i>
-                  <i class="fa fa-check valid-icon icon-right input-icon"></i>
-                  <label class="control-label">Name</label><i class="bar"></i>
-                  <small v-show="errors.has('name')" class="help text-danger">{{ errors.first('name') }}</small>
-                </div>
-              </div>
-            </div>
-            <div slot="page2" class="form-wizard-tab-content">
-              <p>Zebras communicate with facial expressions and sounds. They make loud braying or barking sounds and
-                soft snorting sounds. The position of their ears, how wide open their eyes are, and whether they show
-                their teeth all send a signal. For example, ears flat back means trouble, or "you better follow orders!"</p>
-              <vuestic-simple-select
-                label="Select country"
-                v-model="country"
-                name="country"
-                :required="true"
-                ref="countrySelect"
-                v-bind:options="countriesList">
-              </vuestic-simple-select>
-            </div>
-            <div slot="page3" class="form-wizard-tab-content">
-              <h4>Confirm selection</h4>
-              <p>
-                Zebras communicate with facial expressions and sounds. They make loud braying or barking sounds and
-                soft snorting sounds. The position of their ears, how wide open their eyes are, and whether they show
-                their teeth all send a signal. For example, ears flat back means trouble, or "you better follow orders!"
-              </p>
-            </div>
-            <div slot="wizardCompleted" class="form-wizard-tab-content">
-              <h4>Your Profile Was Completed!</h4>
-              <p>
-                Zebras communicate with facial expressions and sounds. They make loud braying or barking sounds and
-                soft snorting sounds. The position of their ears, how wide open their eyes are, and whether they show
-                their teeth all send a signal. For example, ears flat back means trouble, or "you better follow orders!"
-              </p>
-            </div>
-          </vuestic-wizard>
+          <div class="container d-flex justify-content-center">
+            <vuestic-profile-card :name="'Veronique Lee'" :location="'Malaga, Spain'"></vuestic-profile-card>
+          </div>
         </widget>
       </div>
     </div>
@@ -64,63 +14,13 @@
 
 <script>
   import Widget from 'components/common/widget/Widget'
-  import VuesticWizard from 'components/common/vuestic-wizard/VuesticWizard'
-  import CountriesList from 'components/forms/CountriesList'
-  import VuesticSimpleSelect from 'components/common/vuestic-simple-select/VuesticSimpleSelect'
+  import VuesticProfileCard from '../../common/vuestic-profile-card/VuesticProfileCard.vue'
 
   export default {
-    name: 'alerts',
+    name: 'profile-card',
     components: {
-      Widget,
-      VuesticWizard,
-      VuesticSimpleSelect
-    },
-    methods: {
-      isFormFieldValid (field) {
-        let isValid = false
-        if (this.fields[field]) {
-          isValid = this.fields[field].validated && this.fields[field].valid
-        }
-        return isValid
-      },
-      validateFormField (fieldName) {
-        this.$validator.validate(fieldName, this[fieldName])
-      }
-    },
-    data () {
-      return {
-        steps: [
-          {
-            label: 'Step 1. Name',
-            slot: 'page1',
-            onNext: () => {
-              this.validateFormField('name')
-            },
-            isValid: () => {
-              return this.isFormFieldValid('name')
-            }
-          },
-          {
-            label: 'Step 2. Country',
-            slot: 'page2',
-            onNext: () => {
-              this.$refs.countrySelect.validate()
-            },
-            isValid: () => {
-              return this.$refs.countrySelect.isValid()
-            }
-          },
-          {
-            label: 'Step 3. Confirm',
-            slot: 'page3'
-          }
-        ],
-        name: '',
-        email: '',
-        countriesList: CountriesList,
-        chosenCountry: '',
-        country: ''
-      }
+      VuesticProfileCard,
+      Widget
     }
   }
 </script>
