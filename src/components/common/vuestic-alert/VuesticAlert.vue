0c42c12fa785236f30687728b4e3261b63f00477
smartapant <smartapant@gmail.com>
2017-07-25 16:21:50 +0300

    feat(alert): VuesticAlert component

---
+++ src/components/common/vuestic-alert/VuesticAlert.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class='vuestic-alert alert' :class="alertClass">
+    <slot></slot>
+    <i class="fa fa-close alert-close" v-if="withCloseBtn"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vuestic-alert',
+    computed: {
+      alertClass () {
+        return {
+          'with-close': this.withCloseBtn,
+          'alert-success': this.type === 'success',
+          'alert-danger': this.type === 'danger',
+          'alert-warning': this.type === 'warning',
+          'alert-info': this.type === 'info'
+        }
+      }
+    },
+    props: {
+      type: {
+        type: String,
+        default: 'success'
+      },
+      withCloseBtn: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style lang='scss'>
+  @import '../../../sass/_variables.scss';
+</style>
