6889108885929d3ab1f929d419cc329c040e418d
Alexander Rudnik <alexanderrud0603@gmail.com>
2017-08-01 12:28:46 +0300

    fix(form-elements): set cursor not-allowed to disabled checkboxes and radio buttons

--- src/components/forms/form-elements/FormElements.vue
+++ src/components/forms/form-elements/FormElements.vue
@@ -316,4 +316,9 @@
     display: flex !important;
     justify-content: flex-start;
   }
+
+  input[type=checkbox]:disabled + label, input[type=radio]:disabled + label,
+  input[type=checkbox]:disabled, input[type=radio]:disabled {
+    cursor: not-allowed;
+  }
 </style>
