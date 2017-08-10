d1b310455c0616969c27bcf29792bd09e5e80859
Jerico Pulvera <Jerico.pulvera01z@gmail.com>
2017-08-10 19:07:40 +0800

    Update Navbar.vue

--- src/components/layout/navbar/Navbar.vue
+++ src/components/layout/navbar/Navbar.vue
@@ -20,7 +20,7 @@
 
       <div class="offset-lg-8"></div>
       <div class="col nav-item dropdown navbar-dropdown d-flex align-items-center justify-content-center" v-dropdown>
-        <a class="nav-link dropdown-toggle d-flex align-items-center justify-content" href="#" @click.prevent="toggleDropdown">
+        <a class="nav-link dropdown-toggle d-flex align-items-center justify-content" href="#" @click.prevent="closeMenu">
           <span class="i-nav-messages notify"></span>
         </a>
         <div class="dropdown-menu">
@@ -36,7 +36,7 @@
         </div>
       </div>
       <div class="col nav-item dropdown navbar-dropdown d-flex align-items-center justify-content-center" v-dropdown>
-        <a class="nav-link dropdown-toggle d-flex align-items-center justify-content" href="#" @click.prevent="toggleDropdown">
+        <a class="nav-link dropdown-toggle d-flex align-items-center justify-content" href="#" @click.prevent="closeMenu">
           <span class="i-nav-notification notify"></span>
         </a>
         <div class="dropdown-menu">
@@ -55,7 +55,7 @@
         </div>
       </div>
       <div class="col nav-item dropdown navbar-dropdown d-flex align-items-center justify-content-center" v-dropdown>
-        <a class="nav-link dropdown-toggle d-flex align-items-center justify-content" href="#" @click.prevent="toggleDropdown">
+        <a class="nav-link dropdown-toggle d-flex align-items-center justify-content" href="#" @click.prevent="closeMenu">
           <span class="avatar-container">
             <img src="http://i.imgur.com/UTc7Fne.png" />
           </span>
@@ -90,8 +90,8 @@
     ]),
     methods: {
       ...mapActions([
+        'closeMenu',
         'toggleSidebar',
-        'toggleDropdown',
         'isToggleWithoutAnimation'
       ])
     }
