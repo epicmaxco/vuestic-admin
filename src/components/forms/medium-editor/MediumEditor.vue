3a7f23ca7d417a5f165882f5af5054d11c673f79
smartapant <smartapant@gmail.com>
2018-01-18 14:54:50 +0300

    feat(medium editor): page stub

---
+++ src/components/forms/medium-editor/MediumEditor.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="medium-editor">
+    <div class="row">
+      <div class="col-md-12">
+        <vuestic-widget :headerText="'forms.mediumEditor.title' | translate">
+
+        </vuestic-widget>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'medium-editor',
+
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    mounted () {
+
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
