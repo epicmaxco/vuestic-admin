5f5871c6554ebd776960a1103e0ab4537297628c
Yev Dyko <yevdyko@gmail.com>
2018-03-27 01:12:31 +0200

    fix(components): refactor set component

--- src/components/ui/icons/Set.vue
+++ src/components/ui/icons/Set.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="Set row">
+  <div class="set row">
     <div class="header col-12">
       <div class="row">
         <div class="header-text col-lg-4">
-          <h2>{{set.name}}</h2>
-          <router-link :to="{path: '/ui/icons'}">{{'icons.back' | translate}}</router-link>
+          <h2>{{ set.name }}</h2>
+          <router-link :to="{ path: '/ui/icons' }">
+            {{ 'icons.back' | translate }}
+          </router-link>
         </div>
 
         <div class="search col-lg-4">
           <div class="form-group with-icon-left">
             <div class="input-group">
-              <input id="input-icon-left" name="input-icon-left" required v-model="selector"/>
+              <input
+                v-model="selector"
+                id="input-icon-left"
+                name="input-icon-left"
+                required
+              />
               <i class="fa fa-search icon-left input-icon"></i>
-              <label class="control-label" for="input-icon-left">{{'icons.search' | translate}}</label><i class="bar"></i>
+              <label class="control-label" for="input-icon-left">
+                {{ 'icons.search' | translate }}
+              </label>
+              <i class="bar"></i>
             </div>
           </div>
         </div>
+
         <div class="range col-lg-4">
           <h4>A</h4>
           <vuestic-slider
@@ -28,15 +39,30 @@
       </div>
     </div>
     <template v-for="list in validatedLists">
-      <vuestic-widget :headerText="list.name" class="col-12">
-        <span v-if="list.icons.length === 0">{{'icons.none' | translate}}</span>
-        <template v-for="i in Math.floor(list.icons.length/8+1)">
+      <vuestic-widget
+        :headerText="list.name"
+        class="col-12"
+      >
+        <span v-if="list.icons.length === 0">
+          {{ 'icons.none' | translate }}
+        </span>
+        <template v-for="i in Math.floor(list.icons.length / 8 + 1)">
           <div class="row">
-            <div class="col-8-custom icon-grid-container" v-for="j in 8" v-if="list.icons[(i-1)*8 + j-1]">
+            <div
+              v-for="j in 8"
+              v-if="list.icons[(i - 1) * 8 + j - 1]"
+              class="col-8-custom icon-grid-container"
+            >
               <div class="icon">
-                <span :class="iconClass(list.icons[(i-1)*8 + j-1])" aria-hidden="true"
-                      :style="'font-size: '+iconSize+'px'"></span>
-                <div class="iconText">{{list.icons[(i-1)*8 + j-1]}}</div>
+                <span
+                  :class="iconClass(list.icons[(i - 1) * 8 + j - 1])"
+                  :style="`font-size: ${iconSize}px`"
+                  aria-hidden="true"
+                >
+                </span>
+                <div class="iconText">
+                  {{ list.icons[(i - 1) * 8 + j - 1] }}
+                </div>
               </div>
             </div>
           </div>
@@ -49,33 +75,17 @@
 <script>
   export default {
     name: 'set',
-    props: ['name', 'sets'],
-    methods: {
-      iconClass (icon) {
-        return this.set.prefix + ' ' + this.set.prefix + '-' + icon
-      }
-    },
-    computed: {
-      set () {
-        for (let set of this.sets) {
-          if (set.href === this.name) return set
-        }
+
+    props: {
+      name: {
+        type: String
       },
-      validatedLists () {
-        if (this.selector === '') {
-          return this.set.lists
-        }
-        let result = [{name: 'Found Icons', icons: []}]
-        this.set.lists.forEach(list => {
-          list.icons.forEach(icon => {
-            if (icon.match(this.selector)) {
-              result[0].icons.push(icon)
-            }
-          })
-        })
-        return result
+
+      sets: {
+        type: Array
       }
     },
+
     data: function () {
       return {
         selector: '',
@@ -86,24 +96,62 @@
           max: 40
         }
       }
+    },
+
+    computed: {
+      set () {
+        for (let set of this.sets) {
+          if (set.href === this.name) return set
+        }
+      },
+
+      validatedLists () {
+        if (this.selector === '') {
+          return this.set.lists
+        }
+
+        let result = [
+          {
+            name: 'Found Icons',
+            icons: []
+          }
+        ]
+
+        this.set.lists.forEach(list => {
+          list.icons.forEach(icon => {
+            if (icon.match(this.selector)) {
+              result[0].icons.push(icon)
+            }
+          })
+        })
+
+        return result
+      }
+    },
+
+    methods: {
+      iconClass (icon) {
+        return `${this.set.prefix} ${this.set.prefix}-${icon}`
+      }
     }
   }
 </script>
 
 <style lang="scss">
   @import "../../../sass/variables";
-  @import '~bootstrap/scss/mixins/breakpoints';
+  @import "~bootstrap/scss/mixins/breakpoints";
   @import "~bootstrap/scss/functions";
-  @import '~bootstrap/scss/variables';
+  @import "~bootstrap/scss/variables";
 
-  .Set{
+  .set {
     .header {
+      padding: 1.75rem 0 1.125rem;
       background-color: white;
-      padding: 1.75rem 0 1.125rem 0;
 
       .header-text {
-        text-align: left;
         padding-left: 2.5rem;
+        text-align: left;
+
         h2 {
           margin-bottom: 0;
         }
@@ -113,6 +161,7 @@
         display: flex;
         align-items: center;
         justify-content: center;
+
         .input-group {
           width: 13.25rem;
         }
@@ -138,23 +187,25 @@
 
     .row {
       .icon-grid-container {
+        position: relative;
+        float: left;
+        height: 6rem;
+        min-height: 1px;
+        margin: 0 0 .5rem;
         padding-right: .625rem;
         padding-left: .625rem;
-        margin: 0 0 .5rem;
         text-align: center;
-        height: 6rem;
-        position: relative;
-        min-height: 1px;
-        float: left;
 
         &:hover {
-          background-color: $brand-primary;
           color: white;
+          background-color: $brand-primary;
           cursor: pointer;
         }
+
         .icon {
-          padding: 1rem 0 0;
           margin: 0 0 .5rem;
+          padding: 1rem 0 0;
+
           .iconText {
             font-size: .6rem;
             text-align: center;
