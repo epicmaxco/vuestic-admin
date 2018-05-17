1f6d9d8a476fc1b9a1664577bd69f7343c5f35f6
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-05-17 19:21:21 +0300

    close task 173

--- src/components/maps/yandex-maps/YandexMapsPage.vue
+++ src/components/maps/yandex-maps/YandexMapsPage.vue
@@ -4,35 +4,15 @@
       <div class="col-md-12">
         <vuestic-widget class="widget-viewport-height" headerText="Yandex Maps">
           <yandex-map
-            :coords="[54.62896654088406, 39.731893822753904]"
-            zoom="10"
-            style="width: 600px; height: 600px;"
-            :cluster-options="{
-    1: {clusterDisableClickZoom: true}
-  }"
-            :behaviors="['ruler']"
-            :controls="['trafficControl']"
+            :use-object-manager:="true"
+            :coords="[55.8, 37.8]"
+            :zoom="8"
+            style="width: 1450px; height: 765px;"
+            :behaviors="['default']"
+            :controls="['trafficControl','zoomControl', 'geolocationControl','fullscreenControl', 'searchControl']"
             :placemarks="placemarks"
-            map-type="hybrid">
+            map-type="hybrid"  >
           </yandex-map>
-          <ymap-marker
-            marker-type="placemark"
-            :coords="[54.7, 39.7]"
-            hint-content="Hint content 1"
-            :balloon="{header: 'header', body: 'body', footer: 'footer'}"
-            :icon="{color: 'green', glyph: 'cinema'}"
-            cluster-name="1"
-          ></ymap-marker>
-          <ymap-marker
-            marker-type="circle"
-            :coords="[54.62896654088406, 39.731893822753904]"
-            circle-radius="1600"
-            hint-content="Hint content 1"
-            :marker-fill="{color: '#000000', opacity: 0.4}"
-            :marker-stroke="{color: '#ff0000', width: 5}"
-            :balloon="{header: 'header', body: 'body', footer: 'footer'}"
-          ></ymap-marker>
-
         </vuestic-widget>
       </div>
     </div>
@@ -40,35 +20,37 @@
 </template>
 
 <script>
-  // or for a single instance
 
-  import {yandexMap, ymapMarker} from 'vue-yandex-maps'
+  import { yandexMap, ymapMarker } from 'vue-yandex-maps'
 
   export default {
     name: 'yandex-maps-page',
-
     components: {
       yandexMap,
       ymapMarker
     },
-
     data () {
       return {
         placemarks: [
           {
             coords: [54.8, 39.8],
-            properties: {}, // define properties here
-            options: {}, // define options here
+            properties: {},
+            options: {},
             clusterName: '1',
+            balloonTemplate: '<div>"Your custom template"</div>',
+            callbacks: { click: function () {} }
           }
         ]
       }
     }
   }
 </script>
-
 <style lang="scss">
-.yandex-maps-page {
-  height: 130%;
-}
+  @import "../../../sass/_variables.scss";
+
+  .yandex-maps-page {
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+  }
 </style>
