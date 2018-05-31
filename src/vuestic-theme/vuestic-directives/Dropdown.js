6efec810131292b965a1bc234f2b0eda9ed8ae94
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-05-31 18:14:18 +0300

    remove listeners

--- src/vuestic-theme/vuestic-directives/Dropdown.js
+++ src/vuestic-theme/vuestic-directives/Dropdown.js
@@ -5,7 +5,7 @@ export default {
     let ddItem = el.querySelector('.dropdown-menu-content')
     let closeOnMenuClick = binding.modifiers.closeOnMenuClick
     let isBlocked = binding.modifiers.isBlocked
-    ddToggle.addEventListener('click', function (evt) {
+    window.handleClickOnToggle = evt => {
       evt.preventDefault()
       let isShown = el.classList.contains('show')
       setTimeout(() => {
@@ -19,19 +19,23 @@ export default {
           ddMenu.classList.toggle('show', !isShown)
         }
       })
-    })
+    }
 
-    window.addEventListener('click', function () {
+    ddToggle.addEventListener('click', window.handleClickOnToggle)
+
+    window.handleClickOnWindow = () => {
       if (!isBlocked) {
         el.classList.remove('show')
         ddMenu.classList.remove('show')
       }
-    })
+    }
+    window.addEventListener('click', window.handleClickOnWindow)
 
-    ddItem.addEventListener('click', function () {
+    window.handleClickOnItem = () => {
       el.classList.remove('show')
       ddMenu.classList.remove('show')
-    })
+    }
+    ddItem.addEventListener('click', window.handleClickOnItem)
 
     ddMenu.addEventListener('click', function (evt) {
       if (!closeOnMenuClick) {
@@ -40,9 +44,8 @@ export default {
     })
   },
   unbind: function (el, binding) {
-    window.removeEventListener('click')
-    el.querySelector('.dropdown-toggle').removeEventListener('click')
-    el.querySelector('.dropdown-menu').removeEventListener('click')
-    el.querySelector('.dropdown-menu-content').removeEventListener('click')
+    window.removeEventListener('click', window.handleClickOnWindow)
+    window.removeEventListener('click', window.handleClickOnToggle)
+    window.removeEventListener('click', window.handleClickOnItem)
   }
 }
