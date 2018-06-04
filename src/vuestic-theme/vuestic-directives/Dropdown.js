782f5cc665faedaa26fb03a7f10fe26a4c15cb01
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-06-04 18:06:29 +0300

    working variant. non-working unbind and some other dropdowns

--- src/vuestic-theme/vuestic-directives/Dropdown.js
+++ src/vuestic-theme/vuestic-directives/Dropdown.js
@@ -1,46 +1,37 @@
 export default {
   bind: function (el, binding) {
     // let dd = el.querySelector('.dropdown')
-    let ddToggle = el.querySelector('.dropdown-toggle input')
+    let ddToggle = el.querySelector('.dropdown-toggle')
+    let ddToggleInput = el.querySelector('.dropdown-toggle input')
     let ddMenu = el.querySelector('.dropdown-menu')
     let ddItem = el.querySelector('.dropdown-menu-content')
     let closeOnMenuClick = binding.modifiers.closeOnMenuClick
     let isBlocked = binding.modifiers.isBlocked
-    ddToggle.addEventListener('click', function (evt) {
-      console.log(evt.target)
-      evt.preventDefault()
-      let isShown = el.classList.contains('show')
-      setTimeout(() => {
-        if (isBlocked) {
+
+    if (isBlocked) {
+      ddToggleInput.addEventListener('click', (evt) => {
+        let isShown = el.classList.contains('show')
+        setTimeout(() => {
           if (!isShown) {
             el.classList.toggle('show', !isShown)
             ddMenu.classList.toggle('show', !isShown)
           }
-        } else {
-          el.classList.toggle('show', !isShown)
-          ddMenu.classList.toggle('show', !isShown)
-        }
+        })
       })
-      evt.stopPropagation()
-    })
+    } else {
+      ddToggle.addEventListener('click', function (evt) {
+        let isShown = el.classList.contains('show')
+        el.classList.toggle('show', !isShown)
+        ddMenu.classList.toggle('show', !isShown)
+      })
+    }
 
-    // ddMenu.onblur = function (evt) {
-    //   console.log(evt.onfocus())
-    //   el.classList.remove('show')
-    //   ddMenu.classList.remove('show')
-    // }
-    //
-    ddToggle.addEventListener('focusout', () => {
-      console.log('af')
-      el.classList.remove('show')
-      ddMenu.classList.remove('show')
+    window.addEventListener('click', function (evt) {
+      if (evt.target !== ddToggleInput) {
+        el.classList.remove('show')
+        ddMenu.classList.remove('show')
+      }
     })
-
-    // window.addEventListener('click', function (evt) {
-    //   el.classList.remove('show')
-    //   ddMenu.classList.remove('show')
-    //   evt.stopPropagation()
-    // })
 
     ddItem.addEventListener('click', function (evt) {
       el.classList.remove('show')
@@ -52,18 +43,6 @@ export default {
       if (!closeOnMenuClick) {
         evt.stopPropagation()
       }
-    })
-
-    // ddToggle.addEventListener('focusin', () => {
-    //   console.log('FOCUSOUTTTTTT')
-    //   ddMenu.classList.remove('show')
-    //   el.classList.remove('show')
-    // })
-
-    ddToggle.addEventListener('focusout', () => {
-      // console.log('FOCUSOUTTTTTT')
-      // ddMenu.classList.remove('show')
-      // el.classList.remove('show')
     })
   },
   unbind: function (el, binding) {
