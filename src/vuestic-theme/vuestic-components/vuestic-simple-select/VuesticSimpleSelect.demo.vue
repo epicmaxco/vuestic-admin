75eb986fdcaa9626fe59e580189b4c612b956e9a
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-06-13 19:53:59 +0300

    Vuestic select enhancements (#233)
    * fix vertical scrollbar
    
    * set minimal height to thumb
    
    * change style to vue
    
    * test
    
    * auto scroll when typing first letters(Test version).
    
    * prepare
    
    * demo
    
    * fix disappear items
    
    * unbind
    
    * initial commit
    
    * remove listeners
    
    * add logic to simple select
    
    * refactor logic
    
    * fix bug with dissapeared dropdown
    
    * problem with select item
    
    * working variant. non-working unbind and some other dropdowns
    
    * working variant
    
    * working select + demo
    
    * refactor logic[1]
    
    * computed sort
    
    * last refactor
    
    * refactor demo.
    
    * fix reactive problem
    
    * fix dissapearing value
    
    * non-reactive
    
    * Minor
    
    * Minor
    
    * fix setup profile tab
    
    * fix setup profile tab [1]
    
    * final refactor
    
    * delete useless dependency
    
    * unused variable
    
    * style refactor
    
    * remove useless getter

---
+++ src/vuestic-theme/vuestic-components/vuestic-simple-select/VuesticSimpleSelect.demo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row justify-content-around">
+    <vuestic-simple-select
+      style="width: 240px; padding-top: 400px"
+      label="Select country"
+      v-model="selectedCountry"
+      :options="CountriesList"
+    />
+    <vuestic-simple-select
+      style="width: 240px"
+      label="Select country duplicate"
+      v-model="selectedCountry"
+      :options="CountriesList"
+    />
+  </div>
+</template>
+
+<script>
+
+import CountriesList from 'data/CountriesList'
+import VuesticSimpleSelect from './VuesticSimpleSelect'
+
+export default {
+  components: {
+    VuesticSimpleSelect,
+  },
+  data () {
+    return {
+      selectedCountry: '',
+      CountriesList,
+    }
+  },
+}
+</script>
