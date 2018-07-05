31fdb75611ebfd1e904afbf24f5959fefd313911
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-07-05 17:45:16 +0300

    vuestic-color-picker-input demo

---
+++ src/vuestic-theme/vuestic-components/vuestic-color-picker/VuesticColorPickerInput.demo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="demo-container">
+    <div class="demo-container__item">
+      <vuestic-color-picker-input v-model="value" mode="advanced"/>
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script>
+import VuesticColorPickerInput from './VuesticColorPickerInput'
+
+export default {
+  components: {
+    VuesticColorPickerInput,
+  },
+  data () {
+    return {
+      value: '#AAA'
+    }
+  }
+}
+</script>
