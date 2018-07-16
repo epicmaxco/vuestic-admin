4604667ea0862de394889013d9a2eb08a922e323
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-07-16 18:25:36 +0300

    minor

---
+++ src/vuestic-theme/vuestic-components/vuestic-multi-select/VuesticMultiSelect.demo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="demo-container">
+    <div class="demo-container__item">
+      <vuestic-multi-select
+        label="Select country"
+        v-model="selectedCountries"
+        :options="CountriesList"
+      />
+    </div>
+    <div class="demo-container__item">
+      <vuestic-multi-select
+        label="Select country duplicate"
+        v-model="selectedCountries"
+        :options="CountriesList"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import CountriesList from 'data/CountriesList'
+import VuesticMultiSelect from './VuesticMultiSelect'
+
+export default {
+  components: {
+    VuesticMultiSelect,
+  },
+  data () {
+    return {
+      selectedCountries: [],
+      CountriesList,
+    }
+  },
+}
+</script>
