a2a8f14932f6de96aa0bf2964a0c4d9f533725bc
Eugene Mischuk <eugene.mischuk@gmail.com>
2018-07-18 23:53:26 +0300

    feat: add tags

---
+++ src/components/ui/tags/Tags.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tags-page">
+    <div class="row">
+
+      <div class="col-md-12">
+        <vuestic-widget :headerText="$t('tags.tags')">
+          <div class="row btn-margin-row">
+            <div class="col-sm-4 col-lg-2 d-flex">
+              <vuestic-tag :name="$t('tags.type.default')"/>
+            </div>
+            <div class="col-sm-4 col-lg-2 d-flex">
+              <vuestic-tag :name="$t('tags.type.danger')" type="danger"/>
+            </div>
+            <div class="col-sm-4 col-lg-2 d-flex">
+              <vuestic-tag :name="$t('tags.type.info')" type="info"/>
+            </div>
+            <div class="col-sm-4 col-lg-2 d-flex">
+              <vuestic-tag :name="$t('tags.type.dark')" type="dark"/>
+            </div>
+            <div class="col-sm-4 col-lg-2 d-flex">
+              <vuestic-tag :name="$t('tags.type.warning')" type="warning"/>
+            </div>
+            <div class="col-sm-4 col-lg-2 d-flex">
+              <vuestic-tag :name="$t('tags.type.pale')" type="pale"/>
+            </div>
+          </div>
+        </vuestic-widget>
+      </div>
+
+      <div class="col-md-12">
+        <vuestic-widget :headerText="$t('tags.tagsGroup')">
+          <vuestic-tag-group v-model="tags"/>
+        </vuestic-widget>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tags',
+    data () {
+      return {
+        tags: [
+          'Ford',
+          'Toyota',
+          'Volkswagen',
+          'Opel',
+          'Honda',
+          'Porsche'
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/vuestic-theme/vuestic-components/vuestic-tag/VuesticTagGroup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="vuestic-tag-group">
+    <vuestic-tag
+      v-for="(item, index) in value"
+      :key="index"
+      :name="item"
+      @remove="removeTag(index)"
+    />
+  </div>
+</template>
+
+<script>
+  import VuesticTag from './VuesticTag.vue'
+
+  export default {
+    name: 'vuestic-tag-group',
+    props: {
+      value: {
+        type: Array
+      }
+    },
+    components: {
+      VuesticTag
+    },
+    methods: {
+      removeTag (tagIndex) {
+        this.$emit('input', this.value.filter((item, i) => i !== tagIndex))
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .vuestic-tag-group {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
