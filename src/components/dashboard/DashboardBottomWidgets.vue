f0f4e0effeb661848322050f7223b18870f84702
alexanderrudnik <alexanderrud0603@gmail.com>
2018-07-19 14:48:40 +0300

    feat(project): replace http with https in img sources

--- src/components/dashboard/DashboardBottomWidgets.vue
+++ src/components/dashboard/DashboardBottomWidgets.vue
@@ -23,52 +23,52 @@
           {
             name: 'Irina Myatelskaya',
             text: 'joined the network',
-            photoURL: 'http://i.imgur.com/VuTDC8u.png'
+            photoURL: 'https://i.imgur.com/VuTDC8u.png'
           },
           {
             name: 'Andrei Hrabouski',
             text: 'has just started a live video',
-            photoURL: 'http://i.imgur.com/W3mGrmW.png'
+            photoURL: 'https://i.imgur.com/W3mGrmW.png'
           },
           {
             name: 'Evan You',
             text: 'joined the network',
-            photoURL: 'http://i.imgur.com/D7DOGBH.jpg'
+            photoURL: 'https://i.imgur.com/D7DOGBH.jpg'
           }
         ],
         news: [
           {
-            photoURL: 'http://i.imgur.com/PiTDDcA.png'
+            photoURL: 'https://i.imgur.com/PiTDDcA.png'
           },
           {
-            photoURL: 'http://i.imgur.com/M6GugaM.png'
+            photoURL: 'https://i.imgur.com/M6GugaM.png'
           },
           {
-            photoURL: 'http://i.imgur.com/vEy3fRU.png'
+            photoURL: 'https://i.imgur.com/vEy3fRU.png'
           },
           {
-            photoURL: 'http://i.imgur.com/Xrywphx.png'
+            photoURL: 'https://i.imgur.com/Xrywphx.png'
           },
           {
-            photoURL: 'http://i.imgur.com/dqVtQGY.png'
+            photoURL: 'https://i.imgur.com/dqVtQGY.png'
           },
           {
-            photoURL: 'http://i.imgur.com/qP7rTCy.png'
+            photoURL: 'https://i.imgur.com/qP7rTCy.png'
           },
           {
-            photoURL: 'http://i.imgur.com/6YLsM43.png'
+            photoURL: 'https://i.imgur.com/6YLsM43.png'
           },
           {
-            photoURL: 'http://i.imgur.com/9PAOx55.png'
+            photoURL: 'https://i.imgur.com/9PAOx55.png'
           },
           {
-            photoURL: 'http://i.imgur.com/mVpc04D.png'
+            photoURL: 'https://i.imgur.com/mVpc04D.png'
           },
           {
-            photoURL: 'http://i.imgur.com/WdbTSLn.png'
+            photoURL: 'https://i.imgur.com/WdbTSLn.png'
           },
           {
-            photoURL: 'http://i.imgur.com/ZXRIHfk.png'
+            photoURL: 'https://i.imgur.com/ZXRIHfk.png'
           }
         ]
       }
