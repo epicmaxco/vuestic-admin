1f949f3dbc87d9efda7be6bd1e9796427c283126
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-07-25 17:57:55 +0300

    fix (404-custom) : styles, fix (404 pages page) : no background color

--- src/components/pages/404-pages/VuesticPageNotFoundCustom.vue
+++ src/components/pages/404-pages/VuesticPageNotFoundCustom.vue
@@ -4,19 +4,19 @@
       <div class="not-found-wallpaper">
         <router-link class="i-vuestic vuestic-page-not-found-custom__i-vuestic" :to="{path: '/'}"/>
       </div>
-      <div class="not-found-wallpaper">
+      <div class="vuestic-page-not-found-custom__wallpaper">
         <img src="https://i.imgur.com/wodIng5.png" class="vuestic-page-not-found-custom__custom-image"/>
       </div>
-      <div class="not-found-wallpaper">
+      <div class="vuestic-page-not-found-custom__wallpaper">
         <h3 class="vuestic-page-not-found-custom__text">The page's gonna fishing.</h3>
       </div>
-      <div class="not-found-wallpaper col-md-12">
+      <div class="vuestic-page-not-found-custom__wallpaper col-md-12">
         <div class="row vuestic-page-not-found-custom__message">
-          <h4 class="vuestic-page-not-found-custom__text-small">If you fell that it's not right, please send us a message at &nbsp</h4>
+          <h4 class="vuestic-page-not-found-custom__text-small">If you feel that it's not right, please send us a message at &nbsp</h4>
           <a href="mailto:hello@epicmax.co">hi@epicmax.co</a>
         </div>
       </div>
-      <div class="not-found-wallpaper col-md-12 vuestic-page-not-found-custom__wallpaper-button">
+      <div class="vuestic-page-not-found-custom__wallpaper col-md-12 vuestic-page-not-found-custom__wallpaper-button">
         <router-link :to="{ path: './dashboard' }">
           <button class="btn btn-primary vuestic-page-not-found-custom__button">
             {{ 'Back to Dashboard' }}
@@ -24,13 +24,13 @@
         </router-link>
       </div>
     </div>
-    <div class="not-found-wallpaper vuestic-page-not-found-custom__not-found-wallpaper-sad">
+    <div class="vuestic-page-not-found-custom__not-found-wallpaper-sad">
       <img src="https://i.imgur.com/hu80UGU.png" class="vuestic-page-not-found-custom__sad-image"/>
     </div>
-    <div class="made-by-footer">
+    <div class="vuestic-page-not-found-custom__footer">
       <span>
         ©2018. Made by &nbsp
-        <a href="http://epicmax.co"> Epicmax</a>
+        <a href="http://epicmax.co"> Epicmax.</a>
       </span>
     </div>
   </div>
@@ -49,7 +49,7 @@ export default {
 
   &__custom-image {
     padding-top: 30px;
-    width: 25%;
+    width: 30%;
     margin-right: 3%;
   }
 
@@ -70,8 +70,8 @@ export default {
   }
 
   &__sad-image {
-    width: 60%;
-    height: 100%;
+    width: 505px;
+    height: 400px;
   }
 
   &__text {
@@ -80,47 +80,48 @@ export default {
   }
 
   &__text-small {
-    font-family: SourceSansPro;
     font-size: 16px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
     line-height: 1.5;
-    letter-spacing: normal;
     text-align: center;
     color: #bbb4b2;
   }
 
   &__i-vuestic {
     margin-top: 3%;
+    z-index: 2;
+    height: $auth-wallpaper-ivuestic-h;
+    width: 100%;
   }
 
   &__not-found-wallpaper-sad {
+    overflow: hidden;
     position: absolute;
     height: 40%;
+    padding-left: 70%;
     bottom: 0;
-    padding-left: 1000px;
   }
 
-  .not-found-wallpaper {
-    background-color: $top-nav-bg;
+  &__wallpaper {
     overflow: hidden;
+    background-color: $top-nav-bg;
     display: flex;
     align-items: flex-start;
     justify-content: center;
-    .i-vuestic {
-      z-index: 2;
-      height: $auth-wallpaper-ivuestic-h;
-      width: 100%;
-    }
   }
 
   @include media-breakpoint-down(xs) {
     .vuestic-page-not-found-custom {
+      overflow: hidden;
       height: $auth-mobile-main-h;
 
       &__custom-image {
-        width: 70%;
+        width: 90%;
+      }
+
+      &__sad-image {
+        width: 200px;
+        height: 200px;
+        overflow: hidden;
       }
 
       &__wallpaper-button {
@@ -137,8 +138,9 @@ export default {
       }
 
       &__not-found-wallpaper-sad {
-        padding: 0;
-        height: 35%;
+        overflow: hidden;
+        height: 25%;
+        padding-left: 50%;
       }
 
       &__text-small {
@@ -146,16 +148,14 @@ export default {
       }
     }
   }
-  .made-by-footer {
+
+  &__footer {
     display: flex;
     color: white;
     justify-content: center;
-    align-items: center;
-    padding-top: 25px;
     padding-bottom: $made-by-footer-pb;
     position: absolute;
     bottom: 0;
-    height: calc(#{$layout-padding} + #{$widget-mb});
     width: 100%;
   }
 }
