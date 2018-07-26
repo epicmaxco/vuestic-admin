edcd6544d85bc5b666c8ad0ab1f449e4d5be0a8a
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-07-26 13:56:37 +0300

    fix (404 pages) : use cards provide interface for 404 pages

--- src/components/pages/404-pages/404PagesPage.vue
+++ src/components/pages/404-pages/404PagesPage.vue
@@ -6,60 +6,52 @@
           404 pages
         </div>
       </div>
-        <div class="row">
-          <div class="col-md-3 not-found-pages__box">
-            <div class="not-found-pages__advanced-wallpaper">
-              <img src="https://i.imgur.com/GzUR0Wz.png not-found-pages__advanced-pic"/>
-              <div class="not-found-pages__container">
-                <h6 class="not-found-pages__advanced-text">Advanced layout</h6>
-                <router-link :to="{ path: '/pages/not-found-advanced' }">
-                  <button class="btn btn-primary not-found-pages__advanced-button">
-                    {{ 'View Example' }}
-                  </button>
-                </router-link>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="not-found-pages__simple-wallpaper">
-              <img src="https://i.imgur.com/HttcXPi.png"/>
-              <div class="not-found-pages__container">
-                <h6 class="not-found-pages__advanced-text">Simple</h6>
-                <router-link :to="{ path: '/pages/not-found-simple' }">
-                  <button class="btn btn-primary not-found-pages__advanced-button">
-                    {{ 'View Example' }}
-                  </button>
-                </router-link>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="not-found-pages__simple-wallpaper">
-              <img src="https://i.imgur.com/dlcZMiG.png"/>
-              <div class="not-found-pages__container">
-                <h6 class="not-found-pages__advanced-text">Custom image</h6>
-                <router-link :to="{ path: '/pages/not-found-custom' }">
-                  <button class="btn btn-primary not-found-pages__advanced-button">
-                    {{ 'View Example' }}
-                  </button>
-                </router-link>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="not-found-pages__simple-wallpaper">
-              <img src="https://i.imgur.com/qcOlDz7.png"/>
-              <div class="not-found-pages__container">
-                <h6 class="not-found-pages__advanced-text">Large text heading</h6>
-                <router-link :to="{ path: '/pages/not-found-large-text' }">
-                  <button class="btn btn-primary not-found-pages__advanced-button">
-                    {{ 'View Example' }}
-                  </button>
-                </router-link>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="row">
+        <vuestic-card
+          image="https://i.imgur.com/GzUR0Wz.png">
+          {{ 'Advanced layout' }}
+          <p class="pt-3 mb-0">
+            <router-link :to="{ path: '/pages/not-found-advanced' }">
+              <button class="btn btn-primary not-found-pages__advanced-button">
+                {{ 'View Example' }}
+              </button>
+            </router-link>
+          </p>
+        </vuestic-card>
+        <vuestic-card
+          image="https://i.imgur.com/HttcXPi.png">
+          {{ 'Simple' }}
+          <p class="pt-3 mb-0">
+            <router-link :to="{ path: '/pages/not-found-simple' }">
+              <button class="btn btn-primary not-found-pages__advanced-button">
+                {{ 'View Example' }}
+              </button>
+            </router-link>
+          </p>
+        </vuestic-card>
+        <vuestic-card
+          image="https://i.imgur.com/dlcZMiG.png">
+          {{ 'Custom image' }}
+          <p class="pt-3 mb-0">
+            <router-link :to="{ path: '/pages/not-found-custom' }">
+              <button class="btn btn-primary not-found-pages__advanced-button">
+                {{ 'View Example' }}
+              </button>
+            </router-link>
+          </p>
+        </vuestic-card>
+        <vuestic-card
+          image="https://i.imgur.com/qcOlDz7.png">
+          {{ 'Large text heading' }}
+          <p class="pt-3 mb-0">
+            <router-link :to="{ path: '/pages/not-found-large-text' }">
+              <button class="btn btn-primary not-found-pages__advanced-button">
+                {{ 'View Example' }}
+              </button>
+            </router-link>
+          </p>
+        </vuestic-card>
+      </div>
     </div>
   </div>
 </template>
@@ -68,10 +60,13 @@
 
 import VuesticWidget
   from '../../../vuestic-theme/vuestic-components/vuestic-widget/VuesticWidget'
+import VuesticCard
+  from '../../../vuestic-theme/vuestic-components/vuestic-card/VuesticCard'
 
 export default {
   name: 'not-found-pages',
   components: {
+    VuesticCard,
     VuesticWidget
   }
 }
@@ -79,16 +74,6 @@ export default {
 
 <style lang="scss">
 .not-found-pages {
-  display: flex;
-  &__container {
-    height: 40%;
-    text-align: center;
-  }
-
-  &__advanced-text {
-    margin-top: 6%;
-  }
-
   &__advanced-button {
     text-align: center;
     line-height: 6px;
@@ -99,20 +84,6 @@ export default {
     border-radius: 18px;
     background-color: #4ae387;
     box-shadow: 0 4px 9.6px 0.4px rgba(79, 206, 145, 0.5);
-  }
-
-  &__advanced-wallpaper {
-    width: 240px;
-    height: 268px;
-    background-color: #ffffff;
-    box-shadow: 0 8px 14.7px 1.3px rgba(188, 207, 190, 0.5);
-  }
-
-  &__simple-wallpaper {
-    width: 240px;
-    height: 268px;
-    background-color: #ffffff;
-    box-shadow: 0 8px 14.7px 1.3px rgba(188, 207, 190, 0.5);
   }
 }
 </style>
