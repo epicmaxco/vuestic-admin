7e2b0169fa53b997f3f3335d455f39926af43028
Asva <ontrew@gmail.com>
2018-08-14 19:28:12 +0300

    wip: add eslint config and reform package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,18 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
   env: {
-    browser: true,
+    node: true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
   ],
-  // add your custom rules here
-  'rules': {
-    'comma-dangle': 'off',
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    'no-multiple-empty-lines': 0,
-    'no-mixed-operators': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ["error", "only-multiline"]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   }
 }
