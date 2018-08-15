e167f06782923e107bb7d66a0df1db87ed9171ff
Asva <ontrew@gmail.com>
2018-08-16 01:10:40 +0300

    wip: general config done
    Still many errors though.

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,5 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
 module.exports = {
-  "plugins": {
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+  plugins: {
+    autoprefixer: {}
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 // Polyfills
-import 'es6-promise/auto'
-import 'babel-polyfill'
+// import 'es6-promise/auto'
+// import 'babel-polyfill'
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
-import App from './App'
-import store from './store'
-import router from './router'
-import VuesticPlugin from 'vuestic-theme/vuestic-plugin'
+// import App from './App'
+// import store from './store'
+// import router from './router'
+import VuesticPlugin from '@/vuestic-theme/vuestic-plugin'
 import './i18n'
 import YmapPlugin from 'vue-yandex-maps'
 
@@ -19,20 +19,20 @@ Vue.use(YmapPlugin)
 // NOTE: workaround for VeeValidate + vuetable-2
 Vue.use(VeeValidate, {fieldsBagName: 'formFields'})
 
-router.beforeEach((to, from, next) => {
-  store.commit('setLoading', true)
-  next()
-})
+// router.beforeEach((to, from, next) => {
+//   store.commit('setLoading', true)
+//   next()
+// })
 
-router.afterEach((to, from) => {
-  store.commit('setLoading', false)
-})
+// router.afterEach((to, from) => {
+//   store.commit('setLoading', false)
+// })
 
 /* eslint-disable no-new */
 
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
-})
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   render: h => h(App)
+// })
