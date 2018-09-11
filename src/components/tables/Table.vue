d13f507b972d189d3b5f6c58e669e4f990de472d
Alexander Boriskin <alexander.boriskin12@gmail.com>
2018-09-11 19:09:55 +0300

    bug(table-bordered): resolve issue

--- src/components/tables/Table.vue
+++ src/components/tables/Table.vue
@@ -73,6 +73,75 @@
               </tr>
               </tbody>
             </table>
+            <table class="table table-bordered">
+              <thead>
+              <tr>
+                <td>{{'tables.headings.name' | translate}}</td>
+                <td>{{'tables.headings.email' | translate}}</td>
+                <td>{{'tables.headings.city' | translate}}</td>
+                <td align="right">{{'tables.headings.score' | translate}}</td>
+                <td></td>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>Matthew McCormick</td>
+                <td>matthew30@mail.ol</td>
+                <td>Vancouver</td>
+                <td align="right">93</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Nancy Bo</td>
+                <td>nancy@boonweb.com</td>
+                <td>Washington</td>
+                <td align="right">280</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Frederiko Lopez</td>
+                <td>fr.lopez@webmail.sp</td>
+                <td>Barcelona</td>
+                <td align="right">16</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Stanley Hummer</td>
+                <td>mr_winner_2999@gmail.cb</td>
+                <td>Manchester</td>
+                <td align="right">57</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Lendley Wintz</td>
+                <td>9938198146@mailster.io</td>
+                <td>Wien</td>
+                <td align="right">113</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Barbara Noz</td>
+                <td>barbaranoz@mailster.io</td>
+                <td>Brussels</td>
+                <td align="right">68</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Matthew McCormick</td>
+                <td>matthew30@mail.ol</td>
+                <td>Vancouver</td>
+                <td align="right">93</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Nancy Bo</td>
+                <td>nancy@boonweb.com</td>
+                <td>Washington</td>
+                <td align="right">280</td>
+                <td></td>
+              </tr>
+              </tbody>
+            </table>
           </div>
         </vuestic-widget>
       </div>
