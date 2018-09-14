91f039c2373b2444b9e8f0a6ebc40779ec570a5f
Sanya Boriskin <39219429+AlexanderBoriskin@users.noreply.github.com>
2018-09-14 14:35:21 +0300

    Improvement/move external layout styling (#325)
    * ïmprovement(card-layout-styling): update demo
    
    * ïmprovement(move-external-layout-styling): fix bug

--- src/vuestic-theme/vuestic-components/vuestic-card/VuesticCard.demo.vue
+++ src/vuestic-theme/vuestic-components/vuestic-card/VuesticCard.demo.vue
@@ -1,49 +1,51 @@
 <template>
-  <div
-    style="left: 420px; top: 180px; height: calc(100% - 180px); width: calc(100% - 420px); position: absolute; overflow: auto"
-  >
-    <vuestic-card
-      stripe="warning"
-      title-on-image
-      overlay
-      style="width: 400px;"
-      image="https://picsum.photos/300/200/?random"
-    >
-      <template slot="title">
-        Card with overlay and text on top of image
-      </template>
-      Running out of pages in your passport. Two trailer park girls go around the outside.
-    </vuestic-card>
-
-    <br>
-
-    <vuestic-card
-      stripe="danger"
-      style="width: 400px;"
-      image="https://picsum.photos/300/200/?random"
-      theme="bright"
-    >
-      <template slot="title">
-        Card title that wraps to a new line
-      </template>
-      This is a longer card with supporting text below as a natural lead-in to
-      additional content. This content is a little bit longer.
-      <p slot="footer">Last updated 3 mins ago</p>
-    </vuestic-card>
-
-    <br>
-
-    <vuestic-card
-      stripe="primary"
-      style="width: 400px;"
-      image="https://picsum.photos/300/200/?random"
-      theme="dark"
-    >
-      <p slot="title">Card title that wraps to a new line</p>
-      This is a longer card with supporting text below as a natural lead-in to
-      additional content. This content is a little bit longer.
-      <p slot="footer">Last updated 3 mins ago</p>
-    </vuestic-card>
+  <div class="demo-container">
+    <div class="demo-container__item">
+      <div class="row">
+        <div class="col-md-3">
+          <vuestic-card
+            stripe="warning"
+            title-on-image
+            overlay
+            image="https://picsum.photos/300/200/?random">
+            <template slot="title">
+              Card with overlay and text on top of image
+            </template>
+            Running out of pages in your passport. Two trailer park girls go around the outside.
+          </vuestic-card>
+        </div>
+        <div class="col-md-3">
+          <vuestic-card
+            stripe="danger"
+            image="https://picsum.photos/300/200/?random"
+            theme="bright">
+            <template slot="title">
+              Card title that wraps to a new line
+            </template>
+            This is a longer card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.
+            <p slot="footer">Last updated 3 mins ago</p>
+          </vuestic-card>
+        </div>
+      </div>
+    </div>
+    <div class="demo-container__item">
+      <div class="row">
+        <div class="col-md-4">
+          <vuestic-card
+            stripe="danger"
+            image="https://picsum.photos/300/200/?random"
+            theme="bright">
+            <template slot="title">
+              Card title that wraps to a new line
+            </template>
+            This is a longer card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.
+            <p slot="footer">Last updated 3 mins ago</p>
+          </vuestic-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
