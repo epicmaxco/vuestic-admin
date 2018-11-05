7a23154f94e9f16fbd4d0ce610d2d8d32380a3b0
Alexander Boriskin <alexander.boriskin12@gmail.com>
2018-11-05 18:39:52 +0300

    feature(button-component): create base template

---
+++ src/vuestic-theme/vuestic-components/vuestic-button/VuesticButton.demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo-container">
+    <div class="demo-container__item" style="width: 900px;">
+      <div class="row">
+        <div class="col-lg-6">
+          <vuestic-button tag="button" disabled>
+            <span slot="title">Large Button</span>
+          </vuestic-button>
+        </div>
+        <div class="col-lg-6">
+          <vuestic-button small :color="'vuestic-blue'">
+            <span slot="title">Large Button</span>
+          </vuestic-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import VuesticButton from './VuesticButton'
+
+export default {
+  components: {
+    VuesticButton
+  }
+}
+</script>
