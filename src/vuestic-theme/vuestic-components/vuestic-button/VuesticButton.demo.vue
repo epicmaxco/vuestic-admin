d394b526b93d41eafcce7b68f5cd914ced377647
Alexander Boriskin <alexander.boriskin12@gmail.com>
2018-11-06 19:42:31 +0300

    feature(button-component): add types

--- src/vuestic-theme/vuestic-components/vuestic-button/VuesticButton.demo.vue
+++ src/vuestic-theme/vuestic-components/vuestic-button/VuesticButton.demo.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="demo-container">
-    <div class="demo-container__item" style="width: 900px;">
+    <div class="demo-container__item" style="width: 1500px;">
       <div class="row">
-        <div class="col-lg-6">
-          <vuestic-button tag="button" disabled>
+        <div class="col-lg-4">
+          <vuestic-button tag="button" :color="'vuestic-green'">
+            <span slot="title">Default Button</span>
+          </vuestic-button>
+        </div>
+        <div class="col-lg-4">
+          <vuestic-button large :color="'vuestic-blue'">
             <span slot="title">Large Button</span>
           </vuestic-button>
         </div>
-        <div class="col-lg-6">
-          <vuestic-button small :color="'vuestic-blue'">
+        <div class="col-lg-4">
+          <vuestic-button small :color="'vuestic-red'">
+            <span slot="title">Small Button</span>
+          </vuestic-button>
+        </div>
+      </div>
+      <div class="row" style="margin-top: 100px;">
+        <div class="col-lg-4">
+          <vuestic-button tag="button" :color="'vuestic-yellow'">
+            <span slot="title">Default Button</span>
+          </vuestic-button>
+        </div>
+        <div class="col-lg-4">
+          <vuestic-button large :color="'vuestic-gray'">
             <span slot="title">Large Button</span>
+          </vuestic-button>
+        </div>
+        <div class="col-lg-4">
+          <vuestic-button small :color="'vuestic-dark'">
+            <span slot="title">Small Button</span>
           </vuestic-button>
         </div>
       </div>
