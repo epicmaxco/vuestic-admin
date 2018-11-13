c039c8ef5036a261c3f86a24f0dc5c550813679d
Alexander Boriskin <alexander.boriskin12@gmail.com>
2018-11-13 18:03:42 +0300

    feature/button-component: icons behaviour

--- src/vuestic-theme/vuestic-components/vuestic-button/VuesticButton.demo.vue
+++ src/vuestic-theme/vuestic-components/vuestic-button/VuesticButton.demo.vue
@@ -3,65 +3,69 @@
     <div class="demo-container__item" style="width: 1500px;">
       <div style="margin-top: 50px;">
         <vuestic-button :color="'vuestic-button--success'">
-          <img src="../../../assets/icons/plus-icon-def.svg"/>
-          <span slot="title">Default Button</span>
+          <img slot="icon"
+               src="../../../assets/icons/plus-icon-def.svg"/>
+          Default Button
         </vuestic-button>
         <vuestic-button large :color="'vuestic-button--info'">
-          <img src="../../../assets/icons/plus-icon-lg.svg"/>
-          <span slot="title">Large Button</span>
+          <img slot="icon" src="../../../assets/icons/plus-icon-lg.svg"/>
+          Large Button
         </vuestic-button>
         <vuestic-button small :color="'vuestic-button--danger'">
-          <img src="../../../assets/icons/plus-icon-sm.svg"/>
-          <span slot="title">Mini Button</span>
+          <img slot="icon" src="../../../assets/icons/plus-icon-sm.svg"/>
+          Mini Button
         </vuestic-button>
         <vuestic-button tag="button" :color="'vuestic-button--warning'">
-          <span slot="title">Default Button</span>
+          <img slot="icon-right" src="../../../assets/icons/dropdown-icon-def.svg"/>
+          Default Button
         </vuestic-button>
         <vuestic-button large :color="'vuestic-button--gray'">
-          <span slot="title">Large Button</span>
+          <img slot="icon-right" src="../../../assets/icons/dropdown-icon-lg.svg"/>
+          Large Button
         </vuestic-button>
         <vuestic-button small :color="'vuestic-button--dark'">
-          <span slot="title">Mini Button</span>
+          <img slot="icon-right" src="../../../assets/icons/dropdown-icon-sm.svg"/>
+          Mini Button
         </vuestic-button>
       </div>
       <div style="margin-top: 50px;">
         <vuestic-button outline :color="'vuestic-button--success'">
-          <span slot="title">Default Button</span>
+          <span>Default Button</span>
         </vuestic-button>
         <vuestic-button large outline :color="'vuestic-button--info'">
-          <span slot="title">Large Button</span>
+          <span>Large Button</span>
         </vuestic-button>
         <vuestic-button small outline :color="'vuestic-button--danger'">
-          <span slot="title">Mini Button</span>
+          <span>Mini Button</span>
         </vuestic-button>
         <vuestic-button outline :color="'vuestic-button--warning'">
-          <span slot="title">Default Button</span>
+          <span>Default Button</span>
         </vuestic-button>
         <vuestic-button large outline :color="'vuestic-button--gray'">
-          <span slot="title">Large Button</span>
+          <span>Large Button</span>
         </vuestic-button>
         <vuestic-button small outline :color="'vuestic-button--dark'">
-          <span slot="title">Mini Button</span>
+          <span>Mini Button</span>
         </vuestic-button>
       </div>
       <div style="margin-top: 50px;">
         <vuestic-button flat :color="'vuestic-button--success'">
-          <span slot="title">Default Button</span>
+          <span>Default Button</span>
         </vuestic-button>
         <vuestic-button large flat :color="'vuestic-button--info'">
-          <span slot="title">Large Button</span>
+          <span>Large Button</span>
         </vuestic-button>
         <vuestic-button small flat :color="'vuestic-button--danger'">
-          <span slot="title">Mini Button</span>
+          <span>Mini Button</span>
         </vuestic-button>
         <vuestic-button flat :color="'vuestic-button--warning'">
-          <span slot="title">Default Button</span>
+          <span>Default Button</span>
         </vuestic-button>
         <vuestic-button large flat :color="'vuestic-button--gray'">
-          <span slot="title">Large Button</span>
+          <span>Large Button</span>
         </vuestic-button>
         <vuestic-button small flat :color="'vuestic-button--dark'">
-          <span slot="title">Mini Button</span>
+          <span>Mini Button</span>
         </vuestic-button>
       </div>
     </div>
