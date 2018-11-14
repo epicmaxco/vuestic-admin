7c56c7edcdf73684c8b99ce4fb720582ec81203f
Alexander Boriskin <alexander.boriskin12@gmail.com>
2018-11-14 18:08:49 +0300

    feature/icon-component: create first version of component

--- src/vuestic-theme/vuestic-components/vuestic-icon/VuesticIcon.demo.vue
+++ src/vuestic-theme/vuestic-components/vuestic-icon/VuesticIcon.demo.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="demo-container">
-    <div class="demo-container__item"
-         v-for="icon in icons"
-         :key="icon"
-    >
-      <vuestic-icon :icon="icon"/>
+    <div class="demo-container__item" style="margin-top: 20px; margin-left: 20px;">
+      <vuestic-icon :icon="'vuestic-i vuestic-i-comments'" :color="'info'" large/>
+    </div>
+    <div class="demo-container__item" style="margin-top: 20px; margin-left: 20px;">
+      <vuestic-icon :icon="'vuestic-i vuestic-i-components'" :color="'danger'" small/>
+    </div>
+    <div class="demo-container__item" style="margin-top: 20px; margin-left: 20px;">
+      <vuestic-icon :icon="'vuestic-i vuestic-i-dashboard'" :color="'success'" :size="60"/>
+    </div>
+    <div class="demo-container__item" style="margin-top: 20px; margin-left: 20px;">
+      <vuestic-icon :icon="'vuestic-i vuestic-i-extras'" :color="'warning'" large/>
+    </div>
+    <div class="demo-container__item" style="margin-top: 20px; margin-left: 20px;">
+      <vuestic-icon :icon="'vuestic-i vuestic-i-files'" :color="'gray'" small/>
+    </div>
+    <div class="demo-container__item" style="margin-top: 20px; margin-left: 20px;">
+      <vuestic-icon :icon="'vuestic-i vuestic-i-forms'" :color="'dark'" :size="'80px'"/>
+    </div>
+    <div class="demo-container__item" style="margin-top: 20px; margin-left: 20px;">
+      <h4>Fixed Width</h4>
+      <div v-for="(item, key) in icons" :key="key">
+        <vuestic-icon :icon="item.icon" :color="'info'" fixed-width/>
+        <span style="margin-left: 30px;">{{ item.text }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import VuesticIcon from './VuesticIcon.vue'
-import { vuesticIcons } from './vuestic-icons'
 
 export default {
   components: {
@@ -19,7 +37,11 @@ export default {
   },
   computed: {
     icons: () => {
-      return Object.keys(vuesticIcons)
+      return [
+        { icon: 'fontelico fontelico-emo-beer', text: 'Item 1' },
+        { icon: 'fontelico fontelico-emo-devil', text: 'Item 2' },
+        { icon: 'fontelico fontelico-emo-surprised', text: 'Item 3' }
+      ]
     },
   },
 }
