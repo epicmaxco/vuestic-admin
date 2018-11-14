96d436c79c049583ba09328ff57658b1a0831367
Alexander Boriskin <alexander.boriskin12@gmail.com>
2018-11-14 18:08:49 +0300

    feature/icon-component: create first version of component

--- src/vuestic-theme/vuestic-components/vuestic-icon/VuesticIcon.vue
+++ src/vuestic-theme/vuestic-components/vuestic-icon/VuesticIcon.vue
@@ -1,29 +1,78 @@
 <template>
-  <component :is="iconComponent"/>
+  <i class="vuestic-icon"
+    :class="[icon, iconClass]"
+    :style="iconStyle">
+  </i>
 </template>
 
 <script>
-import { vuesticIcons } from './vuestic-icons'
-
 export default {
   name: 'vuestic-icon',
   props: {
     icon: {
-      validator: (icon) => {
-        return icon in vuesticIcons
-      },
+      type: String
+    },
+    small: {
+      type: Boolean,
+      default: false
+    },
+    large: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: [String, Number],
+      default: undefined
+    },
+    fixedWidth: {
+      type: Boolean
+    },
+    rotation: {
+      type: [String, Number]
+    },
+    color: {
+      type: String,
+      default: undefined
     },
   },
   computed: {
-    iconComponent () {
-      return vuesticIcons[this.icon]
+    iconClass: function () {
+      return {
+        'vuestic-icon--large': this.large,
+        'vuestic-icon--small': this.small,
+        'vuestic-icon--fixed': this.fixedWidth,
+        'vuestic-icon--success': this.color === 'success',
+        'vuestic-icon--info': this.color === 'info',
+        'vuestic-icon--danger': this.color === 'danger',
+        'vuestic-icon--warning': this.color === 'warning',
+        'vuestic-icon--gray': this.color === 'gray',
+        'vuestic-icon--dark': this.color === 'dark',
+      }
     },
-  },
+    iconStyle: function () {
+      return {
+        transform: 'rotate(' + this.rotation + 'deg)',
+        fontSize: typeof this.size === 'number' ? this.size + 'px' : this.size
+      }
+    },
+  }
 }
 </script>
 
 <style lang="scss">
 .vuestic-icon {
+  &--large {
+    font-size: 36px;
+  }
 
+  &--small {
+    font-size: 16px;
+  }
+
+  &--fixed {
+    display: inline-block;
+    width: 50px;
+    text-align: center;
+  }
 }
 </style>
