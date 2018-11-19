4d1d229ae726aabb0f68d1b179e46559933d3350
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-11-19 18:24:51 +0300

    minor

---
+++ src/vuestic-theme/vuestic-sass/global/VuesticLink.demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="demo-container">
+    <div class="demo-container__item">
+      <p class="link">default link</p>
+    </div>
+    <div class="demo-container__item">
+      <p class="link-secondary">secondary link</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
