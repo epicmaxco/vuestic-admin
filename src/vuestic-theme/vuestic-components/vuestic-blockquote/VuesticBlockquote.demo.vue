8a1922ec915c95a86d7d3107d8e52ea1e0a021b6
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-11-27 16:06:54 +0300

    minor

---
+++ src/vuestic-theme/vuestic-components/vuestic-blockquote/VuesticBlockquote.demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="demo-container">
+    <div class="demo-container__item">
+      <vuestic-blockquote :text="quote" :author="author">
+        <p style="color: #babfc2; margin-bottom: 0.5rem;">{{ quote }}</p>
+      </vuestic-blockquote>
+    </div>
+  </div>
+</template>
+
+<script>
+import VuesticBlockquote from './VuesticBlockquote'
+
+export default {
+  components: {
+    VuesticBlockquote
+  },
+  data () {
+    return {
+      quote: "BQ: Let’s talk about meat fondue recipes and what you need to know" +
+        " first. Meat fondue also known as oil fondue is a method of cooking all kinds.",
+      author: "Mister Lebowski"
+    }
+  }
+}
+</script>
