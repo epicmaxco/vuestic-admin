4ce8f14968333c9ab82fdeccf011dd0fe1b459a1
Vlad Shusterman <vlad.shusterman30@gmail.com>
2018-11-27 16:06:54 +0300

    minor

--- src/components/ui/typography/Typography.vue
+++ src/components/ui/typography/Typography.vue
@@ -1,38 +1,50 @@
 <template>
   <div class="ui-typography">
-    <div class="va-row">
-      <div class="md12 flex">
-        <vuestic-widget>
-          <p class="display-1 text--bold">display-1 Heading</p>
-          <p class="display-5 ui-typography__item">Of all of the celestial bodies that capture our
-            attention and fascination as astronomers, none has a greater influence
-            on life on planet Earth than it’s own satellite, the moon. When
-            you think about it.</p>
-          <p class="display-2 text--bold">display-2 Heading</p>
-          <p class="display-5 ui-typography__item">None has a greater influence on life on planet
-            Earth than it’s own satellite, the moon. When you think about it.</p>
-          <p class="display-3 text--bold">display-3 Heading</p>
-          <p class="display-5 ui-typography__item">Let’s talk about meat fondue recipes and what you
-            need to know first. Meat fondue also known as oil fondue is a method
-            of cooking all kinds of meats, poultry, and seafood in a pot of
-            heated oil.</p>
-          <p class="display-4 text--bold">display-4 Heading</p>
-          <p class="display-5 ui-typography__item">There is something about parenthood that gives us
-            a sense of history and a deeply rooted desire to send on into the
-            next generation the great things we have discovered about life.</p>
-          <p class="display-5 text--bold">display-5 Heading</p>
-          <p class="display-5 ui-typography__item">
-            There is a moment in the life of any aspiring astronomer that it is
-            time to buy that first telescope. It’s exciting to think about
-            setting up your own viewing station.
-          </p>
-          <p class="code-snippet">
-            &lt;p&gt class=“code_snippet”> <br/>
-            &emsp;This is a wonderful example.<br/>
-            &emsp; &lt;a&gt href=“#” onClick=“”>Read more&lt;/a&gt<br/>
-            &lt;/p&gt <br/>
-          </p>
-          <div class="ui-typography__item--links">
+    <vuestic-widget>
+      <div class="va-row column">
+        <div class="md12 flex">
+          <div class="ui-typography__item">
+            <p class="display-1 text--bold">display-1 Heading</p>
+            <p class="display-5 ui-typography__item">Of all of the celestial bodies that capture our
+              attention and fascination as astronomers, none has a greater influence
+              on life on planet Earth than it’s own satellite, the moon. When
+              you think about it.</p>
+          </div>
+          <div class="ui-typography__item">
+            <p class="display-2 text--bold">display-2 Heading</p>
+            <p class="display-5 ui-typography__item">None has a greater influence on life on planet
+              Earth than it’s own satellite, the moon. When you think about it.</p>
+          </div>
+          <div class="ui-typography__item">
+            <p class="display-3 text--bold">display-3 Heading</p>
+            <p class="display-5 ui-typography__item">Let’s talk about meat fondue recipes and what you
+              need to know first. Meat fondue also known as oil fondue is a method
+              of cooking all kinds of meats, poultry, and seafood in a pot of
+              heated oil.</p>
+          </div>
+          <div class="ui-typography__item">
+            <p class="display-4 text--bold">display-4 Heading</p>
+            <p class="display-5 ui-typography__item">There is something about parenthood that gives us
+              a sense of history and a deeply rooted desire to send on into the
+              next generation the great things we have discovered about life.</p>
+          </div>
+          <div class="ui-typography__item">
+            <p class="display-5 text--bold">display-5 Heading</p>
+            <p class="display-5 ui-typography__item">
+              There is a moment in the life of any aspiring astronomer that it is
+              time to buy that first telescope. It’s exciting to think about
+              setting up your own viewing station.
+            </p>
+          </div>
+          <div class="ui-typography__item">
+            <p class="code-snippet">
+              &lt;p&gt class=“code_snippet”> <br/>
+              &emsp;This is a wonderful example.<br/>
+              &emsp; &lt;a&gt href=“#” onClick=“”>Read more&lt;/a&gt<br/>
+              &lt;/p&gt <br/>
+            </p>
+          </div>
+          <div class="ui-typography__item">
             <div class="flex md8">
               <a href="/not-visited" class="link ui-typography__default-link">default link</a>
               <a href="/not-visited" class="link-secondary flex offset--md1">secondary link</a>
@@ -45,16 +57,16 @@
           <div class="ui-typography__item">
             <p class="display-3 text--bold">Text transforms</p>
             <ul class="vue-unordered">
-              <li>.text--uppercase - Sets text-transform to uppercase</li>
-              <li>.text-lowercase - Sets text-transform to lowercase</li>
-              <li>.text-capitalize - Sets text-transform to capitalize</li>
+              <li><span class="text--highlighted">.text--uppercase</span> - Sets text-transform to uppercase</li>
+              <li><span class="text--highlighted">.text-lowercase</span> - Sets text-transform to lowercase</li>
+              <li><span class="text--highlighted">.text-capitalize</span> - Sets text-transform to capitalize</li>
             </ul>
           </div>
           <div class="ui-typography__item">
             <p class="display-3 text--bold">Text wrapping</p>
             <ul class="vue-unordered">
-              <li>.text--no-wrap - Sets whitespace to no-wrap</li>
-              <li>.text--truncate - Truncates overflowed text</li>
+              <li> <span class="text--highlighted">.text--no-wrap </span> - Sets whitespace to no-wrap</li>
+              <li> <span class="text--highlighted">.text--truncate</span> - Truncates overflowed text</li>
             </ul>
           </div>
           <p class="display-3 text--bold">Lists</p>
@@ -68,7 +80,7 @@
             <li>Earth than it’s own satellite, the moon. When you think about it.</li>
             <li>Attention and fascination as .</li>
           </ul>
-          <div>
+          <div class="ui-typography__item">
             <p class="display-3 text--bold">Other Elements</p>
             <span class="display-5">None has a greater influence on </span>
             <span class="display-5 text--highlighted">highlighted text</span>
@@ -78,24 +90,53 @@
             <span class="display-5 text--selected">selected chunk of text</span>
             <span class="display-5">. When you think about it.</span>
           </div>
-        </vuestic-widget>
+          <div class="va-row">
+            <div class="well" style="width: 600px;">
+              <p class="display-3 text--bold">Display-3 Heading</p>
+              <span class="display-5">Of all of the celestial bodies that capture our
+              attention and fascination as astronomers, none has a greater
+              influence on life on planet Earth than it’s own satellite,
+              the moon. When you think about it.</span>
+            </div>
+            <vuestic-blockquote :text="quote" :author="author" class="flex offset--md1">
+              <p class="ui-typography__quote">{{ quote }}</p>
+              <p class="ui-typography__author">{{ '— '+ author }}</p>
+            </vuestic-blockquote>
+          </div>
+        </div>
       </div>
-    </div>
+    </vuestic-widget>
   </div>
 </template>
 
 <script>
+import VuesticBlockquote
+  from '../../../vuestic-theme/vuestic-components/vuestic-blockquote/VuesticBlockquote'
+
 export default {
   name: 'typography',
+  components: {
+    VuesticBlockquote
+  },
   data () {
-    return {}
+    return {
+      quote: "BQ: Let’s talk about meat fondue recipes and what you need to know" +
+        " first. Meat fondue also known as oil fondue is a method of cooking all kinds.",
+      author: "Mister Lebowski"
+    }
   }
 }
 </script>
 
 <style lang="scss">
 .ui-typography {
-
+  &__item {
+    margin-bottom: 2rem;
+  }
+  &__quote, &__author {
+    color: #babfc2;
+    margin-bottom: 0.5rem;
+  }
 }
 
   .vue-lists ul, ol {
---
+++ src/vuestic-theme/vuestic-components/vuestic-blockquote/VuesticBlockquote.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="vuestic-blockquote">
+    <div class="vuestic-blockquote__content">
+      <div class="vuestic-blockquote__line"/>
+      <div class="va-row column">
+        <slot></slot>
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vuestic-blockquote',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    author: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.vuestic-blockquote {
+  width: 600px;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  &__content {
+    display: inline-flex;
+  }
+  &__line {
+    margin-right: 1rem;
+    width: 0.4rem;
+    border-radius: 2px;
+    background-color: $vue-green;
+  }
+}
+</style>
