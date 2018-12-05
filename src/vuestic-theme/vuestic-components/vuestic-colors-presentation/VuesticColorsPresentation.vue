4f4fae83b1c6e46d657611ce2e21d52a0c0d21da
NastassiaDanilova <nastassia.danilova@gmail.com>
2018-12-05 09:45:36 +0300

    color presentation component and demo

---
+++ src/vuestic-theme/vuestic-components/vuestic-colors-presentation/VuesticColorsPresentation.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="vuestic-colors-presentation">
+    <vuestic-tooltip :options="tooltipOptions">
+      <div class="vuestic-colors-presentation__color" :style="computedStyle" @click="colorCopy"></div>
+    </vuestic-tooltip>
+    <div class="vuestic-colors-presentation__description" v-if="name || description">
+      <div class="vuestic-colors-presentation__name link">{{name}}</div>
+      <div class="vuestic-colors-presentation__text">{{description}}</div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import VuesticTooltip from '../vuestic-tooltip/VuesticTooltip'
+
+export default {
+  name: 'vuestic-colors-presentation',
+
+  components: { VuesticTooltip },
+
+  props: {
+    color: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: Number,
+      default: 40
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data () {
+    return {
+      tooltipOptions: {
+        content: 'Click to copy color to clipboard',
+        placement: 'right'
+      }
+    }
+  },
+
+  computed: {
+    computedStyle () {
+      return {
+        background: this.color,
+        width: `${this.width}px`
+      }
+    }
+  },
+
+  methods: {
+    colorCopy () {
+      this.$copyText(this.color)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "../../vuestic-sass/resources/resources";
+
+  .vuestic-colors-presentation {
+    display: flex;
+    margin-bottom: 16px;
+    &__color {
+      height: 40px;
+    }
+    &__description {
+      margin-left: 8px;
+    }
+    &__name {
+      color: $vue-darkest-blue;
+    }
+    &__text {
+      color: $va-link-color-secondary;
+    }
+  }
+</style>
