e295fe026c64a348f915c93d2872054b1b45bc6f
Asva <ontrew@gmail.com>
2018-12-28 17:36:09 +0300

    fix: minor fixes
    * features tab flex
    * datepicker chevrons flex

--- src/components/dashboard/features-tab/FeaturesTab.vue
+++ src/components/dashboard/features-tab/FeaturesTab.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="overview-tab dashboard-tab">
     <div
-      class="flex overview-ro justify--space-around justify--start">
+      class="d-flex overview-row justify--space-around justify--start">
 
       <div class="overview-col">
         <div class="overview-item">
