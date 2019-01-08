7d0c5c6e14d57bdb7cca3d34b4d4c0902e48efab
Alexander Boriskin <alexander.boriskin12@gmail.com>
2019-01-08 19:48:54 +0300

    feat (notification-component): update component

--- src/vuestic-theme/vuestic-components/vuestic-alert/VaNotification.vue
+++ src/vuestic-theme/vuestic-components/vuestic-alert/VaNotification.vue
@@ -1,27 +1,31 @@
 <template>
   <transition v-if="value" name="fade">
     <div class="va-notification" :class="notificationClass">
-      <slot name="icon"></slot>
-      <slot></slot>
-      <vuestic-icon-close-btn v-if="closeable"
-                              class="va-notification__close-btn"
-                              @click="hideNotification()"/>
+      <div class="col">
+        <slot/>
+      </div>
+      <div class="col-md-1 va-notification__close-button">
+        <vuestic-icon-close-button
+          v-if="closeable"
+          @click="hideNotification()"
+        />
+      </div>
     </div>
   </transition>
 </template>
 
 <script>
-import VuesticIconCloseBtn from './../vuestic-icon/VuesticIconCloseBtn'
+import VuesticIconCloseButton from '../vuestic-icon/VuesticIconCloseButton'
 
 export default {
   name: 'va-notification',
   components: {
-    VuesticIconCloseBtn
+    VuesticIconCloseButton
   },
   computed: {
     notificationClass () {
       return {
-        'va-notification--with-close-btn': this.closeable,
+        'va-notification--with-close-button': this.closeable,
         'va-notification--success': this.color === 'success',
         'va-notification--danger': this.color === 'danger',
         'va-notification--warning': this.color === 'warning',
@@ -55,67 +59,63 @@ export default {
 
 <style lang='scss'>
   .va-notification {
-    position: relative;
     display: flex;
     flex-direction: row;
     align-items: center;
     padding: 0.75rem 0.5rem;
-    margin: 4px auto;
+    margin: 0.25rem auto;
     border: 0 solid transparent;
     border-radius: 0.5rem;
 
     &__badge {
-      padding: 2px 8px;
-      display: inline-block;
+      display: inline-flex;
+      padding: 0.125rem 0.5rem;
       font-family: $font-family-sans-serif;
       font-weight: bold;
       text-transform: uppercase;
       font-size: .625rem;
       color: $white;
       letter-spacing: .0625rem;
-      margin-right: 8px;
-      border-radius: 9px;
+      margin-right: 0.5rem;
+      border-radius: 0.5rem;
     }
 
-    & &--with-close-btn {
-      padding-right: $with-close-pr;
-    }
-
-    &__close-btn {
-      position: absolute;
-      right: 8px;
-      top: calc(50% - #{$font-size-base} / 2);
+    &__close-button {
+      display: flex;
       font-size: $font-size-base;
       cursor: pointer;
     }
   }
 
   $vuestic-colors: (
-    success: (#63e5b3, #23e066, #77cea4, #40e583, #d6ffd3, #c0fbc7, #acebcc),
-    danger: (#ff7455, #e34b4a, #b86e6d, #e34b4a, #ffebeb, #fbd2d2, #ffcaca),
-    warning: (#ffd72d, #feb900, #cbb06e, #ffc202, #fff3d1, #ffebb1, #ece1c6),
-    info: (#32b5e4, #2c82e0, #6c97ac, #2c82e0, #caeeff, #b2defb, #b5dcec),
-    gray: (#cdd0d5, #b4b6b9, #a3aab0, #babfc2, #e6e9ec, #dfe3e5, #d1d1d1),
-    dark: (#576675, #34495e, #aebcca, #34495e, #afb6bb, #afb6bb, #8b9194)
+    success: (#40e583, #d6ffd3, #acebcc),
+    danger: (#e34b4a, #ffebeb, #ffcaca),
+    warning: (#ffc202, #fff3d1, #ece1c6),
+    info: (#2c82e0, #caeeff, #b5dcec),
+    gray: (#babfc2, #e6e9ec, #d1d1d1),
+    dark: (#34495e, #afb6bb, #8b9194)
   );
 
   @each $name, $colors in $vuestic-colors {
-    $close-icon-color: nth($colors, 4);
-    $background-color: nth($colors, 5);
-    $box-shadow-color: nth($colors, 7);
+    $close-icon-color: nth($colors, 1);
+    $background-color: nth($colors, 2);
+    $box-shadow-color: nth($colors, 3);
 
     .va-notification--#{$name}{
       background: $background-color;
-      box-shadow: 0 2px 3px 0 $box-shadow-color;
+      box-shadow: 0 0.125rem 0.125rem 0 $box-shadow-color;
 
       .va-notification__badge--#{$name}{
         background-color: $close-icon-color;
       }
 
-      .va-notification__close-btn {
-        top: calc(50% - 0.75rem);
-        width: 24px;
-        height: 24px;
+      .va-notification__close-button {
+        width: 1.5rem;
+        height: 1.5rem;
+
+        svg {
+          margin: auto;
+        }
 
         path:last-of-type {
           fill: $close-icon-color;
