ef2525b1add71ea4ad6eb739612f706fd583c6cb
Alexander Boriskin <alexander.boriskin12@gmail.com>
2019-01-08 13:43:43 +0300

    feat (button-component): update demo presentation

--- src/vuestic-theme/vuestic-components/vuestic-button/VuesticButton.demo.vue
+++ src/vuestic-theme/vuestic-components/vuestic-button/VuesticButton.demo.vue
@@ -1,75 +1,254 @@
 <template>
   <div class="demo-container">
     <div class="demo-container__item" style="width: 1500px;">
-      <div style="margin-top: 50px;">
-        <h4>Buttons</h4>
-        <vuestic-button :color="'success'">
-          <img slot="icon" src="../../../assets/icons/plus-icon-def.svg"/>
-          Default Button
-        </vuestic-button>
-        <vuestic-button large :color="'info'">
-          <img slot="icon" src="../../../assets/icons/plus-icon-lg.svg"/>
-          Large Button
-        </vuestic-button>
-        <vuestic-button small :color="'danger'">
-          <img slot="icon" src="../../../assets/icons/plus-icon-sm.svg"/>
-          Mini Button
-        </vuestic-button>
-        <vuestic-button tag="button" :color="'warning'">
-          <img slot="icon-right" src="../../../assets/icons/dropdown-icon-def.svg"/>
-          Default Button
-        </vuestic-button>
-        <vuestic-button large :color="'gray'">
-          <img slot="icon-right" src="../../../assets/icons/dropdown-icon-lg.svg"/>
-          Large Button
-        </vuestic-button>
-        <vuestic-button small :color="'dark'">
-          <img slot="icon-right" src="../../../assets/icons/dropdown-icon-sm.svg"/>
-          Mini Button
-        </vuestic-button>
-      </div>
-      <div style="margin-top: 50px;">
-        <h4>Links</h4>
-        <vuestic-button :href="'http://epic-spinners.epicmax.co/'" outline :color="'success'">
-          <span>Default Button</span>
-        </vuestic-button>
-        <vuestic-button :href="'http://epic-spinners.epicmax.co/'" large outline :color="'info'">
-          <span>Large Button</span>
-        </vuestic-button>
-        <vuestic-button :href="'http://epic-spinners.epicmax.co/'" small outline :color="'danger'">
-          <span>Mini Button</span>
-        </vuestic-button>
-        <vuestic-button :href="'http://epic-spinners.epicmax.co/'" outline :color="'warning'">
-          <span>Default Button</span>
-        </vuestic-button>
-        <vuestic-button :href="'http://epic-spinners.epicmax.co/'" large outline :color="'gray'">
-          <span>Large Button</span>
-        </vuestic-button>
-        <vuestic-button :href="'http://epic-spinners.epicmax.co/'" small outline :color="'dark'">
-          <span>Mini Button</span>
-        </vuestic-button>
-      </div>
-      <div style="margin-top: 50px;">
-        <h4>Router links</h4>
-        <vuestic-button :to="{ name: 'charts' }" flat :color="'success'">
-          Default Button
-        </vuestic-button>
-        <vuestic-button :to="{ name: 'charts' }" flat large :tag="'router-link'" :color="'info'">
-          Large Button
-        </vuestic-button>
-        <vuestic-button :to="{ name: 'charts' }" flat small :tag="'router-link'" :color="'danger'">
-          Mini Button
-        </vuestic-button>
-        <vuestic-button :to="{ name: 'charts' }" flat :tag="'router-link'" :color="'warning'">
-          Default Button
-        </vuestic-button>
-        <vuestic-button :to="{ name: 'charts' }" flat large :tag="'router-link'" :color="'gray'">
-          Large Button
-        </vuestic-button>
-        <vuestic-button :to="{ name: 'charts' }" flat small :color="'dark'" :tag="'router-link'">
-          Mini Button
-        </vuestic-button>
-      </div>
+      <table class="table table-bordered test-table">
+        <tr>
+          <th>Description</th>
+          <th>Vuestic Button</th>
+        </tr>
+        <tr>
+          <td>Types</td>
+          <td>
+            <div class="row">
+              <div class="col-4">
+                <vuestic-button color="success">
+                  Default Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button outline color="success">
+                  Outline Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button flat color="success">
+                  Flat Button
+                </vuestic-button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Sizes
+          </td>
+          <td>
+            <div class="row">
+              <div class="col-4">
+                <vuestic-button color="success">
+                  Default Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button large color="success">
+                  Large Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button small color="success">
+                  Small Button
+                </vuestic-button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Colors
+          </td>
+          <td>
+            <div class="row">
+              <div class="col-2">
+                <vuestic-button color="success">
+                  Success Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button color="info">
+                  Info Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button color="danger">
+                  Danger Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button color="warning">
+                  Warning Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button color="gray">
+                  Gray Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button color="dark">
+                  Dark Button
+                </vuestic-button>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-2">
+                <vuestic-button outline color="success">
+                  Success Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button outline color="info">
+                  Info Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button outline color="danger">
+                  Danger Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button outline color="warning">
+                  Warning Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button outline color="gray">
+                  Gray Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button outline color="dark">
+                  Dark Button
+                </vuestic-button>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-2">
+                <vuestic-button flat color="success">
+                  Success Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button flat color="info">
+                  Info Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button flat color="danger">
+                  Danger Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button flat color="warning">
+                  Warning Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button flat color="gray">
+                  Gray Button
+                </vuestic-button>
+              </div>
+              <div class="col-2">
+                <vuestic-button flat color="dark">
+                  Dark Button
+                </vuestic-button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>Disabled state</td>
+          <td>
+            <div class="row">
+              <div class="col-4">
+                <vuestic-button disabled color="success">
+                  Default Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button disabled outline color="success">
+                  Outline Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button disabled flat color="success">
+                  Flat Button
+                </vuestic-button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Button tags
+          </td>
+          <td>
+            <div class="row">
+              <div class="col-4">
+                <vuestic-button color="success">
+                  Default Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button href="http://epic-spinners.epicmax.co/" color="success">
+                  A-link Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button :to="{ name: 'charts' }" color="success">
+                  Router-link Button
+                </vuestic-button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            With icons
+          </td>
+          <td>
+            <div class="row">
+              <div class="col-4">
+                <vuestic-button color="success">
+                  <vuestic-icon-plus slot="icon"/>
+                  Plus Button
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button color="success">
+                  Dropdown button
+                  <vuestic-icon-dropdown slot="icon-right"/>
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button color="success">
+                  <vuestic-icon-plus slot="icon"/>
+                  Mixed button
+                  <vuestic-icon-dropdown slot="icon-right"/>
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button color="success">
+                  <span slot="icon">A</span>
+                  Button with left icon
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button color="success">
+                  Button with right icon
+                  <span slot="icon-right">B</span>
+                </vuestic-button>
+              </div>
+              <div class="col-4">
+                <vuestic-button color="success">
+                  <span slot="icon">A</span>
+                  Button with two icons
+                  <span slot="icon-right">B</span>
+                </vuestic-button>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
@@ -77,10 +256,14 @@
 <script>
 
 import VuesticButton from './VuesticButton'
+import VuesticIconPlus from './../vuestic-icon/VuesticIconPlus'
+import VuesticIconDropdown from './../vuestic-icon/VuesticIconDropdown'
 
 export default {
   components: {
-    VuesticButton
+    VuesticButton,
+    VuesticIconPlus,
+    VuesticIconDropdown
   }
 }
 </script>
---
+++ src/vuestic-theme/vuestic-components/vuestic-icon/VuesticIconPlus.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg class="vuestic-icon-plus" xmlns="http://www.w3.org/2000/svg" width="27" viewBox="0 0 27 26">
+    <g fill="none" fill-rule="evenodd">
+      <path d="M-3-3h32v31.03H-3z"/>
+      <path d="M14.667 6.465H12v5.171H6.667v2.586H12v5.172h2.667v-5.172H20v-2.586h-5.333V6.465zM13.333 0C5.973 0 0 5.792 0 12.93c0 7.136 5.973 12.929 13.333 12.929s13.334-5.793 13.334-12.93S20.693 0 13.333 0zm0 23.273c-5.88 0-10.666-4.642-10.666-10.344 0-5.702 4.786-10.343 10.666-10.343C19.213 2.586 24 7.227 24 12.929s-4.787 10.344-10.667 10.344z"/>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'vuestic-icon-plus'
+}
+</script>
