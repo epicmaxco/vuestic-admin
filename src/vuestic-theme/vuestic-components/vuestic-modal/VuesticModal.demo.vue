70a6477d58766e9231467b7fb3f1863a4b172dc7
nastassiadanilova <nastassia.danilova@epicmax.co>
2019-01-15 15:11:24 +0300

    vuestic modal

---
+++ src/vuestic-theme/vuestic-components/vuestic-modal/VuesticModal.demo.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="demo-container">
+  <div class="demo-container__item">
+    <p>simple modal with title and message props</p>
+    <button @click="triggerSimpleModal">Show modal</button>
+    <p>modal position</p>
+    <div>
+      <vuestic-radio-button value="center" v-model="modalPosition" option="center"/>
+      <vuestic-radio-button value="top" v-model="modalPosition" option="top"/>
+      <vuestic-radio-button value="right" v-model="modalPosition" option="right"/>
+      <vuestic-radio-button value="bottom" v-model="modalPosition" option="bottom"/>
+      <vuestic-radio-button value="left" v-model="modalPosition" option="left"/>
+    </div>
+    <vuestic-modal-new
+      v-model="isModalShow"
+      title="Simple Popup, Full Width"
+      message="About 64% of all on-line teens say that do things online that they wouldn’t want their parents to know about.   11% of all adult internet users visit dating websites and spend their time Some of the classify their behavior as “cyber affair”   More then 60% of employees use company PC for the personal needs during their work hours as long as 80 minutes per day."
+      okText="Accept"
+      cancelText="Decline"
+      :position="modalPosition"
+    />
+  </div>
+  <div class="demo-container__item">
+    <p>fullscreen modal with, slots use</p>
+    <button @click="triggerFullscreenModal">Show modal</button>
+    <vuestic-modal-new
+      v-model="isFullScreenModalShow"
+      :fullscreen="true">
+      <template slot="header">
+        <h2>Step 2. Centered Layout</h2>
+      </template>
+      <slot>
+        <div>About 64% of all on-line teens say that do things online that they wouldn’t want their parents to know about.   11% of all adult internet users visit dating websites and spend their time Some of the classify their behavior as “cyber affair”   More then 60% of employees use company PC for the personal needs during their work hours as long as 80 minutes per day.</div>
+      </slot>
+    </vuestic-modal-new>
+  </div>
+  <div class="demo-container__item">
+    <p>no-outside-dismiss, no-esc-dismiss, max-width: 80vw, max-height: 150px</p>
+    <button @click="triggerActionsModal">Show modal</button>
+    <vuestic-modal-new
+      v-model="isActionsModalShow"
+      :no-outside-dismiss="true"
+      :no-esc-dismiss="true"
+      max-width="80vw"
+      max-height="150px"
+      title="Step 2. Centered Layout"
+      message="About 64% of all on-line teens say that do things online that they wouldn’t want their parents to know about.   11% of all adult internet users visit dating websites and spend their time Some of the classify their behavior as “cyber affair”   More then 60% of employees use company PC for the personal needs during their work hours as long as 80 minutes per day."
+    />
+  </div>
+</div>
+</template>
+
+<script>
+import VuesticModalNew from './VuesticModalNew'
+import VuesticRadioButton from '../vuestic-radio-button/VuesticRadioButton'
+
+export default {
+  components: { VuesticRadioButton, VuesticModalNew },
+  data () {
+    return {
+      isModalShow: false,
+      isFullScreenModalShow: false,
+      isActionsModalShow: false,
+      modalPosition: 'center'
+    }
+  },
+  methods: {
+    triggerSimpleModal () {
+      this.isModalShow = !this.isModalShow
+    },
+    triggerFullscreenModal () {
+      this.isFullScreenModalShow = !this.isFullScreenModalShow
+    },
+    triggerActionsModal () {
+      this.isActionsModalShow = !this.isActionsModalShow
+    }
+  }
+}
+</script>
---
+++ src/vuestic-theme/vuestic-components/vuestic-modal/VuesticModalNew.vue
@@ -0,0 +1,232 @@
+<template>
+<div
+  v-if="valueProxy"
+  class="vuestic-modal-new__overlay"
+  @click="checkOutside"
+>
+  <transition name="modal" duration="5000">
+    <div
+      class="vuestic-modal-new"
+      :class="computedClass"
+      v-if="valueProxy"
+      :style="{maxWidth, maxHeight}"
+    >
+      <i
+        v-if="closeButton || fullscreen"
+        @click="cancel"
+        class="ion ion-md-close vuestic-modal-new__close"
+      />
+      <div v-if="title" class="vuestic-modal-new__title">{{title}}</div>
+      <div v-if="hasHeaderSlot" class="vuestic-modal-new__header"><slot name="header"/></div>
+      <div v-if="message" class="vuestic-modal-new__message">{{message}}</div>
+      <div class="vuestic-modal-new__content">
+        <slot/>
+      </div>
+      <div v-if="cancelText !== '' || okText !== ''" class="vuestic-modal-new__actions">
+        <button v-if="cancelText !== ''" class="btn btn-secondary btn-micro" @click="cancel">{{cancelText}}</button>
+        <button class="btn btn-primary btn-micro" @click="ok">{{okText}}</button>
+      </div>
+    </div>
+  </transition>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'vuestic-modal-new',
+  props: {
+    value: {
+      required: true,
+      default: false
+    },
+    position: {
+      type: String,
+      validator: function (value) {
+        return ['center', 'top', 'right', 'bottom', 'left'].indexOf(value) !== -1
+      }
+    },
+    title: String,
+    message: String,
+    okText: {
+      type: String,
+      default: 'OK'
+    },
+    cancelText: {
+      type: String,
+      default: 'Cancel'
+    },
+    closeButton: {
+      type: Boolean,
+      default: true
+    },
+    fullscreen: Boolean,
+    mobileFullscreen: {
+      type: Boolean,
+      default: true
+    },
+    noOutsideDismiss: Boolean,
+    noEscDismiss: Boolean,
+    maxWidth: {
+      type: String,
+      default: '600px'
+    },
+    maxHeight: {
+      type: String,
+      default: '100vh'
+    }
+  },
+  computed: {
+    valueProxy: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      },
+    },
+    computedClass () {
+      return {
+        'vuestic-modal-new_fullscreen': this.fullscreen,
+        'vuestic-modal-new_mobile-fullscreen': this.mobileFullscreen,
+        [`vuestic-modal-new_position-${this.position}`]: this.position
+      }
+    },
+    hasHeaderSlot () {
+      return this.$slots.header
+    }
+  },
+  watch: {
+    valueProxy (value) {
+      if (value) {
+        window.addEventListener('keyup', this.listenKeyUp)
+      } else {
+        window.removeEventListener('keyup', this.listenKeyUp)
+      }
+    },
+  },
+  methods: {
+    close () {
+      this.valueProxy = false
+    },
+    cancel () {
+      this.close()
+      this.onCancel && this.onCancel()
+    },
+    ok () {
+      this.close()
+      this.onOk && this.onCancel()
+    },
+    checkOutside (e) {
+      if (!this.noOutsideDismiss) {
+        let modal
+        e.target.childNodes.forEach(node => {
+          if (node.classList && node.classList.contains('vuestic-modal-new')) {
+            modal = node
+          }
+        })
+        if (modal) {
+          this.cancel()
+        }
+      }
+    },
+    listenKeyUp (e) {
+      if (e.code === 'Escape' && !this.noEscDismiss) {
+        this.cancel()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.vuestic-modal-new {
+  &__overlay {
+    background-color: rgba(0,0,0,0.6);
+    z-index: 1000;
+    position: absolute !important;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+  }
+  background: #fff;
+  position: absolute;
+  margin: auto;
+  min-height: 50px;
+  height: fit-content;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  border-radius: 6px;
+  box-shadow: 0 2px 3px 0 rgba(52, 56, 85, 0.25);
+  padding: 20px 24px 24px;
+  overflow: auto;
+  display: flex;
+  flex-flow: column;
+  &_fullscreen {
+    min-width: 100vw !important;
+    height: 100vh !important;
+    border-radius: 0;
+  }
+  &_mobile-fullscreen {
+    @media all and (max-width: map-get($grid-breakpoints, sm)) {
+      min-width: 100vw !important;
+      height: 100vh !important;
+      border-radius: 0;
+    }
+  }
+  &_position{
+    &-top {
+      bottom: auto;
+      margin: 1rem auto;
+    }
+    &-right {
+      left: auto;
+      margin: auto 1rem;
+    }
+    &-bottom {
+      top: auto;
+      margin: 1rem auto;
+    }
+    &-left {
+      right: auto;
+      margin: auto 1rem;
+    }
+  }
+  &__close {
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    cursor: pointer;
+    font-size: 1.5rem;
+    color: $va-link-color-secondary;
+  }
+  &__title {
+    color: $vu-info;
+    font-size: 0.625rem;
+    line-height: 1.2em;
+    text-transform: uppercase;
+    font-weight: $font-weight-bold;
+    letter-spacing: 0.6px;
+    margin-bottom: 24px;
+  }
+  &__header {
+  }
+  &__message {
+    margin-bottom: 1.5rem;
+  }
+  &__actions {
+    display: flex;
+    justify-content: center;
+    margin-top: auto;
+    .btn {
+      margin-right: 20px;
+      &:last-of-type {
+        margin-tight: 0;
+      }
+    }
+  }
+}
+</style>
