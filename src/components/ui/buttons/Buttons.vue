ce23543d2f3870db99f5e8dfa90c63eb2305a69e
Alexander Boriskin <alexander.boriskin12@gmail.com>
2019-01-18 18:17:21 +0300

    feat(button-component): remove old grid classes and old icons

--- src/components/ui/buttons/Buttons.vue
+++ src/components/ui/buttons/Buttons.vue
@@ -1,84 +1,84 @@
 <template>
   <div class="buttons-page">
-    <div class="row">
-      <div class="col-12">
+    <div class="va-row">
+      <div class="flex md12 xl12">
         <vuestic-widget class="larger-padding"
                         :headerText="$t('buttons.main')">
-          <div class="row">
-            <div class="col-3">
+          <div class="va-row">
+            <div class="flex sm6 lg3 xl3">
               <va-button> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button disabled> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button outline> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button outline disabled> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button flat> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button flat disabled> {{ $t('buttons.button') }} </va-button>
             </div>
           </div>
         </vuestic-widget>
       </div>
 
-      <div class="col-12">
+      <div class="flex md12">
         <vuestic-widget class="larger-padding"
                         :headerText="$t('buttons.size')">
-          <div class="row">
-            <div class="col-3">
+          <div class="va-row">
+            <div class="flex sm6 lg3 xl3">
               <va-button small> {{ $t('buttons.small') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button large> {{ $t('buttons.large') }} </va-button>
             </div>
           </div>
         </vuestic-widget>
       </div>
-      <div class="col-12">
+      <div class="flex md12">
         <vuestic-widget class="larger-padding"
                         :headerText="$t('buttons.advanced')">
-          <div class="row">
-            <div class="col-3">
+          <div class="va-row">
+            <div class="flex sm6 lg3 xl3">
               <va-button icon="ion-md-close ion"> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button icon-right="ion-ios-arrow-down arrow-down"> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button icon="ion-md-close ion"/>
             </div>
           </div>
         </vuestic-widget>
       </div>
 
-      <div class="col-12">
+      <div class="flex md12">
         <vuestic-widget class="larger-padding"
                         :headerText="$t('buttons.colors')">
-          <div class="row">
-            <div class="col-3">
+          <div class="va-row">
+            <div class="flex sm6 lg3 xl3">
               <va-button color="danger"> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button color="info"> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button color="dark"> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button color="warning"> {{ $t('buttons.button') }} </va-button>
             </div>
           </div>
-          <div class="row">
-            <div class="col-3">
+          <div class="va-row">
+            <div class="flex sm6 lg3 xl3">
               <va-button color="success"> {{ $t('buttons.button') }} </va-button>
             </div>
-            <div class="col-3">
+            <div class="flex sm6 lg3 xl3">
               <va-button color="gray"> {{ $t('buttons.button') }} </va-button>
             </div>
           </div>
