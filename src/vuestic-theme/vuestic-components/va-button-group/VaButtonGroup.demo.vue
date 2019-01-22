832aed6197407c0d2791c015ddc5aaed26846f93
Alexander Boriskin <alexander.boriskin12@gmail.com>
2019-01-22 18:41:33 +0300

    feat(button-group-component): update demo & update styles

---
+++ src/vuestic-theme/vuestic-components/va-button-group/VaButtonGroup.demo.vue
@@ -0,0 +1,102 @@
+<template>
+  <VbDemo>
+    <VbContainer>
+      <table class="table table-bordered">
+        <tr>
+          <th>Description</th>
+          <th>Va Button Group</th>
+        </tr>
+        <tr>
+          <td>Button Group Types</td>
+          <td>
+            <VbContainer title="Default Button Group">
+              <va-button-group>
+                <va-button> Button 1 </va-button>
+                <va-button> Button 2 </va-button>
+                <va-button> Button 3 </va-button>
+              </va-button-group>
+              <va-button-group>
+                <va-button> Button 1 </va-button>
+                <va-button> Button 2 </va-button>
+                <va-button> Button 3 </va-button>
+                <va-button> Button 4 </va-button>
+              </va-button-group>
+            </VbContainer>
+            <VbContainer title="Outline Button Group">
+              <va-button-group>
+                <va-button outline> Button 1 </va-button>
+                <va-button outline> Button 2 </va-button>
+                <va-button outline> Button 3 </va-button>
+              </va-button-group>
+              <va-button-group>
+                <va-button outline> Button 1 </va-button>
+                <va-button outline> Button 2 </va-button>
+                <va-button outline> Button 3 </va-button>
+                <va-button outline> Button 4 </va-button>
+              </va-button-group>
+            </VbContainer>
+          </td>
+        </tr>
+        <tr>
+          <td>Button Group With Icons</td>
+          <td>
+            <VbContainer title="Default Button Group">
+              <va-button-group>
+                <va-button icon="glyphicon glyphicon-pencil"/>
+                <va-button icon="maki maki-art-gallery"/>
+                <va-button icon="glyphicon glyphicon-pencil"/>
+              </va-button-group>
+            </VbContainer>
+            <VbContainer title="Outline Button Group">
+              <va-button-group>
+                <va-button outline icon="glyphicon glyphicon-pencil"/>
+                <va-button outline icon="maki maki-art-gallery"/>
+                <va-button outline icon="glyphicon glyphicon-pencil"/>
+              </va-button-group>
+            </VbContainer>
+          </td>
+        </tr>
+        <tr>
+          <td>Mixed Button Groups</td>
+          <td>
+            <VbContainer>
+              <va-button-group>
+                <va-button color="gray" icon="glyphicon glyphicon-envelope">
+                  First
+                </va-button>
+                <va-button color="gray" icon="entypo entypo-user"> Second </va-button>
+                <va-button color="gray" icon="ion ion-ios-alarm"> Third </va-button>
+              </va-button-group>
+
+              <va-button-group>
+                <va-button color="danger" icon="maki maki-art-gallery">
+                  First
+                </va-button>
+                <va-button color="danger"> Second </va-button>
+                <va-button color="danger"> Third </va-button>
+              </va-button-group>
+
+              <va-button-group>
+                <va-button color="info">
+                  First
+                </va-button>
+                <va-button color="info" icon-right="glyphicon glyphicon-pencil"> Second </va-button>
+                <va-button color="info">
+                  Third
+                </va-button>
+              </va-button-group>
+
+              <va-button-group>
+                <va-button color="warning" icon="glyphicon glyphicon-envelope"/>
+                <va-button color="warning" icon="entypo entypo-user"/>
+                <va-button color="warning" icon-right="ion ion-ios-alarm">
+                  Update
+                </va-button>
+              </va-button-group>
+            </VbContainer>
+          </td>
+        </tr>
+      </table>
+    </VbContainer>
+  </VbDemo>
+</template>
--- src/vuestic-theme/vuestic-components/va-button/VaButton.vue
+++ src/vuestic-theme/vuestic-components/va-button/VaButton.vue
@@ -13,7 +13,9 @@
     :active-class="activeClass"
     :exact="exact"
     :exact-active-class="exactActiveClass"
-    v-on="inputListeners">
+    v-on="inputListeners"
+    tabindex="0"
+  >
     <div class="va-button__content">
       <i
         v-if="icon"
@@ -315,9 +317,21 @@ export default {
         color: $border-color !important;
       }
 
-      &:active {
+      &:active, &:focus {
         background-color: $focus-color;
         color: $border-color !important;
+
+        .va-button-group & {
+          background-color: $border-color;
+
+          .va-button__content {
+            color: white;
+          }
+        }
+
+        i {
+          color: white;
+        }
       }
 
       &.va-button--disabled {
