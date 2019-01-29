e2a8fafc5be0d47d1d4ef1c5e7428ff85de0c9cc
Asva <ontrew@gmail.com>
2019-01-29 14:35:31 +0300

    minor

--- src/vuestic-theme/vuestic-components/va-notification/VaNotification.demo.vue
+++ src/vuestic-theme/vuestic-components/va-notification/VaNotification.demo.vue
@@ -1,76 +1,54 @@
 <template>
   <VbDemo>
-    <VbContainer>
-      <table style="width: 500px" class="table table-bordered">
-        <tr>
-          <th>Description</th>
-          <th>Va Notification</th>
-        </tr>
-        <tr>
-          <td>Colorful Notification</td>
-          <td>
-            <VaContainer>
-              <va-notification>
-                <span class="va-badge va-badge--success"> Paid </span>
-                You successfully read this important alert message.
-              </va-notification>
-              <va-notification color="info">
-                <span class="va-badge va-badge--info"> Info </span>
-                This alert needs your attention, but it's not super important. Longer text.
-              </va-notification>
-              <va-notification color="warning">
-                <span class="va-badge va-badge--warning"> On Hold </span>
-                Better check yourself, you're not looking too good.
-              </va-notification>
-              <va-notification color="danger">
-                <span class="va-badge va-badge--danger"> Danger </span>
-                Change a few things up and try submitting again.
-              </va-notification>
-              <va-notification color="gray">
-                <span class="va-badge va-badge--gray"> Processing </span>
-                Better check yourself, you're not looking too good.
-              </va-notification>
-              <va-notification color="dark">
-                <span class="va-badge va-badge--dark"> New Label </span>
-                Change a few things up and try submitting again.
-              </va-notification>
-            </VaContainer>
-          </td>
-        </tr>
-        <tr>
-          <td>Visible Notification</td>
-          <td>
-            <VaContainer>
-              <va-notification v-model="isVisible">
-                <span class="va-badge va-badge--success"> Paid </span>
-                You successfully read this important alert message.
-              </va-notification>
-            </VaContainer>
-          </td>
-        </tr>
-        <tr>
-          <td>Closeable Notification</td>
-          <td>
-            <VaContainer>
-              <va-notification v-model="isVisible2" closeable>
-                <span class="va-badge va-badge--success"> Paid </span>
-                You successfully read this important alert message.
-              </va-notification>
-            </VaContainer>
-          </td>
-        </tr>
-      </table>
+    <VbContainer title="Colorful Notification">
+      <va-notification>
+        <span class="va-badge va-badge--success"> Paid </span>
+        You successfully read this important alert message.
+      </va-notification>
+      <va-notification color="info">
+        <span class="va-badge va-badge--info"> Info </span>
+        This alert needs your attention, but it's not super important. Longer text.
+      </va-notification>
+      <va-notification color="warning">
+        <span class="va-badge va-badge--warning"> On Hold </span>
+        Better check yourself, you're not looking too good.
+      </va-notification>
+      <va-notification color="danger">
+        <span class="va-badge va-badge--danger"> Danger </span>
+        Change a few things up and try submitting again.
+      </va-notification>
+      <va-notification color="gray">
+        <span class="va-badge va-badge--gray"> Processing </span>
+        Better check yourself, you're not looking too good.
+      </va-notification>
+      <va-notification color="dark">
+        <span class="va-badge va-badge--dark"> New Label </span>
+        Change a few things up and try submitting again.
+      </va-notification>
+    </VbContainer>
+    <VbContainer title="Closeable Notification">
+      <vuestic-checkbox
+        label="Toggle visibility"
+        v-model="isCloseableNotificationVisible"
+      />
+      <va-notification v-model="isCloseableNotificationVisible" closeable>
+        <span class="va-badge va-badge--success"> Paid </span>
+        You successfully read this important alert message.
+      </va-notification>
     </VbContainer>
   </VbDemo>
 </template>
 
 <script>
+import VuesticCheckbox from '../vuestic-checkbox/VuesticCheckbox'
+import VaNotification from './VaNotification'
+
 export default {
+  components: { VuesticCheckbox, VaNotification },
   data () {
     return {
-      isVisible: true,
-      isVisible2: true
+      isCloseableNotificationVisible: true,
     }
-  }
+  },
 }
 </script>
