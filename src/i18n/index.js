919b5d76c959fac58c077bbd0a1b74c5d3926a2f
gaisy <068_100006675626_junezk@git.cloud.tencent.com>
2019-03-04 11:13:48 +0800

    add "Simplified Chinese" language.

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,9 +4,10 @@ import Vue from 'vue'
 Vue.i18n.add('br', require('./pt_br.json'))
 Vue.i18n.add('en', require('./en.json'))
 Vue.i18n.add('es', require('./es.json'))
+Vue.i18n.add('cn', require('./cn.json'))
 
 // set the start locale to use
-Vue.i18n.set('en')
+Vue.i18n.set('cn')
 
 // set fallback for non-translated strings
 Vue.i18n.fallback('en')
