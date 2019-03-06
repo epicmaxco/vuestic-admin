b51072167240f4012157c1bcde2a716a3fe193ec
Asva <ontrew@gmail.com>
2019-03-06 17:04:16 +0300

    chore: tweak demo

--- src/vuestic-theme/vuestic-components/vuestic-dropdown-new/VuesticDropdownNew.demo.vue
+++ src/vuestic-theme/vuestic-components/vuestic-dropdown-new/VuesticDropdownNew.demo.vue
@@ -13,18 +13,6 @@
       </vuestic-dropdown-new>
     </VbContainer>
 
-    <!-- <VbContainer title="Title">
-      <vuestic-dropdown-new title="Title" :message="dropdownText">
-        <button slot="actuator">Dropdown with Title</button>
-      </vuestic-dropdown-new>
-    </VbContainer>
-    <VbContainer title="Absolute">
-      <vuestic-dropdown-new absolute>
-        {{ dropdownText }}
-        <button slot="actuator">Open</button>
-      </vuestic-dropdown-new>
-    </VbContainer> -->
-
     <VbContainer title="Max Width">
       <vuestic-dropdown-new max-width="150px">
         {{ dropdownText }}
@@ -39,17 +27,6 @@
       </vuestic-dropdown-new>
     </VbContainer>
 
-    <!-- <VbContainer title="Dropdown activation">
-      <button>Just button</button>
-      <button @click="toggleNewDropdown()">External actuator</button>
-      <vuestic-dropdown-new
-        v-model="valueProxy"
-      >
-        With model
-        <button slot="actuator">Actuator</button>
-      </vuestic-dropdown-new>
-    </VbContainer> -->
-
     <VbContainer title="Nested Dropdown">
       <vuestic-dropdown-new>
         <button slot="actuator">Open</button>
@@ -58,6 +35,14 @@
           <vuestic-dropdown-new>
             {{ dropdownText }}
             <button slot="actuator">Open child</button>
+          </vuestic-dropdown-new>
+          <vuestic-dropdown-new>
+            <button slot="actuator">Open child</button>
+            {{ dropdownText }}
+            <vuestic-dropdown-new>
+              {{ dropdownText }}
+              <button slot="actuator">Open child</button>
+            </vuestic-dropdown-new>
           </vuestic-dropdown-new>
         </div>
       </vuestic-dropdown-new>
@@ -73,35 +58,6 @@
         <button slot="actuator">Focus</button>
       </vuestic-dropdown-new>
     </VbContainer>
-
-    <VbContainer title="Content events">
-      <vuestic-dropdown-new>
-        <span
-          @click="handleClick"
-          @mouseenter="handleMouseOver"
-          @mouseleave="handleMouseLeave"
-        >Event Listeners</span>
-        <button slot="actuator">Open</button>
-      </vuestic-dropdown-new>
-    </VbContainer>
-
-    <!-- <div class="demo-container__item">
-      <div class="demo-container__item-title">Dropdown with model</div>
-      <button id="open-dropdown" @click="toggleNewDropdown" ref="btn">Dropdown</button>
-      <vuestic-dropdown-new
-        v-model="showDropdown"
-        position="B"
-        :absolute="true"
-        max-width="200px"
-        :max-height="300"
-        @onClick="handleClick"
-        @onMouseEnter="handleMouseOver"
-        @onMouseLeave="handleMouseLeave"
-        target="open-dropdown"
-      >
-        <span>With model</span>
-      </vuestic-dropdown-new>
-    </div>-->
   </VbDemo>
 </template>
 
@@ -128,32 +84,5 @@ export default {
       dropdownText: 'Dropdown text. Dropdown text. Dropdown text. Dropdown text. Dropdown text. Dropdown text. Dropdown text. Dropdown text. Dropdown text. Dropdown text. Dropdown text. ',
     }
   },
-  methods: {
-    toggleNewDropdown () {
-      this.showDropdown = !this.showDropdown
-    },
-    handleClick (e) {
-      // eslint-disable-next-line no-console
-      console.log('click ', e)
-    },
-    handleMouseOver (e) {
-      // eslint-disable-next-line no-console
-      console.log('mouseover ', e)
-    },
-    handleMouseLeave (e) {
-      // eslint-disable-next-line no-console
-      console.log('mouseleave ', e)
-    }
-  },
-  computed: {
-    valueProxy: {
-      get () {
-        return this.showDropdown
-      },
-      set (value) {
-        // this.showDropdown = value
-      },
-    }
-  }
 }
 </script>
