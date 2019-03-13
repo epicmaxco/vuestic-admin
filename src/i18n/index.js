593a413b3a3a54bb1de6d4c6ef84862f79d954e7
Alexander Boriskin <alexander.boriskin12@gmail.com>
2019-03-13 11:56:48 +0300

    feat(chinese-lang): change default settings

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -7,7 +7,7 @@ Vue.i18n.add('es', require('./es.json'))
 Vue.i18n.add('cn', require('./cn.json'))
 
 // set the start locale to use
-Vue.i18n.set('cn')
+Vue.i18n.set('en')
 
 // set fallback for non-translated strings
 Vue.i18n.fallback('en')
