915d48cec5149118be18ec5a87655dce2cd98995
nastassiadanilova <vasiletsnastassia@gmail.com>
2019-04-01 20:31:29 +0300

    Feat/VaCountBadge (#519)
    * feat: VaNumberIcon
    
    * fix: checkbox style
    
    * fix: install approximate-number
    
    * feat: font size according to displayNumber length
    
    * chore: minor things
    
    * minor

--- src/vuestic-theme/vuestic-plugin.js
+++ src/vuestic-theme/vuestic-plugin.js
@@ -70,6 +70,7 @@ import VaTimelineItem
 import VaPopup
   from './vuestic-components/va-popup/quasar/components/popup/VaPopup.vue'
 
+import VaCountBadge from './vuestic-components/va-count-badge/VaCountBadge'
 import VaCard from './vuestic-components/va-card/VaCard'
 import { installQuasarPlatform } from './vuestic-components/va-popup/quasar/install'
 
@@ -124,6 +125,7 @@ const VuesticPlugin = {
       VaTimelineItem,
       VaPopup,
       VaCard,
+      VaCountBadge,
     ].forEach(component => {
       Vue.component(component.name, component)
     })
