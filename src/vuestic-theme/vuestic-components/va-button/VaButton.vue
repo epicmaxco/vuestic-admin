adc55256677d0517498670e07f75dca5b9315377
Matthew Mamonov aka smellyshovel <g.smellyshovel@gmail.com>
2019-07-15 11:47:15 +0300

    revert changes

--- src/vuestic-theme/vuestic-components/va-button/VaButton.vue
+++ src/vuestic-theme/vuestic-components/va-button/VaButton.vue
@@ -232,10 +232,6 @@ export default {
 <style lang='scss'>
 @import "../../vuestic-sass/resources/resources";
 
-a.va-button:hover {
-  color: white !important;
-}
-
 .va-button {
   display: inline-block;
   margin: $btn-margin;
