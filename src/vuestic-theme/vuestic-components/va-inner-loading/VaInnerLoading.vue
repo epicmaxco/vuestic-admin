e586f8e74dcf17262228e76c8744dccdf410fb0e
jean-moldovan <jean.moldovan@gmail.com>
2019-07-16 17:57:21 +0300

    feat(inner-loading): add inner loading component

---
+++ src/vuestic-theme/vuestic-components/va-inner-loading/VaInnerLoading.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="inner-loading-container">
+      <slot />
+
+      <div v-if="loading" class="inner-loading-overlay">
+        <spring-spinner
+          :animation-duration="2000"
+          :size="48"
+          color="#4ae387"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { SpringSpinner } from 'epic-spinners'
+
+export default {
+  name: 'va-inner-loading',
+  components: {
+    SpringSpinner,
+  },
+  props: {
+    loading: Boolean,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .inner-loading-container {
+    position: relative;
+  }
+
+  .inner-loading-overlay {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    background: rgba(255, 255, 255, 0.5);
+  }
+</style>
