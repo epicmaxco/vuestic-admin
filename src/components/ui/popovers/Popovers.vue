65feb6d0f4f1e6563dd7ad71ce68dbb0bff336a3
nastassiadanilova <nastassia.danilova@gmail.com>
2019-07-23 13:27:50 +0300

    fix: replace old selects on popovers page

--- src/components/ui/popovers/Popovers.vue
+++ src/components/ui/popovers/Popovers.vue
@@ -3,41 +3,41 @@
     <div class="va-row">
       <div class="flex xs12 md6">
         <va-card :title="$t('popovers.popoverStyle')">
-          <va-simple-select
-            class="mt-4"
+          <va-select
+            class="mb-4"
             label="color scheme"
             v-model="popover.color"
             :options="colors"
           />
-          <va-simple-select
-            class="mt-4"
+          <va-select
+            class="mb-4"
             label="icon (font-awesome)"
             v-model="popover.icon"
             :options="icons"
+            key-by="icon"
           />
           <va-input
+            class="mb-4"
             v-model="popover.title"
             label="Title"
           />
           <va-input
-            class="pt-4"
+            class="mb-4"
             v-model="popover.message"
             label="Message"
           />
-          <div class="va-row popover-example mt-5">
-            <va-popover
-              :icon="popover.icon"
-              :color="popover.color"
-              :title="popover.title"
-              :message="popover.message"
-              placement="right"
-              open
-            >
-              <va-button>
-                {{ $t('notificationsPage.popovers.showPopover') }}
-              </va-button>
-            </va-popover>
-          </div>
+          <va-popover
+            :icon="popover.icon.icon"
+            :color="popover.color"
+            :title="popover.title"
+            :message="popover.message"
+            placement="right"
+            open
+          >
+            <va-button>
+              {{ $t('notificationsPage.popovers.showPopover') }}
+            </va-button>
+          </va-popover>
         </va-card>
       </div>
 
@@ -96,11 +96,16 @@ export default {
   data () {
     return {
       icons: [
-        'fa fa-print', 'fa fa-star',
+        {
+          icon: 'fa fa-print',
+          text: 'print',
+        },
+        {
+          icon: 'fa fa-star',
+          text: 'star',
+        },
       ],
-      colors: [
-        'success', 'info', 'danger', 'warning', 'gray', 'dark',
-      ],
+      colors: ['success', 'info', 'danger', 'warning', 'gray', 'dark'],
       popover: {
         title: 'Hey folks!',
         message: 'This tooltip is amazing:D',
@@ -117,8 +122,5 @@ export default {
   &__content {
     @include va-flex-center();
   }
-}
-.popover-example {
-  min-height: 100px;
 }
 </style>
