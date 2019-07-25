621c412b51c9c80c09449acad7ec8f492aed4142
jean-moldovan <jean.moldovan@gmail.com>
2019-07-25 10:33:27 +0200

    feat(data-table): add server-side pagination to demos

--- src/vuestic-theme/vuestic-components/va-table/VaTable.demo.vue
+++ src/vuestic-theme/vuestic-components/va-table/VaTable.demo.vue
@@ -1,17 +1,23 @@
 <template>
   <vb-demo>
     <vb-card title="Static Data" width="100%">
-      <va-table-static-example />
+      <va-table-static-data-example />
+    </vb-card>
+
+    <vb-card title="Server-side Pagination" width="100%">
+      <va-table-api-mode-example />
     </vb-card>
   </vb-demo>
 </template>
 
 <script>
-import VaTableStaticExample from './VaTableStaticExample.vue'
+import VaTableStaticDataExample from './VaTableStaticDataExample.vue'
+import VaTableApiModeExample from './VaTableApiModeExample.vue'
 
 export default {
   components: {
-    VaTableStaticExample,
+    VaTableStaticDataExample,
+    VaTableApiModeExample,
   },
 }
 </script>
