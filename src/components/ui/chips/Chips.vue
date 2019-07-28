015ced074416c1a82e5becfc40151423f6a74a1f
Matthew Mamonov aka smellyshovel <g.smellyshovel@gmail.com>
2019-07-28 22:02:24 +0300

    fix: bug №30 and align badges in theme settings dropdown

--- src/components/ui/chips/Chips.vue
+++ src/components/ui/chips/Chips.vue
@@ -1,101 +1,67 @@
 <template>
-  <div class="tags">
-    <div class="row">
-      <div class="flex xs12">
-        <va-card :title="$t('chips.chips')">
-          <div class="row mb-3">
-            <va-chip>
-              Success chip
-            </va-chip>
-            <va-chip color="info">
-              Info chip
-            </va-chip>
-            <va-chip color="danger">
-              Danger chip
-            </va-chip>
-            <va-chip color="warning">
-              Warning chip
-            </va-chip>
-            <va-chip color="gray">
-              Gray chip
-            </va-chip>
-            <va-chip color="dark">
-              Dark chip
-            </va-chip>
+<div class="row">
+  <div class="flex xs12">
+    <va-card :title="$t('chips.chips')">
+      <div class="row">
+        <div class="flex xs12">
+          <div class="row mb-2">
+            <va-chip class="ma-2" color="primary">Primary chip</va-chip>
+            <va-chip class="ma-2" color="secondary">Secondary chip</va-chip>
+            <va-chip class="ma-2" color="success">Success chip</va-chip>
+            <va-chip class="ma-2" color="info">Info chip</va-chip>
+            <va-chip class="ma-2" color="danger">Danger chip</va-chip>
+            <va-chip class="ma-2" color="warning">Warning chip</va-chip>
+            <va-chip class="ma-2" color="gray">Gray chip</va-chip>
+            <va-chip class="ma-2" color="dark">Dark chip</va-chip>
           </div>
           <div class="row">
-            <va-chip outline>
-              Success chip
-            </va-chip>
-            <va-chip outline color="info">
-              Info chip
-            </va-chip>
-            <va-chip outline color="danger">
-              Danger chip
-            </va-chip>
-            <va-chip outline color="warning">
-              Warning chip
-            </va-chip>
-            <va-chip outline color="gray">
-              Gray chip
-            </va-chip>
-            <va-chip outline color="dark">
-              Dark chip
-            </va-chip>
+            <va-chip class="ma-2" color="primary" outline>Primary chip</va-chip>
+            <va-chip class="ma-2" color="secondary" outline>Secondary chip</va-chip>
+            <va-chip class="ma-2" color="success" outline>Success chip</va-chip>
+            <va-chip class="ma-2" color="info" outline>Info chip</va-chip>
+            <va-chip class="ma-2" color="danger" outline>Danger chip</va-chip>
+            <va-chip class="ma-2" color="warning" outline>Warning chip</va-chip>
+            <va-chip class="ma-2" color="gray" outline>Gray chip</va-chip>
+            <va-chip class="ma-2" color="dark" outline>Dark chip</va-chip>
           </div>
-        </va-card>
+        </div>
       </div>
-
-      <div class="flex xs12">
-        <va-card :title="$t('chips.badges')">
-          <div class="row mb-3">
-            <va-badge>
-              Paid
-            </va-badge>
-            <va-badge color="info">
-              Info
-            </va-badge>
-            <va-badge color="danger">
-              Danger
-            </va-badge>
-            <va-badge color="warning">
-              On Hold
-            </va-badge>
-            <va-badge color="gray">
-              Processing
-            </va-badge>
-            <va-badge color="dark">
-              New Label
-            </va-badge>
-          </div>
-          <div class="row">
-            <va-badge outline>
-              Paid
-            </va-badge>
-            <va-badge outline color="info">
-              Info
-            </va-badge>
-            <va-badge outline color="danger">
-              Danger
-            </va-badge>
-            <va-badge outline color="warning">
-              On Hold
-            </va-badge>
-            <va-badge outline color="gray">
-              Processing
-            </va-badge>
-            <va-badge outline color="dark">
-              New Label
-            </va-badge>
-          </div>
-        </va-card>
-      </div>
-    </div>
+    </va-card>
   </div>
+
+  <div class="flex xs12">
+    <va-card :title="$t('chips.badges')">
+      <div class="row">
+        <div class="flex xs12">
+          <div class="row mb-2">
+            <va-badge class="ma-2" color="primary">Primary badge</va-badge>
+            <va-badge class="ma-2" color="secondary">Secondary badge</va-badge>
+            <va-badge class="ma-2" color="success">Success badge</va-badge>
+            <va-badge class="ma-2" color="info">Info badge</va-badge>
+            <va-badge class="ma-2" color="danger">Danger badge</va-badge>
+            <va-badge class="ma-2" color="warning">Warning badge</va-badge>
+            <va-badge class="ma-2" color="gray">Gray badge</va-badge>
+            <va-badge class="ma-2" color="dark">Dark badge</va-badge>
+          </div>
+          <div class="row">
+            <va-badge class="ma-2" color="primary" outline>Primary badge</va-badge>
+            <va-badge class="ma-2" color="secondary" outline>Secondary badge</va-badge>
+            <va-badge class="ma-2" color="success" outline>Success badge</va-badge>
+            <va-badge class="ma-2" color="info" outline>Info badge</va-badge>
+            <va-badge class="ma-2" color="danger" outline>Danger badge</va-badge>
+            <va-badge class="ma-2" color="warning" outline>Warning badge</va-badge>
+            <va-badge class="ma-2" color="gray" outline>Gray badge</va-badge>
+            <va-badge class="ma-2" color="dark" outline>Dark badge</va-badge>
+          </div>
+        </div>
+      </div>
+    </va-card>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'tags',
+  name: 'chips',
 }
 </script>
