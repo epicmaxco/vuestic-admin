d2999ddf4aee0880bb125b36f08b09db1d6043c6
Asva <ontrew@gmail.com>
2019-07-29 14:48:29 +0300

    fix: bug №14

--- src/components/statistics/progress-bars/Widgets/CircleBars.vue
+++ src/components/statistics/progress-bars/Widgets/CircleBars.vue
@@ -1,17 +1,17 @@
 <template>
   <va-card
-    class="circle-bars progress-bar-widget"
+    class="circle-bars"
     :title="$t('progressBars.circle')"
   >
-    <div class="row justify--space-between">
-      <div v-for="n in 10" :key="n" class="flex xs3 sm2 lg1">
+    <div class="row">
+      <div v-for="n in 10" :key="n" class="flex xs4 sm2 lg1">
         <div class="d-flex justify--center">
           <div>
             <va-progress-circle :value="value * n / 10"/>
           </div>
         </div>
       </div>
-      <div class="flex xs3 sm2 lg1">
+      <div class="flex xs4 sm2 lg1">
         <div class="d-flex justify--center">
           <div>
             <va-progress-circle indeterminate/>
@@ -20,15 +20,15 @@
       </div>
     </div>
 
-    <div class="row justify--space-between">
-      <div v-for="n in 10" :key="n" class="flex xs3 sm2 lg1">
+    <div class="row">
+      <div v-for="n in 10" :key="n" class="flex xs4 sm2 lg1">
         <div class="d-flex justify--center">
           <div>
             <va-progress-circle :value="value * n / 10">{{ value * n / 10 }}%</va-progress-circle>
           </div>
         </div>
       </div>
-      <div class="flex xs3 sm2 lg1">
+      <div class="flex xs4 sm2 lg1">
         <div class="d-flex justify--center">
           <div>
             <va-progress-circle indeterminate/>
@@ -41,6 +41,7 @@
 
 <script>
 export default {
+  name: 'circle-bars',
   data () {
     return {
       value: 0,
