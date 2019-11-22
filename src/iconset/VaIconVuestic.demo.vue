731d479431880572bed2a1c80b4ec15401ca1bf2
Kuchuk Andrey <kreezag@gmail.com>
2019-11-22 19:36:04 +0300

    add iconset iconst to app

---
+++ src/iconset/VaIconVuestic.demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="demo-container">
+    <div class="demo-container__item">
+      <va-icon-vuestic />
+    </div>
+  </div>
+</template>
+
+<script>
+import VaIconVuestic from './VaIconVuestic.vue'
+
+export default {
+  components: {
+    VaIconVuestic,
+  },
+}
+</script>
