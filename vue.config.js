2af97e2f1812015d6da4fa438dc9a0ae4a435b9b
Kuchuk Andrey <kreezag@gmail.com>
2019-11-25 13:16:43 +0300

    fix vue-config.remove unused dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,68 +1,52 @@
 const path = require('path')
 const StylelintPlugin = require('stylelint-webpack-plugin')
 
-let config
-
-if (process.env.VUE_APP_BOOK) {
-  config = {
-    lintOnSave: false,
-    transpileDependencies: ['vuestic-ui'],
-    pages: {
-      index: {
-        entry: 'src/vue-book/book-main.js',
-        template: 'public/index.html',
+module.exports = {
+  lintOnSave: false,
+  transpileDependencies: [
+    'vuestic-ui',
+    'epic-spinners',
+  ],
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/app/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Vuestic Admin',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+        '@': path.resolve('src'),
       },
     },
-  }
+    plugins: [new StylelintPlugin({
+      files: ['src/**/*.{vue,htm,html,css,sss,less,scss}'],
+    })],
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        // Preload vuestic-ui variables and mixins for every component
+        data: `@import "~vuestic-ui/src/components/vuestic-sass/resources/resources.scss";`,
+      },
+    },
+  },
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: './src/service-worker.js',
+      importWorkboxFrom: 'local',
+    },
+  },
 }
-
-if (!process.env.VUE_APP_BOOK) {
-  config = {
-    lintOnSave: false,
-    transpileDependencies: ['vuestic-ui'],
-    pages: {
-      index: {
-        // entry for the page
-        entry: 'src/app/main.js',
-        // the source template
-        template: 'public/index.html',
-        // output as dist/index.html
-        filename: 'index.html',
-        // when using title option,
-        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-        title: 'Vuestic Admin',
-        // chunks to include on this page, by default includes
-        // extracted common chunks and vendor chunks.
-        chunks: ['chunk-vendors', 'chunk-common', 'index'],
-      },
-    },
-    configureWebpack: {
-      resolve: {
-        alias: {
-          'vue$': 'vue/dist/vue.esm.js',
-          '@': path.resolve('src'),
-        },
-      },
-      plugins: [new StylelintPlugin({
-        files: ['src/**/*.{vue,htm,html,css,sss,less,scss}'],
-      })],
-    },
-    css: {
-      loaderOptions: {
-        sass: {
-          // Preload vuestic-ui variables and mixins for every component
-          data: `@import "~vuestic-ui/src/components/vuestic-sass/resources/resources.scss";`,
-        },
-      },
-    },
-    pwa: {
-      workboxPluginMode: 'InjectManifest',
-      workboxOptions: {
-        swSrc: './src/service-worker.js',
-        importWorkboxFrom: 'local',
-      },
-    },
-  }
-}
-
-module.exports = config
