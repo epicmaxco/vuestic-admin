c558234557a1ac1166f1f55d8fa6fcb5f8768533
Kuchuk Andrey <kreezag@gmail.com>
2019-11-26 12:04:16 +0300

    add va-navbar component from vuestic-ui to app

---
+++ src/components/admin/app-navbar/components/va-navbar/VaNavbar.demo.vue
@@ -0,0 +1,15 @@
+<template>
+  <VbDemo>
+    <VbCard title="Position" width="100%">
+      <VaNavbar />
+    </VbCard>
+  </VbDemo>
+</template>
+
+<script>
+import VaNavbar from './VaNavbar'
+
+export default {
+  components: { VaNavbar },
+}
+</script>
