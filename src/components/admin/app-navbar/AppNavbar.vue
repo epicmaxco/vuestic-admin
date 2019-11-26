b51e84384461f96d935128d9fcb63be3c4a6b34b
Kuchuk Andrey <kreezag@gmail.com>
2019-11-26 18:34:23 +0300

    remove unused styles

--- src/components/admin/app-navbar/AppNavbar.vue
+++ src/components/admin/app-navbar/AppNavbar.vue
@@ -9,36 +9,32 @@
 
     <div class="app-navbar__content row">
       <router-link
-        class="app-navbar__logo align--center mr-3"
+        class="app-navbar__logo mr-3"
         to="/"
       >
         <va-icon-vuestic/>
       </router-link>
-      <div class="app-navbar__center flex lg5 md4 flex-center">
-        <div class="row flex-nowrap align--center justify--space-between">
-          <span class="app-navbar__text no-wrap">
-            {{$t('navbar.messageUs')}}&nbsp;
-            <a
-              href="mailto:hello@epicmax.co"
-              target="_blank"
-              class="app-navbar__mailto-link"
-              :style="{color: this.$themes.primary}"
-            >
-              hello@epicmax.co
-            </a>
-          </span>
-          <div>
-            <va-button
-              href="https://github.com/epicmaxco/vuestic-admin"
-              color="#000000"
-              class="app-navbar__button text--bold"
-              icon="fa fa-github"
-              target="_blank"
-            >
-              {{$t('navbar.support')}}
-            </va-button>
-          </div>
-        </div>
+      <div class="app-navbar__center lg5 md4">
+        <span class="app-navbar__text">
+          {{$t('navbar.messageUs')}}&nbsp;
+          <a
+            href="mailto:hello@epicmax.co"
+            target="_blank"
+            class="app-navbar__mailto-link"
+            :style="{color: this.$themes.primary}"
+          >
+            hello@epicmax.co
+          </a>
+        </span>
+        <va-button
+          href="https://github.com/epicmaxco/vuestic-admin"
+          color="#000000"
+          class="app-navbar__button"
+          icon="fa fa-github"
+          target="_blank"
+        >
+          {{$t('navbar.support')}}
+        </va-button>
       </div>
       <app-navbar-actions
         class="app-navbar__actions md5 lg4"
@@ -125,7 +121,6 @@ export default {
 
 <style lang="scss">
 $top-nav-height: 4.0625rem;
-$nav-mobile-brand-top: 1.5rem;
 $nav-border-side-width: 3.1875rem;
 $lighter-gray: #dddddd;
 $font-size-base: 1rem !default;
@@ -145,7 +140,10 @@ $font-size-base: 1rem !default;
   }
 
   &__center {
+    display: flex;
     margin-left: 3rem;
+    justify-content: space-between;
+    align-items: center;
   }
 
   &__text {
@@ -154,7 +152,8 @@ $font-size-base: 1rem !default;
 
   &__button {
     width: 10.8125rem;
-    margin: 0 !important;
+    margin: 0 0 0 1rem !important;
+    font-weight: bold;
 
     .va-button__content__icon-left.fa-github {
       font-size: 1.5rem;
@@ -171,6 +170,7 @@ $font-size-base: 1rem !default;
     width: 9.5rem;
     margin-left: 2.5rem;
     height: auto;
+    align-items: center;
 
     & * {
       max-height: 100%;
