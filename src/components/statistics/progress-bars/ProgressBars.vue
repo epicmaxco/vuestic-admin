b86f04b053aaf8b1022f0b7e053d3c1a1b7a41ff
AlexanderBoriskin <alexander.boriskin12@gmail.com>
2019-12-02 20:29:47 +0300

    fix(bugs-2.0): change progress bar presentation

--- src/components/statistics/progress-bars/ProgressBars.vue
+++ src/components/statistics/progress-bars/ProgressBars.vue
@@ -7,6 +7,11 @@
     </div>
     <div class="row">
       <div class="flex xs12">
+        <bars-state/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="flex xs12">
         <circle-bars/>
       </div>
     </div>
@@ -21,10 +26,11 @@
 <script>
 import HorizontalBars from './Widgets/HorizontalBars'
 import CircleBars from './Widgets/CircleBars'
+import BarsState from './Widgets/BarsState'
 import ColorfulBars from './Widgets/ColorfulBars'
 
 export default {
   name: 'progress-bars',
-  components: { HorizontalBars, CircleBars, ColorfulBars },
+  components: { HorizontalBars, CircleBars, BarsState, ColorfulBars },
 }
 </script>
