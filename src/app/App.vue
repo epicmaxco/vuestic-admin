d49c66307b9ee73bdd9ffb49ae4c29d7f589be55
Pavel Antonenka <antonenko.pvl@gmail.com>
2020-01-31 20:11:37 +0300

    move corporate theme syles to main.scss

--- src/app/App.vue
+++ src/app/App.vue
@@ -28,57 +28,11 @@ export default {
 <style lang="scss">
 @import '../sass/main.scss';
 
-$vue-purple: #6c7fee;
-$purple-box-shadow: 0 4px 9.6px 0.4px rgba($vue-purple, 0.5);
-
 body {
   height: 100%;
 
   #app {
     height: 100%;
-
-    // Temporary colors fix for 2.1.
-    &.inverted-color {
-      ::selection {
-        background-color: $vue-purple;
-        color: $white;
-      }
-
-      .link {
-        color: $vue-purple;
-      }
-
-      .va-data-table {
-        th.sortable {
-          color: $vue-purple;
-        }
-      }
-
-      .va-dropdown {
-        .profile-dropdown,
-        .language-dropdown,
-        .message-dropdown,
-        .notification-dropdown {
-          &__item {
-            &.active,
-            &:hover {
-              color: $vue-purple;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  .toasted-container.inverted-color {
-    .toasted.vuestic-toast {
-      box-shadow: $purple-box-shadow;
-
-      i,
-      i.fa {
-        color: $vue-purple;
-      }
-    }
   }
 }
 </style>
