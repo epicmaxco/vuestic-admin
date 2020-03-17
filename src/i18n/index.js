f65f96aba67ca3046e5cefcf24bf26aaf1243b18
sh4rifi <33723501+sh4rifi@users.noreply.github.com>
2020-03-17 13:59:54 +0330

    add persian language (#743)

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,6 +5,7 @@ Vue.i18n.add('br', require('./pt_br.json'))
 Vue.i18n.add('en', require('./en.json'))
 Vue.i18n.add('es', require('./es.json'))
 Vue.i18n.add('cn', require('./cn.json'))
+Vue.i18n.add('ir', require('./ir.json'))
 
 // set the start locale to use
 Vue.i18n.set('en')
