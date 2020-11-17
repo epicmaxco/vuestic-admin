7280a69cccb13066826b0ba31673c612118177f6
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-17 11:46:56 +0300

    minor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,35 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: [
-    '@vue/standard',
-    'plugin:vue/essential',
-  ],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
-    'curly': 'error',
+    curly: 'error',
     'vue/html-indent': ['error', 2, {
-      'attribute': 1,
-      'baseIndent': 1,
-      'closeBracket': 0,
-      'alignAttributesVertically': false,
-      'ignores': []
-    }]
+      attribute: 1,
+      baseIndent: 1,
+      closeBracket: 0,
+      alignAttributesVertically: false,
+      ignores: [],
+    }],
   },
+
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
   },
+
   globals: {
-    'workbox': 'readonly',
+    workbox: 'readonly',
   },
+
+  extends: [
+    '@vue/standard',
+    'plugin:vue/essential',
+    '@vue/typescript',
+  ],
 }
