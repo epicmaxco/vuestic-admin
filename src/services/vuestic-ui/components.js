255b63f4411f98089082b4d730ab548a7bbaf5e4
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-17 11:45:16 +0300

    fix: packages

--- src/services/vuestic-ui/components.js
+++ src/services/vuestic-ui/components.js
@@ -1,9 +1,9 @@
 import VuesticPlugin from 'vuestic-ui/src/components/vuestic-plugin'
-// import StickyScroll from 'vuestic-ui/src/components/vuestic-directives/StickyScroll'
-// import { colorArray } from 'vuestic-ui/src/components/vuestic-components/va-color-picker/VuesticTheme'
+import StickyScroll from 'vuestic-ui/src/components/vuestic-directives/StickyScroll'
+import { colorArray } from 'vuestic-ui/src/components/vuestic-components/va-color-picker/VuesticTheme'
 // import VuesticPlugin from 'vuestic-ui-next/src/components/vuestic-plugin'
-import StickyScroll from 'vuestic-ui-next/src/components/vuestic-directives/StickyScroll'
-import { colorArray } from 'vuestic-ui-next/src/components/vuestic-components/va-color-picker/VuesticTheme.ts'
+// import StickyScroll from 'vuestic-ui-next/src/components/vuestic-directives/StickyScroll'
+// import { colorArray } from 'vuestic-ui-next/src/components/vuestic-components/va-color-picker/VuesticTheme.ts'
 
 export {
   VuesticPlugin,
