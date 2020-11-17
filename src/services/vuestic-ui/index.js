63badf700e4a5c76dfe00d2bf80d4a859f95c435
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-17 12:14:41 +0300

    add: some new functions

--- src/services/vuestic-ui/index.js
+++ src/services/vuestic-ui/index.js
@@ -1,7 +1,9 @@
+// import { ColorThemeActionsMixin } from 'vuestic-ui/src/services/ColorThemePlugin' // TODO
 import { ColorThemePlugin, ColorThemeMixin, ColorThemeActionsMixin } from 'vuestic-ui/src/services/ColorThemePlugin'
+// import { hex2rgb, hex2hsl } from 'vuestic-ui/src/services/color-functions' // TODO
 import { getHoverColor, getGradientBackground, hex2rgb, hex2hsl } from 'vuestic-ui/src/services/color-functions'
-// import { ColorThemePlugin, ColorThemeMixin, ColorThemeActionsMixin } from 'vuestic-ui-next/src/services/ColorThemePlugin.ts'
-// import { getHoverColor, getGradientBackground, hex2rgb, hex2hsl } from 'vuestic-ui-next/src/services/color-functions.ts'
+// import { ColorThemePlugin, ColorThemeMixin } from 'vuestic-ui-next/src/services/ColorThemePlugin.ts'
+// import { getHoverColor, getGradientBackground } from 'vuestic-ui-next/src/services/color-functions.ts'
 
 const colorShiftHsl = (main, offset = { h: 0, s: 0, l: 0 }) => {
   offset = {
