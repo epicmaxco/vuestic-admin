ab3a0be9f4c96533f97afcde38dd26708915be86
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-18 14:15:37 +0300

    add: new fileUploads

--- src/components/ui/file-upload/FileUpload.vue
+++ src/components/ui/file-upload/FileUpload.vue
@@ -2,45 +2,60 @@
   <div class="file-upload">
     <div class="row">
       <div class="flex xs12">
-        <va-card :title="$t('fileUpload.advancedMediaGallery')">
-          <va-file-upload
-            type="gallery"
-            file-types=".png, .jpg, .jpeg, .gif"
-            dropzone
-            v-model="advancedGallery"
-          />
+        <va-card>
+          <va-card-title>{{ $t('fileUpload.advancedMediaGallery') }}</va-card-title>
+          <va-card-content>
+            <va-file-upload
+              type="gallery"
+              file-types=".png, .jpg, .jpeg, .gif"
+              dropzone
+              v-model="advancedGallery"
+            />
+          </va-card-content>
         </va-card>
       </div>
       <div class="flex xs12">
-        <va-card :title="$t('fileUpload.advancedUploadList')">
-          <va-file-upload
-            dropzone
-            v-model="advancedList"
-          />
+        <va-card>
+          <va-card-title>{{ $t('fileUpload.advancedUploadList') }}</va-card-title>
+          <va-card-content>
+            <va-file-upload
+              dropzone
+              v-model="advancedList"
+            />
+          </va-card-content>
         </va-card>
       </div>
       <div class="flex xs12">
-        <va-card :title="$t('fileUpload.single')">
-          <va-file-upload
-            type="single"
-            v-model="single"
-          />
+        <va-card>
+          <va-card-title>{{ $t('fileUpload.single') }}</va-card-title>
+          <va-card-content>
+            <va-file-upload
+              type="single"
+              v-model="single"
+            />
+          </va-card-content>
         </va-card>
       </div>
       <div class="flex xs12">
-        <va-card :title="$t('fileUpload.mediaGallery')">
-          <va-file-upload
-            type="gallery"
-            file-types=".png, .jpg, .jpeg, .gif"
-            v-model="gallery"
-          />
+        <va-card>
+          <va-card-title>{{ $t('fileUpload.mediaGallery') }}</va-card-title>
+          <va-card-content>
+            <va-file-upload
+              type="gallery"
+              file-types=".png, .jpg, .jpeg, .gif"
+              v-model="gallery"
+            />
+          </va-card-content>
         </va-card>
       </div>
       <div class="flex xs12">
-        <va-card :title="$t('fileUpload.uploadList')">
-          <va-file-upload
-            v-model="list"
-          />
+        <va-card>
+          <va-card-title>{{ $t('fileUpload.uploadList') }}</va-card-title>
+          <va-card-content>
+            <va-file-upload
+              v-model="list"
+            />
+          </va-card-content>
         </va-card>
       </div>
     </div>
