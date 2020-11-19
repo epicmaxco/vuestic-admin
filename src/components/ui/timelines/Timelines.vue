b6333d4a925b7bea68969ca014779328a13f2e67
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-19 11:59:29 +0300

    add: new timelines

--- src/components/ui/timelines/Timelines.vue
+++ src/components/ui/timelines/Timelines.vue
@@ -5,46 +5,48 @@
         <va-card
           no-padding-h
           style="overflow-x: auto;"
-          :title="$t('timelines.horizontalSimple')"
         >
-          <va-timeline style="min-width: 400px;">
-            <va-timeline-item active>
-              <template slot="before">
-                <div
-                  class="title text--center"
-                  :style="{color: $themes.primary}"
-                >
-                  February 2018
-                </div>
-                <div class="va-timeline-item__description">
-                  Complete drafts
-                </div>
-              </template>
-            </va-timeline-item>
-            <va-timeline-item active>
-              <template slot="before">
-                <div
-                  class="title text--center"
-                  :style="{color: $themes.primary}"
-                >
-                  April 2018
-                </div>
-                <div class="va-timeline-item__description">
-                  Push site live
-                </div>
-              </template>
-            </va-timeline-item>
-            <va-timeline-item>
-              <template slot="before">
-                <div class="title title--gray text--center">
-                  June 2018
-                </div>
-                <div class="va-timeline-item__description">
-                  Start ICO
-                </div>
-              </template>
-            </va-timeline-item>
-          </va-timeline>
+          <va-card-title>{{ $t('timelines.horizontalSimple') }}</va-card-title>
+          <va-card-content>
+            <va-timeline style="min-width: 400px;">
+              <va-timeline-item active>
+                <template slot="before">
+                  <div
+                    class="title text--center"
+                    :style="{color: $themes.primary}"
+                  >
+                    February 2018
+                  </div>
+                  <div class="va-timeline-item__description">
+                    Complete drafts
+                  </div>
+                </template>
+              </va-timeline-item>
+              <va-timeline-item active>
+                <template slot="before">
+                  <div
+                    class="title text--center"
+                    :style="{color: $themes.primary}"
+                  >
+                    April 2018
+                  </div>
+                  <div class="va-timeline-item__description">
+                    Push site live
+                  </div>
+                </template>
+              </va-timeline-item>
+              <va-timeline-item>
+                <template slot="before">
+                  <div class="title title--gray text--center">
+                    June 2018
+                  </div>
+                  <div class="va-timeline-item__description">
+                    Start ICO
+                  </div>
+                </template>
+              </va-timeline-item>
+            </va-timeline>
+          </va-card-content>
         </va-card>
       </div>
 
@@ -53,171 +55,188 @@
           no-padding-h
           style="overflow-x: auto;"
           class="timelines__horizontal-long"
-          :title="$t('timelines.horizontalCards')"
         >
-          <va-timeline align-top style="min-width: 600px;" class="timelines__horizontal-long__timeline">
-            <va-timeline-item active>
-              <template slot="before">
-                <div
-                  class="title text--center"
+          <va-card-title>{{ $t('timelines.horizontalCards') }}</va-card-title>
+          <va-card-content>
+            <va-timeline align-top style="min-width: 600px;" class="timelines__horizontal-long__timeline">
+              <va-timeline-item active>
+                <template slot="before">
+                  <div
+                    class="title text--center"
+                    :style="{color: $themes.primary}"
+                  >
+                    February 2018
+                  </div>
+                </template>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="warning"
+                  class="mb-0"
+                >
+                  <va-card-title>{{ dateFirst }}</va-card-title>
+                  <va-card-content>{{ contentFirst }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+              <va-timeline-item active>
+                <template slot="before">
+                  <div
+                    class="title text--center"
+                    :style="{color: $themes.primary}"
+                  >
+                    April 2018
+                  </div>
+                </template>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="info"
+                  class="mb-0"
+                >
+                  <va-card-title>{{ dateSecond }}</va-card-title>
+                  <va-card-content>{{ contentFirst }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+              <va-timeline-item>
+                <template slot="before">
+                  <div class="title title--gray text--center">
+                    June 2018
+                  </div>
+                </template>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="info"
+                  class="mb-0"
+                >
+                  <va-card-title>{{ dateThird }}</va-card-title>
+                  <va-card-content>{{ contentThird }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+            </va-timeline>
+          </va-card-content>
+        </va-card>
+      </div>
+
+      <div class="flex xs12">
+        <va-card no-padding-v>
+          <va-card-title>{{ $t('timelines.verticalLabel') }}</va-card-title>
+          <va-card-content>
+            <va-timeline vertical>
+              <va-timeline-item active>
+                <span
+                  slot="before"
+                  class="title va-timeline-item__text"
                   :style="{color: $themes.primary}"
+                >
+                  Feb 2018
+                </span>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="success"
+                  class="mb-0"
+                >
+                  <va-card-title>{{ titleFirst }}</va-card-title>
+                  <va-card-content>{{ contentFirst }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+              <va-timeline-item active>
+                <span
+                  slot="before"
+                  class="title va-timeline-item__text"
+                  :style="{color: $themes.primary}"
+                >
+                  Apr 2018
+                </span>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="success"
+                  class="mb-0"
+                >
+                  <va-card-title>{{ titleSecond }}</va-card-title>
+                  <va-card-content>{{ contentFirst }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+              <va-timeline-item>
+                <span class="title title--gray va-timeline-item__text" slot="before">
+                  Jun 2018
+                </span>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="success"
+                >
+                  <va-card-title>{{ titleThird }}</va-card-title>
+                  <va-card-content>{{ contentFirst }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+            </va-timeline>
+          </va-card-content>
+        </va-card>
+      </div>
+
+      <div class="flex xs12">
+        <va-card no-padding-v>
+          <va-card-title>{{ $t('timelines.verticalCentered') }}</va-card-title>
+          <va-card-content>
+            <va-timeline vertical centered style="min-width: 300px;">
+              <va-timeline-item color="danger" active>
+                <span
+                  slot="before"
+                  class="title title--danger va-timeline-item__text"
+                  :style="{color: $themes.danger}"
                 >
                   February 2018
-                </div>
-              </template>
-              <va-card
-                slot="after"
-                stripe="warning"
-                class="mb-0"
-              >
-                <template slot="title">{{ dateFirst }}</template>
-                {{ contentFirst }}
-              </va-card>
-            </va-timeline-item>
-            <va-timeline-item active>
-              <template slot="before">
-                <div
-                  class="title text--center"
-                  :style="{color: $themes.primary}"
+                </span>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="danger"
+                  class="mb-0"
+                >
+                  <va-card-title>{{ titleFirst }}</va-card-title>
+                  <va-card-content>{{ contentFirst }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+              <va-timeline-item color="danger" active>
+                <span
+                  slot="before"
+                  class="title title--danger va-timeline-item__text"
+                  :style="{color: $themes.danger}"
                 >
                   April 2018
-                </div>
-              </template>
-              <va-card
-                slot="after"
-                stripe="info"
-                class="mb-0"
-              >
-                <template slot="title">{{ dateSecond }}</template>
-                {{ contentFirst }}
-              </va-card>
-            </va-timeline-item>
-            <va-timeline-item>
-              <template slot="before">
-                <div class="title title--gray text--center">
+                </span>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="danger"
+                >
+                  <va-card-title>{{ titleSecond }}</va-card-title>
+                  <va-card-content>{{ contentFirst }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+              <va-timeline-item color="danger" active>
+                <span
+                  slot="before"
+                  class="title title--danger va-timeline-item__text"
+                  :style="{color: $themes.danger}"
+                >
                   June 2018
-                </div>
-              </template>
-              <va-card
-                slot="after"
-                stripe="info"
-                class="mb-0"
-              >
-                <template slot="title">{{ dateThird }}</template>
-                {{ contentThird }}
-              </va-card>
-            </va-timeline-item>
-          </va-timeline>
-        </va-card>
-      </div>
-
-      <div class="flex xs12">
-        <va-card no-padding-v :title="$t('timelines.verticalLabel')">
-          <va-timeline vertical>
-            <va-timeline-item active>
-              <span
-                slot="before"
-                class="title va-timeline-item__text"
-                :style="{color: $themes.primary}"
-              >
-                Feb 2018
-              </span>
-              <va-card
-                slot="after"
-                stripe="success"
-                class="mb-0"
-              >
-                <template slot="title">{{ titleFirst }}</template>
-                {{ contentFirst }}
-              </va-card>
-            </va-timeline-item>
-            <va-timeline-item active>
-              <span
-                slot="before"
-                class="title va-timeline-item__text"
-                :style="{color: $themes.primary}"
-              >
-                Apr 2018
-              </span>
-              <va-card
-                slot="after"
-                stripe="success"
-                class="mb-0"
-              >
-                <template slot="title">{{ titleSecond }}</template>
-                {{ contentFirst }}
-              </va-card>
-            </va-timeline-item>
-            <va-timeline-item>
-              <span class="title title--gray va-timeline-item__text" slot="before">
-                Jun 2018
-              </span>
-              <va-card
-                slot="after"
-                stripe="success"
-              >
-                <template slot="title">{{ titleSecond }}</template>
-                {{ contentFirst }}
-              </va-card>
-            </va-timeline-item>
-          </va-timeline>
-        </va-card>
-      </div>
-
-      <div class="flex xs12">
-        <va-card no-padding-v :title="$t('timelines.verticalCentered')">
-          <va-timeline vertical centered style="min-width: 300px;">
-            <va-timeline-item color="danger" active>
-              <span
-                slot="before"
-                class="title title--danger va-timeline-item__text"
-                :style="{color: $themes.danger}"
-              >
-                February 2018
-              </span>
-              <va-card
-                slot="after"
-                stripe="danger"
-                class="mb-0"
-              >
-                <template slot="title">{{ titleFirst }}</template>
-                {{ contentFirst }}
-              </va-card>
-            </va-timeline-item>
-            <va-timeline-item color="danger" active>
-              <span
-                slot="before"
-                class="title title--danger va-timeline-item__text"
-                :style="{color: $themes.danger}"
-              >
-                April 2018
-              </span>
-              <va-card
-                slot="after"
-                stripe="danger"
-              >
-                <template slot="title">{{ titleSecond }}</template>
-                {{ contentFirst }}
-              </va-card>
-            </va-timeline-item>
-            <va-timeline-item color="danger" active>
-              <span
-                slot="before"
-                class="title title--danger va-timeline-item__text"
-                :style="{color: $themes.danger}"
-              >
-                June 2018
-              </span>
-              <va-card
-                slot="after"
-                stripe="danger"
-                class="mb-0"
-              >
-                <template slot="title">{{ titleSecond }}</template>
-                {{ contentFirst }}
-              </va-card>
-            </va-timeline-item>
-          </va-timeline>
+                </span>
+                <va-card
+                  slot="after"
+                  stripe
+                  stripe-color="danger"
+                  class="mb-0"
+                >
+                  <va-card-title>{{ titleThird}}</va-card-title>
+                  <va-card-content>{{ contentFirst }}</va-card-content>
+                </va-card>
+              </va-timeline-item>
+            </va-timeline>
+          </va-card-content>
         </va-card>
       </div>
     </div>
