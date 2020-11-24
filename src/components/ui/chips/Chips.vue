541a828d5b145aba06769e414b2d0548f7b46f0a
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-24 13:26:07 +0300

    fix: minor

--- src/components/ui/chips/Chips.vue
+++ src/components/ui/chips/Chips.vue
@@ -8,14 +8,15 @@
           <div class="flex xs12">
             <div class="row">
               <div class="flex xs12">
-                <va-chip class="mb-2" color="primary">Primary chip</va-chip>
-                <va-chip class="mb-2" color="secondary">Secondary chip</va-chip>
-                <va-chip class="mb-2" color="success">Success chip</va-chip>
-                <va-chip class="mb-2" color="info">Info chip</va-chip>
-                <va-chip class="mb-2" color="danger">Danger chip</va-chip>
-                <va-chip class="mb-2" color="warning">Warning chip</va-chip>
-                <va-chip class="mb-2" color="gray">Gray chip</va-chip>
-                <va-chip class="mb-2" color="dark">Dark chip</va-chip>
+                <!-- TODO: use chip instead tag (latest build) -->
+                <va-tag class="mb-2 mr-2" color="primary">Primary chip</va-tag>
+                <va-tag class="mb-2 mr-2" color="secondary">Secondary chip</va-tag>
+                <va-tag class="mb-2 mr-2" color="success">Success chip</va-tag>
+                <va-tag class="mb-2 mr-2" color="info">Info chip</va-tag>
+                <va-tag class="mb-2 mr-2" color="danger">Danger chip</va-tag>
+                <va-tag class="mb-2 mr-2" color="warning">Warning chip</va-tag>
+                <va-tag class="mb-2 mr-2" color="gray">Gray chip</va-tag>
+                <va-tag class="mb-2 mr-2" color="dark">Dark chip</va-tag>
               </div>
             </div>
           </div>
@@ -30,26 +31,26 @@
           <div class="flex xs12">
             <div class="row">
               <div class="flex xs12">
-                <va-badge class="mb-2" color="primary">Primary badge</va-badge>
-                <va-badge class="mb-2" color="secondary">Secondary badge</va-badge>
-                <va-badge class="mb-2" color="success">Success badge</va-badge>
-                <va-badge class="mb-2" color="info">Info badge</va-badge>
-                <va-badge class="mb-2" color="danger">Danger badge</va-badge>
-                <va-badge class="mb-2" color="warning">Warning badge</va-badge>
-                <va-badge class="mb-2" color="gray">Gray badge</va-badge>
-                <va-badge class="mb-2" color="dark">Dark badge</va-badge>
+                <va-badge class="mb-2 mr-2" color="primary" text="Primary badge" />
+                <va-badge class="mb-2 mr-2" color="secondary" text="Secondary badge" />
+                <va-badge class="mb-2 mr-2" color="success" text="Success badge" />
+                <va-badge class="mb-2 mr-2" color="info" text="Info badge" />
+                <va-badge class="mb-2 mr-2" color="danger" text="Danger badge" />
+                <va-badge class="mb-2 mr-2" color="warning" text="Warning badge" />
+                <va-badge class="mb-2 mr-2" color="gray" text="Gray badge" />
+                <va-badge class="mb-2 mr-2" color="dark" text="Dark badge" />
               </div>
             </div>
             <div class="row">
               <div class="flex xs12">
-                <va-badge class="mb-2" color="primary" outline>Primary badge</va-badge>
-                <va-badge class="mb-2" color="secondary" outline>Secondary badge</va-badge>
-                <va-badge class="mb-2" color="success" outline>Success badge</va-badge>
-                <va-badge class="mb-2" color="info" outline>Info badge</va-badge>
-                <va-badge class="mb-2" color="danger" outline>Danger badge</va-badge>
-                <va-badge class="mb-2" color="warning" outline>Warning badge</va-badge>
-                <va-badge class="mb-2" color="gray" outline>Gray badge</va-badge>
-                <va-badge class="mb-2" color="dark" outline>Dark badge</va-badge>
+                <va-badge class="mb-2 mr-2" color="primary" text="Primary badge" transparent />
+                <va-badge class="mb-2 mr-2" color="secondary" text="Secondary badge" transparent />
+                <va-badge class="mb-2 mr-2" color="success" text="Success badge" transparent />
+                <va-badge class="mb-2 mr-2" color="info" text="Info badge" transparent />
+                <va-badge class="mb-2 mr-2" color="danger" text="Danger badge" transparent />
+                <va-badge class="mb-2 mr-2" color="warning" text="Warning badge" transparent />
+                <va-badge class="mb-2 mr-2" color="gray" text="Gray badge" transparent />
+                <va-badge class="mb-2 mr-2" color="dark" text="Dark badge" transparent />
               </div>
             </div>
           </div>
