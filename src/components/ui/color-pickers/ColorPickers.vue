d87563bc61f05bc9c3914c32e0ae465cddecf690
Dmitry Kushnerevich <kushich.dmitry@gmail.com>
2020-11-24 17:21:24 +0300

    fix: color-pickers cards

--- src/components/ui/color-pickers/ColorPickers.vue
+++ src/components/ui/color-pickers/ColorPickers.vue
@@ -1,9 +1,11 @@
+// TODO: fix color components
 <template>
   <div class="color-pickers vuestic-color-picker-page">
     <div class="row">
       <div class="flex md12 xs12">
-        <va-card :title="$t('menu.colorPickers')">
-          <div class="row">
+        <va-card>
+          <va-card-title>{{ $t('menu.colorPickers') }}</va-card-title>
+          <va-card-content class="row">
             <div class="flex xs4 md2">
               <div class="vuestic-color-picker-page__top-square">
                 <span
@@ -53,49 +55,52 @@
                 </va-color-picker-input>
               </div>
             </div>
-          </div>
+          </va-card-content>
         </va-card>
       </div>
     </div>
     <div class="row">
       <div class="flex md12 xs12">
-        <va-card title="Simple Inline">
-          <div class="row">
+        <va-card>
+          <va-card-title>Simple Inline</va-card-title>
+          <va-card-content class="row">
             <div class="flex md1">
               <va-color-square :value="simpleColor"/>
             </div>
             <div class="flex md2">
               <va-palette-custom :palette="palette" v-model="simpleColor"/>
             </div>
-          </div>
+          </va-card-content>
         </va-card>
       </div>
     </div>
     <div class="row">
       <div class="flex md12 xs12">
-        <va-card title="Slider">
-          <div class="row">
+        <va-card>
+          <va-card-title>Slider</va-card-title>
+          <va-card-content class="row">
             <div class="flex xs12 md1">
               <va-color-square :value="sliderColor"/>
             </div>
             <div class="flex md6 xs12">
               <va-slider-color-picker v-model="sliderColor"/>
             </div>
-          </div>
+          </va-card-content>
         </va-card>
       </div>
     </div>
     <div class="row">
       <div class="flex md12 xs12">
-        <va-card title="Advanced">
-          <div class="row">
+        <va-card>
+          <va-card-title>Advanced</va-card-title>
+          <va-card-content class="row">
             <div class="flex md1">
               <va-color-square :value="advancedColor"/>
             </div>
             <div class="flex md7">
               <va-advanced-color-picker v-model="advancedColor"/>
             </div>
-          </div>
+          </va-card-content>
         </va-card>
       </div>
     </div>
