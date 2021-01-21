62064249185cd26b4bf747121a5a43a05a607fdf
Dmitry <kushich.dmitry@gmail.com>
2021-01-21 15:59:34 +0300

    without clss-component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,13 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+<script>
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Options({
+export default {
+  name: 'Home',
   components: {
     HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+  }
+}
 </script>
