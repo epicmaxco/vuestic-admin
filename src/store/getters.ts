7455efc81a949032f6bec27c0c28a3d54e4c9a31
Dmitry <kushich.dmitry@gmail.com>
2021-01-21 11:17:20 +0300

    store

---
+++ src/store/getters.ts
@@ -0,0 +1,9 @@
+const config = (state: any) => state.app.config
+const palette = (state: any) => state.app.config.palette
+const isLoading = (state: any) => state.app.isLoading
+
+export {
+  config,
+  palette,
+  isLoading,
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
+// @ts-nocheck
 import { createStore } from 'vuex'
+import app from './modules/app'
+import * as getters from './getters'
 
 export default createStore({
+  strict: true, // process.env.NODE_ENV !== 'production',
+  getters,
   state: {
   },
   mutations: {
@@ -8,5 +13,6 @@ export default createStore({
   actions: {
   },
   modules: {
+    app,
   }
 })
