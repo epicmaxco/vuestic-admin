0c0287a926ce7f720ddf09da72beeeac48cd122e
m0ksem <m0ksem1337@gmail.com>
2021-02-22 18:58:49 +0200

    Comment broken import from vuestic 0.2.2.

--- src/services/vuestic-ui/index.ts
+++ src/services/vuestic-ui/index.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 // import { ColorThemeActionsMixin } from 'vuestic-ui/src/services/ColorThemePlugin' // TODO
-// // import { ColorThemeMixin, ColorThemeActionsMixin } from 'vuestic-ui/src/services/ColorThemePlugin'
+// import { ColorThemeMixin, ColorThemeActionsMixin } from 'vuestic-ui/src/services/ColorThemePlugin'
 // // import { getHoverColor, getGradientBackground, hex2rgb, hex2hsl } from 'vuestic-ui/src/services/color-functions'
 // // import { ColorThemePlugin, ColorThemeMixin } from 'vuestic-ui-next/src/services/ColorThemePlugin.ts'
 // import { ColorThemePlugin, ColorThemeMixin } from 'vuestic-ui/src/services/ColorThemePlugin'
@@ -14,7 +14,7 @@
 
 export {
 //   ColorThemePlugin,
-//   ColorThemeMixin,
+  // ColorThemeMixin,
 //   ColorThemeActionsMixin,
 //   getHoverColor,
 //   getGradientBackground,
