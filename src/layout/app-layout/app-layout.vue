24f426b048290ea64fd1f7c21b238b7a469cb4ea
m0ksem <m0ksem1337@gmail.com>
2021-02-22 18:55:16 +0200

    Move layout out of components.

---
+++ src/layout/app-layout/app-layout.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="app-layout">
+    <va-navbar class="app-layout__navbar">
+      test
+      <template v-slot:logo>
+        <vuestic-logo/>
+      </template>
+      <template v-slot:center>
+        <span
+          class="app-navbar__text"
+          style="color: #f3f3f3"
+        >
+          <!-- :style="{color: this.$themes.gray}" -->
+          {{$t('navbar.messageUs')}}&nbsp;
+          <a
+            href="mailto:hello@epicmax.co"
+            target="_blank"
+          >
+            <!-- :style="{color: this.$themes.primary}" -->
+            hello@epicmax.co
+          </a>
+        </span>
+      </template>
+    </va-navbar>
+    <div class="app-layout__content">
+      <sidebar/>
+      <router-view class="app-layout__page"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useTheme } from 'vuestic-ui'
+import VuesticLogo from './components/vuestic-logo.vue'
+import Sidebar from './components/sidebar.vue';
+
+export default {
+  name: 'app-layout',
+
+  components: {
+    VuesticLogo, Sidebar
+  },
+
+  computed: {
+    theme() {
+      const theme = useTheme()
+      return theme ? theme.getTheme() : null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  $navbar-height: 4rem;
+
+  .app-layout {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    &__content {
+      display: flex;
+      height: calc(100vh - 4rem);
+      .va-sidebar {
+        position: relative;
+        height: 100%;
+      }
+    }
+    &__page {
+      flex-grow: 2;
+      overflow-y: scroll;
+    }
+  }
+</style>
---
+++ src/layout/app-layout/components/sidebar.vue
@@ -0,0 +1,95 @@
+<template>
+  <va-sidebar class="app-sidebar__menu">
+    <template v-for="(item, key) in items">
+      <app-sidebar-link-group
+        v-if="item.children"
+        :key="key + 'category'"
+        :minimized="minimized"
+        :icon="item.meta && item.meta.iconClass"
+        :title="$t(item.displayName)"
+        :children="item.children"
+        :active-by-default="hasActiveByDefault(item)"
+      >
+        <app-sidebar-link
+          v-for="(subMenuItem, key) in item.children"
+          :key="key"
+          :to="{ name: subMenuItem.name }"
+          :title="$t(subMenuItem.displayName)"
+        />
+      </app-sidebar-link-group>
+      <app-sidebar-link
+        v-else
+        :key="key"
+        :minimized="minimized"
+        :active-by-default="item.name === $route.name"
+        :icon="item.meta && item.meta.iconClass"
+        :to="{ name: item.name }"
+        :title="$t(item.displayName)"
+      />
+    </template>
+  </va-sidebar>
+</template>
+
+<script>
+import AppSidebarLink from "./sidebar/AppSidebarLink";
+import AppSidebarLinkGroup from "./sidebar/AppSidebarLinkGroup";
+import NavigationRoutes from './sidebar/NavigationRoutes';
+
+export default {
+  name: "app-sidebar",
+  inject: ["contextConfig"],
+  components: {
+    AppSidebarLink,
+    AppSidebarLinkGroup
+  },
+  props: {
+    minimized: {
+      type: Boolean,
+      required: true
+    },
+    color: {
+      type: String,
+      default: "secondary"
+    }
+  },
+  data() {
+    return {
+      items: NavigationRoutes.routes
+    };
+  },
+  computed: {
+    computedClass() {
+      return {
+        "app-sidebar--minimized": this.minimized
+      };
+    }
+  },
+  methods: {
+    hasActiveByDefault(item) {
+      return item.children.some(child => child.name === this.$route.name);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.app-sidebar {
+  overflow: auto;
+  display: flex;
+  max-height: 100%;
+  flex: 0 0 16rem;
+
+  &--minimized {
+    flex: 0 0 3.25rem;
+  }
+
+  &__menu {
+    margin-bottom: 0;
+    padding-top: 2.5625rem;
+    padding-bottom: 2.5rem;
+    list-style: none;
+    padding-left: 0;
+    width: 100%;
+  }
+}
+</style>
