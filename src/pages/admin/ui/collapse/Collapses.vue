73a93f1ef00e10f21d876838382ad6682b9fdf35
m0ksem <m0ksem1337@gmail.com>
2021-03-04 18:43:02 +0200

    Fix collapses TODO.

--- src/pages/admin/ui/collapse/Collapses.vue
+++ src/pages/admin/ui/collapse/Collapses.vue
@@ -1,4 +1,3 @@
-// TODO: fix naming
 <template>
   <div class="collapse-page">
     <div class="row">
@@ -6,23 +5,24 @@
         <va-card>
           <va-card-title>{{ $t("collapse.basic") }}</va-card-title>
           <va-card-content>
-            <!-- TODO: use accordion instead expand group & collapse instead expand (latest build) -->
-            <va-collapse v-model="doShowCollapses[0]" class="mb-4" :header="$t('collapse.firstHeader')">
-              <div class="pa-3">
-                <p class="display-3">{{ $t("collapse.content.title") }}</p>
-                <div>
-                  {{ $t("collapse.content.text") }}
+            <va-accordion v-model="basicAccordionValue">
+              <va-collapse class="mb-4" :header="$t('collapse.firstHeader')">
+                <div class="pa-3">
+                  <p class="display-3">{{ $t("collapse.content.title") }}</p>
+                  <div>
+                    {{ $t("collapse.content.text") }}
+                  </div>
                 </div>
-              </div>
-            </va-collapse>
-            <va-collapse v-model="doShowCollapses[1]" :header="$t('collapse.secondHeader')">
-              <div class="pa-3">
-                <p class="display-3">{{ $t("collapse.content.title") }}</p>
-                <div>
-                  {{ $t("collapse.content.text") }}
+              </va-collapse>
+              <va-collapse :header="$t('collapse.secondHeader')">
+                <div class="pa-3">
+                  <p class="display-3">{{ $t("collapse.content.title") }}</p>
+                  <div>
+                    {{ $t("collapse.content.text") }}
+                  </div>
                 </div>
-              </div>
-            </va-collapse>
+              </va-collapse>
+            </va-accordion>
           </va-card-content>
         </va-card>
       </div>
@@ -31,31 +31,33 @@
         <va-card>
           <va-card-title>{{ $t("collapse.collapseWithBackground") }}</va-card-title>
           <va-card-content>
-            <va-collapse v-model="doShowCollapses[2]"
-              class="mb-4"
-              :header="$t('collapse.firstHeader')"
-              color="success"
-              color-all
-            >
-              <div class="pa-3">
-                <p class="display-3">{{ $t("collapse.content.title") }}</p>
-                <div>
-                  {{ $t("collapse.content.text") }}
+            <va-accordion v-model="colorAccordionValue">
+              <va-collapse
+                class="mb-4"
+                :header="$t('collapse.firstHeader')"
+                color="success"
+                color-all
+              >
+                <div class="pa-3">
+                  <p class="display-3">{{ $t("collapse.content.title") }}</p>
+                  <div>
+                    {{ $t("collapse.content.text") }}
+                  </div>
                 </div>
-              </div>
-            </va-collapse>
-            <va-collapse v-model="doShowCollapses[3]"
-              :header="$t('collapse.secondHeader')"
-              color="warning"
-              color-all
-            >
-              <div class="pa-3">
-                <p class="display-3">{{ $t("collapse.content.title") }}</p>
-                <div>
-                  {{ $t("collapse.content.text") }}
+              </va-collapse>
+              <va-collapse
+                :header="$t('collapse.secondHeader')"
+                color="warning"
+                color-all
+              >
+                <div class="pa-3">
+                  <p class="display-3">{{ $t("collapse.content.title") }}</p>
+                  <div>
+                    {{ $t("collapse.content.text") }}
+                  </div>
                 </div>
-              </div>
-            </va-collapse>
+              </va-collapse>
+            </va-accordion>
           </va-card-content>
         </va-card>
       </div>
@@ -64,32 +66,34 @@
         <va-card>
           <va-card-title>{{ $t("collapse.collapseWithCustomHeader") }}</va-card-title>
           <va-card-content>
-            <va-collapse v-model="doShowCollapses[4]" class="mb-4">
-              <template #header>
-                <va-button style="width: 100%;">
-                  {{ $t("collapse.firstHeader") }}
-                </va-button>
-              </template>
-              <div class="pa-3">
-                <p class="display-3">{{ $t("collapse.content.title") }}</p>
-                <div>
-                  {{ $t("collapse.content.text") }}
-                </div>
-              </div>
-            </va-collapse>
-            <va-collapse v-model="doShowCollapses[5]">
-              <template #header>
-                <va-button style="width: 100%;">
-                  {{ $t("collapse.secondHeader") }}
-                </va-button>
-              </template>
-              <div>
-                <p class="display-3">{{ $t("collapse.content.title") }}</p>
+            <va-accordion v-model="customHeaderAccordionValue">
+              <va-collapse class="mb-4">
+                <template #header>
+                  <va-button style="width: 100%;">
+                    {{ $t("collapse.firstHeader") }}
+                  </va-button>
+                </template>
                 <div class="pa-3">
-                  {{ $t("collapse.content.text") }}
+                  <p class="display-3">{{ $t("collapse.content.title") }}</p>
+                  <div>
+                    {{ $t("collapse.content.text") }}
+                  </div>
                 </div>
-              </div>
-            </va-collapse>
+              </va-collapse>
+              <va-collapse>
+                <template #header>
+                  <va-button style="width: 100%;">
+                    {{ $t("collapse.secondHeader") }}
+                  </va-button>
+                </template>
+                <div>
+                  <p class="display-3">{{ $t("collapse.content.title") }}</p>
+                  <div class="pa-3">
+                    {{ $t("collapse.content.text") }}
+                  </div>
+                </div>
+              </va-collapse>
+            </va-accordion>
           </va-card-content>
         </va-card>
       </div>
@@ -103,7 +107,9 @@ export default {
 
   data() {
     return {
-      doShowCollapses: [false, false, false, false, false, false],
+      basicAccordionValue: [false, false],
+      colorAccordionValue: [false, false],
+      customHeaderAccordionValue: [false, false],
     }
   }
 };
