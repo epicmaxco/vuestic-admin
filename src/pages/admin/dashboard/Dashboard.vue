98acf1896019ae298a3563617163433667ff59cd
m0ksem <m0ksem1337@gmail.com>
2021-03-10 16:16:29 +0200

    Comment unused dashboard-table component.

--- src/pages/admin/dashboard/Dashboard.vue
+++ src/pages/admin/dashboard/Dashboard.vue
@@ -2,11 +2,11 @@
   <div class="dashboard">
     <dashboard-charts />
     <dashboard-info-block />
-    <div class="row">
+    <!-- <div class="row">
       <div class="flex xs12">
         <dashboard-table />
       </div>
-    </div>
+    </div> -->
     <div class="row row-equal">
       <div class="flex xs12 lg6">
         <dashboard-tabs @submit="addAddressToMap"/>
