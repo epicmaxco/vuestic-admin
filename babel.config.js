509a5c5b1876b9ca905bd0a6877f8bfcc1b4dcb3
m0ksem <m0ksem1337@gmail.com>
2021-05-05 10:42:22 +0300

    Remove old files.

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [[
-    '@vue/app',
-    {
-      useBuiltIns: 'entry',
-    },
-  ]],
-}
--- unit/example.spec.js
+++
@@ -1,6 +0,0 @@
-describe('App', () => {
-  // Mock test is required so that jest does not complain we have no tests on job.
-  it('runs a mock test', () => {
-    expect(true).toBe(true)
-  })
-})
