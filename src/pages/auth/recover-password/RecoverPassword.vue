f89c7b73e47ae2eb40ba6b31c329bc01aec9c282
Matthew Mamonov <g.smellyshovel@gmail.com>
2022-06-30 19:04:58 +0300

    Restore Recover Passowrd

--- src/pages/auth/recover-password/RecoverPassword.vue
+++ src/pages/auth/recover-password/RecoverPassword.vue
@@ -1,40 +1,34 @@
 <template>
   <form class="login" @submit.prevent="onsubmit">
-    <div class="row mb-2">
-      <!-- <va-input
-        class="mb-3"
-        v-model="email"
-        type="email"
-        :label="t('auth.email')"
-        :error="!!emailErrors.length"
-        :error-messages="emailErrors"
-      /> -->
-    </div>
-    <div class="row justify--center">
-      <!-- <va-button type="submit" class="my-0">{{ t('auth.reset_password') }}</va-button> -->
+    <va-input
+      v-model="email"
+      class="mb-3"
+      type="email"
+      :label="t('auth.email')"
+      :error="!!emailErrors.length"
+      :error-messages="emailErrors"
+    />
+
+    <div class="d-flex justify--center mt-3">
+      <va-button type="submit" class="my-0">{{ t("auth.reset_password") }}</va-button>
     </div>
   </form>
 </template>
 
-<script>
-  export default {
-    name: "RecoverPassword",
-    data() {
-      return {
-        email: "",
-        emailErrors: [],
-      };
-    },
-    methods: {
-      onsubmit() {
-        if (!this.email) {
-          this.emailErrors = ["Email is required"];
-        } else {
-          this.$router.push("/");
-        }
-      },
-    },
-  };
-</script>
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { useRouter } from "vue-router";
+  import { useI18n } from "vue-i18n";
+  const { t } = useI18n();
 
-<style lang="scss"></style>
+  const email = ref("");
+  const emailErrors = ref<string[]>([]);
+
+  function onsubmit() {
+    if (!email.value) {
+      emailErrors.value = ["Email is required"];
+    } else {
+      useRouter().push("/");
+    }
+  }
+</script>
