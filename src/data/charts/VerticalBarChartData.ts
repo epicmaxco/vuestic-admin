42f983b2e7997a08d914e97256c2135741c25971
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-07-11 12:31:28 +0300

    fix: update chart data

--- src/data/charts/VerticalBarChartData.ts
+++ src/data/charts/VerticalBarChartData.ts
@@ -18,13 +18,13 @@ export function useVerticalBarChartData() {
     ],
     datasets: [
       {
-        label: 'USA',
+        label: 'Last year',
         backgroundColor: themes.primary,
         borderColor: 'transparent',
         data: [50, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
       },
       {
-        label: 'USSR',
+        label: 'Current year',
         backgroundColor: themes.info,
         borderColor: 'transparent',
         data: [50, 10, 22, 39, 15, 20, 85, 32, 60, 50, 20, 30],
