923cf3d94e356e2a47e76ed5dce3530048faaac7
Vitaly Raicheu <vitaly.raichev@epicmax.co>
2022-07-11 15:34:31 +0300

    fix: remove redundant components config

--- src/services/vuestic-ui/global-config.ts
+++ src/services/vuestic-ui/global-config.ts
@@ -1,14 +1,10 @@
 import VaIcon from './components/va-icon'
-import VaToast from './components/va-toast'
-import VaButton from './components/va-button'
 import iconsConfig from './icons-config/icons-config'
 import { COLOR_THEMES } from './themes'
 
 export default {
   components: {
     VaIcon,
-    VaToast,
-    VaButton,
     ...COLOR_THEMES[0].components,
   },
   colors: COLOR_THEMES[0].colors,
