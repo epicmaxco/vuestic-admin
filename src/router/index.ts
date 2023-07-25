8108abc38ba8be3c6e0831693ac9afe11143b5e3
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2023-07-25 16:52:22 +0400

    fix: scroll behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -213,7 +213,17 @@ const routes: Array<RouteRecordRaw> = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  //  mode: process.env.VUE_APP_ROUTER_MODE_HISTORY === 'true' ? 'history' : 'hash',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    // For some reason using documentation example doesn't scroll on page navigation.
+    if (to.hash) {
+      return { el: to.hash, behavior: 'smooth' }
+    } else {
+      document.querySelector('.app-layout__page')?.scrollTo(0, 0)
+    }
+  },
   routes,
 })
 
