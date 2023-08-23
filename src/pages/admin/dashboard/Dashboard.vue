fa99698e2547a7b7d7bacd43267f086180d377ca
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2023-08-23 21:37:52 +0400

    feat: add privacy policy

--- src/pages/admin/dashboard/Dashboard.vue
+++ src/pages/admin/dashboard/Dashboard.vue
@@ -7,7 +7,18 @@
     <div class="grid grid-cols-12 gap-6">
       <dashboard-tabs class="col-span-12 lg:col-span-6" @submit="addAddressToMap" />
 
-      <DashboardMap ref="dashboardMap" class="col-span-12 lg:col-span-6" />
+      <dashboard-map ref="dashboardMap" class="col-span-12 lg:col-span-6" />
+    </div>
+    <div class="flex justify-center mt-2">
+      <va-button
+        class="mr-2"
+        size="small"
+        preset="plain"
+        href="https://www.iubenda.com/privacy-policy/19055770"
+        title="Privacy Policy"
+      >
+        Privacy Policy
+      </va-button>
     </div>
   </div>
 </template>
