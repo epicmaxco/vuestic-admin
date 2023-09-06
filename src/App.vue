cc226b4bfbf65246f2ec404526adb98d67904eeb
Stanislau <maildeal@gmail.com>
2023-09-06 16:26:46 +0300

    chore: swap font. Source Pro -> Inter (#940) (#941)
    This closes #940

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,11 @@
   @import 'scss/main.scss';
 
   #app {
-    font-family: 'Source Sans Pro', Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Inter', Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
+    --va-font-family: 'Inter', sans-serif;
   }
 
   body {
