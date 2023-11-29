1d25d7923dec7a86e9da820d142536b7a1c67858
Vasyl <vasyl.hryha@epicmax.co>
2023-11-29 16:45:48 +0200

    - rework ProfileDropdown.vue

---
+++ src/components/navbar/components/GitHubButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <VaButton preset="secondary" color="textPrimary" href="https://github.com/epicmaxco/vuestic-admin" target="_blank">
+    <VaIcon :component="VaIconGitHub" color="#000" />
+  </VaButton>
+</template>
+
+<script lang="ts" setup>
+import VaIconGitHub from '../../icons/VaIconGitHub.vue'
+</script>
