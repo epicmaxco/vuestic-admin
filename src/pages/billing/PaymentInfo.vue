be528c4f1069843dcb4d04c1565b5f538287c3f1
Vasyl <vatres@ukr.net>
2023-12-04 15:49:56 +0200

    Update Billing Page (#989)
    * - add more invoices
    - add `show more/less` button
    
    * - add paymentPlan modal
    - updated PaymentInfo.vue
    
    * - clean payment-cards.ts store
    
    * - chore:rename Change card -> Payment preferences
    
    * - chore: fix modal message

--- src/pages/billing/PaymentInfo.vue
+++ src/pages/billing/PaymentInfo.vue
@@ -2,6 +2,7 @@
   <VaCard class="mb-6">
     <VaCardContent>
       <h3 class="h3">Payment info</h3>
+
       <div class="flex items-center justify-between md:justify-items-stretch">
         <div
           class="flex grow flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-1 justify-between items-start md:items-center"
@@ -15,10 +16,12 @@
             </p>
           </div>
         </div>
+
         <div class="md:w-48 flex flex-col justify-end items-end">
-          <VaButton :to="{ name: 'pricing-plans' }" preset="primary"
-            >Switch to {{ paymentPlan.isYearly ? 'monthly' : 'annual' }}
+          <VaButton preset="primary" @click="togglePaymentPlanModal">
+            Switch to {{ paymentPlan.isYearly ? 'monthly' : 'annual' }}
           </VaButton>
+
           <div v-if="!paymentPlan.isYearly" class="mt-2 text-regularSmall">
             <span>{{ paymentPlan.priceYear }}&nbsp;/year</span>
             <span class="text-danger ml-1">save 16%</span>
@@ -39,19 +42,28 @@
             </div>
           </div>
           <div class="md:w-48 flex justify-end">
-            <VaButton :to="{ name: 'payments' }" preset="primary">Change card</VaButton>
+            <VaButton :to="{ name: 'payments' }" preset="primary">Payment preferences</VaButton>
           </div>
         </div>
       </template>
     </VaCardContent>
   </VaCard>
+
+  <ChangeYourPaymentPlan
+    v-if="isChangeYourPaymentPlanModalOpen"
+    :yearly-plan="paymentPlan.isYearly"
+    @confirm="updatePaymentPlan"
+    @cancel="togglePaymentPlanModal"
+  />
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from 'vue'
 import { usePaymentCardsStore } from '../../stores/payment-cards'
-import { computed } from 'vue'
 
-const paymentPlan = {
+import ChangeYourPaymentPlan from './modals/ChangeYourPaymentPlan.vue'
+
+const paymentPlan = ref({
   id: '1',
   name: 'Gold',
   isYearly: false,
@@ -62,9 +74,19 @@ const paymentPlan = {
   priceYear: '$69.99',
   switchToYearlySave: '16%',
   uploadLimit: '100MB',
-}
+})
 
 const cardStore = usePaymentCardsStore()
 
+const isChangeYourPaymentPlanModalOpen = ref(false)
+
 const paymentCard = computed(() => cardStore.currentPaymentCard)
+const togglePaymentPlanModal = () => {
+  isChangeYourPaymentPlanModalOpen.value = !isChangeYourPaymentPlanModalOpen.value
+}
+
+const updatePaymentPlan = () => {
+  paymentPlan.value.isYearly = !paymentPlan.value.isYearly
+  isChangeYourPaymentPlanModalOpen.value = false
+}
 </script>
