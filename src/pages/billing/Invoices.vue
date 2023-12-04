0f5c4c4676ee4d037e1ac88f6c442823e92a0f21
Vasyl <vasyl.hryha@epicmax.co>
2023-12-04 11:49:56 +0200

    - add more invoices - add `show more/less` button

--- src/pages/billing/Invoices.vue
+++ src/pages/billing/Invoices.vue
@@ -2,7 +2,7 @@
   <VaCard class="mb-6">
     <VaCardContent>
       <h3 class="h3">Invoices</h3>
-      <template v-for="(item, index) in items" :key="item.id">
+      <template v-for="(item, index) in itemsInView" :key="item.id">
         <div class="flex items-center justify-between md:justify-items-stretch">
           <div class="flex items-center w-48">
             {{ item.date }}
@@ -15,29 +15,75 @@
           </div>
         </div>
 
-        <VaDivider v-if="index !== items.length - 1" />
+        <VaDivider v-if="index !== itemsInView.length - 1" />
       </template>
     </VaCardContent>
+    <VaCardActions vertical class="flex flex-wrap content-center mt-4">
+      <VaButton v-if="numberOfInvoicesInVIew < maxNumberOfInvoices" preset="primary" @click="increaseNumberOfInvoices()"
+        >Show more
+      </VaButton>
+      <VaButton v-else preset="primary" @click="numberOfInvoicesInVIew = minNumberOfInvoices">Show less </VaButton>
+    </VaCardActions>
   </VaCard>
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from 'vue'
 import { useToast } from 'vuestic-ui'
+import { useI18n } from 'vue-i18n'
 
 const { init } = useToast()
+const { locale } = useI18n()
 
-const items = [
-  {
-    id: '1',
-    date: 'March 25, 2022',
-    amount: '$6.99',
-  },
-  {
-    id: '2',
-    date: 'February 24, 2022',
-    amount: '$6.99',
-  },
-]
+const minNumberOfInvoices = 7
+const maxNumberOfInvoices = 20
+
+const numberOfInvoicesInVIew = ref(minNumberOfInvoices)
+
+const increaseNumberOfInvoices = (step = 10) => {
+  numberOfInvoicesInVIew.value = Math.min(numberOfInvoicesInVIew.value + step, maxNumberOfInvoices)
+}
+
+function getRandomDateInBetween(start: string, end: string): Date {
+  const startDate = Date.parse(start)
+  const endDate = Date.parse(end)
+
+  return new Date(Math.floor(Math.random() * (endDate - startDate + 1) + startDate))
+}
+
+function getLanguageCode(): string {
+  const countryCodeToLanguageCodeMapping: Record<any, string> = {
+    br: 'pt',
+    cn: 'zh-CN',
+    gb: 'en-GB',
+    ir: 'fa',
+  }
+
+  return countryCodeToLanguageCodeMapping[locale.value] || 'en-GB'
+}
+
+function getRandomDateString(): string {
+  const startDate = '2020-01-01'
+  const endDate = '2023-12-01'
+
+  const dateFormatOptions: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+
+  return getRandomDateInBetween(startDate, endDate).toLocaleDateString(getLanguageCode(), dateFormatOptions)
+}
+
+const allItems = Array.from({ length: maxNumberOfInvoices }, (_, i) => ({
+  id: i,
+  date: getRandomDateString(),
+  amount: `$${(Math.random() * 100).toFixed(2)}`,
+}))
+
+const itemsInView = computed(() => {
+  return allItems.slice(0, numberOfInvoicesInVIew.value)
+})
 
 const download = () => {
   init({
