98cfa78097490bd1c891685724cbfa3097b8de60
Roman4437 <118062031+Roman4437@users.noreply.github.com>
2023-12-27 12:41:48 +0100

    Settings page fixes (#998)

--- src/pages/settings/language-switcher/LanguageSwitcher.vue
+++ src/pages/settings/language-switcher/LanguageSwitcher.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center justify-between">
     <p>Language</p>
     <div class="w-40">
-      <VaSelect v-model="model" class="ellipsis" :options="options" />
+      <VaSelect v-model="model" :options="options" />
     </div>
   </div>
 </template>
@@ -11,9 +11,7 @@ import { computed } from 'vue'
 
 import { useI18n } from 'vue-i18n'
 
-interface LanguageMap {
-  [key: string]: string
-}
+type LanguageMap = Record<string, string>
 
 const { locale } = useI18n()
 
@@ -35,14 +33,10 @@ const languageCodes: LanguageMap = {
 
 const languageName: LanguageMap = Object.fromEntries(Object.entries(languageCodes).map(([key, value]) => [value, key]))
 
-const options = Object.entries(languageCodes).map((language) => language[1])
+const options = Object.values(languageCodes)
 
 const model = computed({
-  get() {
-    return languageCodes[locale.value]
-  },
-  set(value) {
-    return (locale.value = languageName[value])
-  },
+  get() { return languageCodes[locale.value] },
+  set(value) { locale.value = languageName[value] },
 })
 </script>
--- src/pages/settings/notifications/Notifications.vue
+++ src/pages/settings/notifications/Notifications.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="flex flex-col p-4 bg-backgroundSecondary rounded-lg">
     <h3 class="h3 mb-6">Notifications you receive</h3>
-    <div v-for="option of options" :key="option" class="group">
+    <div class="group" v-for="notification in notifications" :key="notification.name">
       <div class="flex items-center justify-between">
-        <p class="text-lg leading-[26px]">
-          {{ option.name }}
+        <p class="text-regularLarge">
+          {{ notification.name }}
         </p>
-        <VaSwitch v-model="option.isEnabled" size="small" @update:modelValue="update(options)" />
+        <VaSwitch v-model="notification.isEnabled" size="small" />
       </div>
       <VaDivider class="py-4 group-last:hidden" />
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { reactive } from 'vue'
 import { useNotificationsStore } from '../../../stores/notifications'
 
-const { notifications, update } = useNotificationsStore()
-
-const options = reactive(notifications)
+const { notifications } = useNotificationsStore()
 </script>
