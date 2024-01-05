35846efed501f4d5b06441f111e0c2146ec260e3
Vasyl <vasyl.hryha@epicmax.co>
2024-01-05 10:46:39 +0200

    - profile dropdown fix margin between sections

--- src/components/navbar/components/dropdowns/ProfileDropdown.vue
+++ src/components/navbar/components/dropdowns/ProfileDropdown.vue
@@ -9,7 +9,7 @@
           </span>
         </VaButton>
       </template>
-      <VaDropdownContent class="profile-dropdown__content md:w-60 px-0 py-4 w-full">
+      <VaDropdownContent class="profile-dropdown__content md:w-60 px-0 py-4 w-full space-y-2">
         <VaList v-for="group in options" :key="group.name" class="space-y-2">
           <header
             v-if="group.name"
