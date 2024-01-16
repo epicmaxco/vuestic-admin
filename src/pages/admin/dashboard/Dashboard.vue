822657c5132eb32fec973deb30a0b4794a618462
Vasyl <vasyl.hryha@epicmax.co>
2024-01-17 01:32:00 +0200

    - feat: update Dashboard.vue

--- src/pages/admin/dashboard/Dashboard.vue
+++ src/pages/admin/dashboard/Dashboard.vue
@@ -1,5 +1,22 @@
-<template>TODO</template>
+<template>
+  <h1 class="h1">Dashboard</h1>
+  <section class="grid grid-cols-8 gap-4">
+    <RevenueUpdates class="col-span-5" />
+    <div class="col-span-3 flex">
+      <YearlyBreakup />
+    </div>
+    <DataSection class="col-span-8" />
+    <RevenueByLocationMap class="col-span-5" />
+    <div class="col-span-3">todo</div>
+    <ProjectTable class="col-span-4" />
+    <div class="col-span-4">todo</div>
+  </section>
+</template>
 
-<script lang="ts" setup></script>
-
-<style lang="scss"></style>
+<script lang="ts" setup>
+import RevenueUpdates from './RevenueUpdates.vue'
+import ProjectTable from './ProjectTable.vue'
+import RevenueByLocationMap from './RevenueByLocationMap.vue'
+import DataSection from './DataSection.vue'
+import YearlyBreakup from './YearlyBreakup.vue'
+</script>
