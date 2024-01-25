30fcb3e840b60d0cacbd70a650d630f435be7272
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2024-01-25 15:35:27 +0300

    fix(settings): fix overflow

--- src/pages/settings/notifications/Notifications.vue
+++ src/pages/settings/notifications/Notifications.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col p-4 bg-backgroundSecondary rounded-lg">
     <h3 class="h3 mb-6">Notifications you receive</h3>
     <div v-for="notification in notifications" :key="notification.name" class="group">
-      <div class="flex items-center justify-between">
+      <div class="flex items-center justify-between overflow-x-hidden">
         <p class="text-regularLarge">
           {{ notification.name }}
         </p>
