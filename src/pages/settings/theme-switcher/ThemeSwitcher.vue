7138b65d11bbf1a60f2c471f8698e57f524faf00
Yauheni Prakopchyk <yauheni.prakopchyk@epicmax.co>
2024-01-26 16:43:25 +0300

    fix: button toogle color

--- src/pages/settings/theme-switcher/ThemeSwitcher.vue
+++ src/pages/settings/theme-switcher/ThemeSwitcher.vue
@@ -1,5 +1,10 @@
 <template>
-  <VaButtonToggle v-model="theme" :options="options" />
+  <VaButtonToggle
+    v-model="theme"
+    color="background-element"
+    border-color="background-element"
+    :options="options"
+  />
 </template>
 <script lang="ts" setup>
 import { computed } from 'vue'
