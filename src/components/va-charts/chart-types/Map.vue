179ed726e163294fe0ff3c78c272dec59f7d67a2
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-01-26 05:13:12 +0200

    fix(map): make map chart

---
+++ src/components/va-charts/chart-types/Map.vue
@@ -0,0 +1,65 @@
+<template>
+  <canvas ref="canvas" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, ChartOptions } from 'chart.js'
+import { ChoroplethController, ProjectionScale, ColorScale, GeoFeature } from 'chartjs-chart-geo'
+import { watchEffect } from 'vue'
+import { ChartData } from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+  ChoroplethController,
+  ProjectionScale,
+  ColorScale,
+  GeoFeature,
+)
+
+const canvas = ref<HTMLCanvasElement | null>(null)
+
+function getColor(revenue: number) {
+  return revenue >= 0.9 ? '#63A6F8' : revenue > 0.4 ? '#8FC0FA' : '#EDF0F1'
+}
+
+const props = defineProps<{
+  chartOptions?: ChartOptions<'choropleth'>
+  chartData: ChartData<'choropleth', { feature: any; value: number }[], string>
+}>()
+
+watchEffect(() => {
+  if (canvas.value === null) {
+    return
+  }
+
+  new ChartJS(canvas.value.getContext('2d')!, {
+    type: 'choropleth',
+    data: props.chartData,
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        projection: {
+          axis: 'x',
+          projection: 'mercator',
+          projectionScale: 1.6,
+        },
+        color: {
+          axis: 'x',
+          quantize: 5,
+          display: false,
+          interpolate: getColor,
+        },
+      },
+    },
+  })
+})
+</script>
