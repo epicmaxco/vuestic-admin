860b8086d7cd5e94ae4897f303c00fb6cdb553dc
Maksim Nedoshev <m0ksem1337@gmail.com>
2024-02-05 17:09:55 +0200

    chore: add admin-landing page to navbar

--- src/components/navbar/components/AppNavbarActions.vue
+++ src/components/navbar/components/AppNavbarActions.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="app-navbar-actions">
     <GithubButton v-if="!isMobile" class="app-navbar-actions__item" />
-    <NotificationDropdown class="app-navbar-actions__item" />
+    <VaButton
+      v-if="!isMobile"
+      preset="secondary"
+      href="https://admin-landing.vuestic.dev/"
+      target="_blank"
+      color="textPrimary"
+      class="app-navbar-actions__item flex-shrink-0 mx-0"
+    >
+      {{ t('aboutVuesticAdmin') }}
+    </VaButton>
     <VaButton
       v-if="!isMobile"
       preset="secondary"
@@ -12,6 +21,7 @@
     >
       {{ t('helpAndSupport') }}
     </VaButton>
+    <NotificationDropdown class="app-navbar-actions__item" />
     <ProfileDropdown class="app-navbar-actions__item app-navbar-actions__item--profile mr-1" />
   </div>
 </template>
